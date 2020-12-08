25aa4a9f55d955800076776d003767d62b7ec5cf
Yuki-ISOBE-tcu-acsl <g1312009@tcu.ac.jp>
2020-12-08 19:05:34 +0900

    コミットのテスト

---
+++ sample/Controller_HL_ATMEC.m
@@ -0,0 +1,88 @@
+function Controller_HL_ATMEC(agent)
+%% controller class demo (1) : construct
+% controller property をController classのインスタンス配列として定義
+dt = agent(1).model.dt;
+Controller_param.P=getParameter();
+
+% HL_Controller
+% %定量誤差大
+% Controller_param.F1=lqrd([0 1;0 0],[0;1],diag([10,1]),[1],dt);                                % z 
+% Controller_param.F2=lqrd([0 1 0 0;0 0 1 0;0 0 0 1; 0 0 0 0],[0;0;0;1],diag([1000,10,10,1]),[1],dt); % xdiag([100,10,10,1])
+% Controller_param.F3=lqrd([0 1 0 0;0 0 1 0;0 0 0 1; 0 0 0 0],[0;0;0;1],diag([1000,10,10,1]),[1],dt); % ydiag([100,10,10,1])
+% Controller_param.F4=lqrd([0 1;0 0],[0;1],diag([100,1]),[1],dt);                       % ヨー角 
+%定量誤差小
+Controller_param.F1=lqr([0 1;0 0],[0;1],diag([1,1]),[1]);
+Controller_param.F2=lqr([0 1 0 0;0 0 1 0;0 0 0 1; 0 0 0 0],[0;0;0;1],diag([100,10,10,1]),[0.1]);
+Controller_param.F3=lqr([0 1 0 0;0 0 1 0;0 0 0 1; 0 0 0 0],[0;0;0;1],diag([100,10,10,1]),[0.1]);
+Controller_param.F4=lqr([0 1;0 0],[0;1],diag([1,1]),[1]);
+
+Controller_param.dt = dt;
+ eig(diag([1,1,1],1)-[0;0;0;1]*Controller_param.F2)
+ 
+Controller.type="HLController_ATMEC";
+Controller.name="hlcontrollerATMEC";
+Controller.param=Controller_param;
+
+%% サブシステムモデル
+Controller.param.A2 = [0 1;0 0];
+Controller.param.B2 = [0;1];
+Controller.param.A4 =diag([1 1 1],1);
+Controller.param.B4 = [0;0;0;1];
+
+%モデルを離散化 x[i+1] = Ad*x[i] + Bd*u[i]
+sys = ss(Controller.param.A2,Controller.param.B2,zeros(2),[0;0]);
+d2 = c2d(sys,dt);
+Controller.param.A2d = d2.A;
+Controller.param.B2d = d2.B;
+sys = ss(Controller.param.A4,Controller.param.B4,zeros(4),[0;0;0;0]);
+d4 = c2d(sys,dt);
+Controller.param.A4d = d4.A;
+Controller.param.B4d = d4.B;
+
+% c2dを使わない1階微分までの近似計算版
+% A2d = eye(2)+obj.A2*dt;
+% B2d = dt*obj.B2;
+% A4d = eye(4)+obj.A4*dt;
+% B4d = dt*obj.B4;
+
+%% MEC_param.
+Kz = [200 50];
+% Kz = [100,0];
+% % Kz = [65.5882 61.2427];
+Kx = [50, 0 ,0, 0];
+Ky = [50, 0, 0, 0];
+
+%w/o MEC
+% Kz = [0 0];
+% Kx = [0 0 0 0];
+% Ky = [0 0 0 0];
+ 
+Controller.param.K = [Kz Kx Ky];
+
+%% RLS_param.
+%推定開始時刻
+Controller.param.FRIT_begin = 0;
+Controller.param.RLS_begin = 20;
+%z
+Controller.param.gamma_z = 10; %初期相関係数
+% Controller.param.alpha_z = 0.01; %ローパスフィルタ強度
+Controller.param.alpha_z = 0; %100%古い情報で更新->更新しない
+Controller.param.lambda_z = 0.99; %忘却係数
+% Controller.param.lambda_z = 1;
+%x
+Controller.param.gamma_x = 1.0; %初期相関係数
+Controller.param.alpha_x = 1.0; %ローパスフィルタ強度
+Controller.param.lambda_x = 0.9999; %忘却係数
+%y
+Controller.param.gamma_y = 1.0; %初期相関係数
+Controller.param.alpha_y = 1.0; %ローパスフィルタ強度
+Controller.param.lambda_y = 0.9999; %忘却係数
+
+%% set2agent
+for i = 1:length(agent)
+    agent(i).set_controller(Controller);
+end
+
+%assignin('base',"Controller_param",Controller_param);
+
+end
---
+++ sample/Controller_HL_MEC.m
@@ -0,0 +1,32 @@
+function Controller_HL_MEC(agent)
+%% controller class demo (1) : construct
+% controller property をController classのインスタンス配列として定義
+dt = agent(1).model.dt;
+Controller_param.P=getParameter();
+% Controller_param.F1=lqrd([0 1;0 0],[0;1],diag([10,1]),[1],dt);                                % z 
+% Controller_param.F2=lqrd([0 1 0 0;0 0 1 0;0 0 0 1; 0 0 0 0],[0;0;0;1],diag([1000,10,10,1]),[1],dt); % xdiag([100,10,10,1])
+% Controller_param.F3=lqrd([0 1 0 0;0 0 1 0;0 0 0 1; 0 0 0 0],[0;0;0;1],diag([1000,10,10,1]),[1],dt); % ydiag([100,10,10,1])
+% Controller_param.F4=lqrd([0 1;0 0],[0;1],diag([100,1]),[1],dt);                       % ヨー角 
+Controller_param.F1=lqr([0 1;0 0],[0;1],diag([1,1]),[1]);
+Controller_param.F2=lqr([0 1 0 0;0 0 1 0;0 0 0 1; 0 0 0 0],[0;0;0;1],diag([100,1,1,1]),[0.1]);
+Controller_param.F3=lqr([0 1 0 0;0 0 1 0;0 0 0 1; 0 0 0 0],[0;0;0;1],diag([100,1,1,1]),[0.1]);
+Controller_param.F4=lqr([0 1;0 0],[0;1],diag([1,1]),[1]);
+
+Controller_param.dt = dt;
+ eig(diag([1,1,1],1)-[0;0;0;1]*Controller_param.F2)
+ 
+Controller.type="HLController_plusMEC_quadcopter";
+Controller.name="hlcontrollerPlusMEC";
+Controller.param=Controller_param;
+
+% Controller.param.K = [150.0 50.0 50.0];
+Controller.param.K = [100.0 50.0 50.0];
+% Controller.param.K = [0 0 0];
+
+for i = 1:length(agent)
+    agent(i).set_controller(Controller);
+end
+
+%assignin('base',"Controller_param",Controller_param);
+
+end
