8758734a1c30ef88594859f1058caac803929553
Yuki-ISOBE-tcu-acsl <g1312009@tcu.ac.jp>
2020-12-10 15:47:36 +0900

    IdealModelをmethod内に組み込み

--- controller/ATMEC_add/IdealModel.m
+++
@@ -1,8 +0,0 @@
-function result = IdealModel(A,B,state,ref,F)
-%AT-MEC 補償ゲインチューニングのFRITアルゴリズムの入力変換関数
-%   理想モデルMを含む計算
-u = F * (ref - state);
-state = A * state + B * u;
-% state = A*state;
-result = state;
-end
