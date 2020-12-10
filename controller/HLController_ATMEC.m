75914b1a359b2ea4896e1a5c8cdc77ddffdc6611
Yuki-ISOBE-tcu-acsl <g1312009@tcu.ac.jp>
2020-12-10 15:47:36 +0900

    IdealModelをmethod内に組み込み

--- controller/HLController_ATMEC.m
+++ controller/HLController_ATMEC.m
@@ -286,6 +286,15 @@ classdef HLController_ATMEC < CONTROLLER_CLASS
         function show(obj)
             obj.result
         end
+        function result = IdealModel(A,B,state,ref,F)
+            %AT-MEC 補償ゲインチューニングのFRITアルゴリズムの入力変換関数
+            %   理想モデルMを含む計算
+            u = F * (ref - state);
+            state = A * state + B * u;
+            % state = A*state;
+            result = state;
+        end
+
     end
 end
 
