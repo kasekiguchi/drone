97a3fac1428d822d15ed7f79bbafdaa397b00955
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2021-06-09 17:11:56 +0900

    Update consensus_agreement.m
    ボロノイを参考に合意制御のリファレンスクラスの作成と修正

--- reference/consensus_agreement.m
+++ reference/consensus_agreement.m
@@ -1,46 +1,66 @@
-function [tc2,xc2,uc2] = consensus_agreement(t0,t1,x0)
-    disp("NowSimulating");
-    tc2 = t0;
-    xc2 = x0;
-    uc2 = situation.u_hold;
+classdef consensus_agreement < REFERENCE_CLASS
+    % 合意重心を算出するクラス
+    %   詳細説明をここに記述
     
-    %------------------------計算-------------------------
-    for t = t0 : +situation.h : t1
-        %入力値の計算
-%   各エージェントへの入力値を計算
-
-    %-------シミュレーションの進み具合を表示--------
-    if rem(t,1) < 1.e-3  
-        disp('.') 
-    end	
-   
-    %オフセットを入れる場合はコメントアウトを外す
-%     X = X+situation.offset;
+    properties
+        param
+        self
+        offset
+    end
     
-    %---------------入力の算出------------------
-    %合意制御:ui=-k((xi-oi)-(xj-oj))
-    k=0.3;
-    Ux = -k * situation.L * X(1:4:4*situation.AgentNumber) - X(2:4:4*situation.AgentNumber);
-    Uy = -k * situation.L * X(3:4:4*situation.AgentNumber) - X(4:4:4*situation.AgentNumber);
-%     Ux(1)=-k*(X(1)-7);
-%     Uy(1)=-k*(X(3)-8);
-    U = [Ux,Uy];
+    methods
+        %%　計算式
+        function obj = consensus_agreement(self,offset)
+            obj.self = self;
+            obj.offset = offset;
+            obj.result.state = STATE_CLASS(struct('state_list',["p"],'num_list',[3]));
+        end
 
-         U = link_eqs(t,x0);
-         sol = ode45(@(t,X) diff_eqs(t,X,U),[t,t+situation.h],x0);
-
-         tc = linspace(t,t+situation.h,100);
-         xc = deval(tc,sol);
-         uc = repmat(U,1,100);
-
-
-    %--------------TC,XC,UCの行列を構成--------------
-         tc2 = [tc2,tc(:,end)];
-         xc2 = [xc2,xc(:,end)];
-         uc2 = [uc2,uc(:,end)];
-     
-    %--------------エージェントの状態の更新-------------
-         x0 = xc(:,end);
-
+            
+       %% 目標位置
+        function  result= do(obj,param)
+            % 【Input】Param = {sensor,estimator,env,param}
+            %  param = range, pos_range, d, void,
+            % 【Output】 result = 目標値（グローバル位置）
+            %% 共通設定１：単純ボロノイセル確定
+            sensor = obj.self.sensor.result;%Param{1}.result;　%　他の機体の位置
+            state = obj.self.estimator.result.state;%Param{2}.state; % 自己位置
+%             env = obj.self.env;%Param{3}.param;             % 環境として予測したもの
+%             param = Param{4}; % 途中で変えられる必要があるか？
+%             if isfield(param,'range'); obj.param.r = param.range;  end
+%             if isfield(param,'pos_range'); obj.param.R = param.pos_range;  end
+%             if isfield(param,'d'); obj.param.d = param.d;  end
+%             if isfield(param,'void'); obj.param.void = param.void;  end
+%             r = obj.param.r; % 重要度を測距できるレンジ
+%             R = obj.param.R; % 通信レンジ
+%             d = obj.param.d; % グリッド間隔
+%             void = obj.param.void; % VOID幅
+            if isfield(sensor,'neighbor')
+                neighbor=sensor.neighbor; % 通信領域内のエージェント位置 絶対座標
+            elseif isfield(sensor,'rigid')
+                neighbor=[sensor.rigid(1:size(sensor.rigid,2)~=obj.self.id).p];
+            end
+            if ~isempty(neighbor)% 通信範囲にエージェントが存在するかの判別
+                neighbor_rpos=neighbor-state.p; % 通信領域内のエージェントの相対位置
+    %        if size(neighbor_rpos,2)>=1 % 隣接エージェントの位置点重み更新
+                % 以下は計算負荷を下げられるが重み付きvoronoiをやるとセル形状が崩れる
+                %     tri=delaunay([0,neighbor_rpos(1,:)],[0,neighbor_rpos(2,:)]); % 自機体(0,0)を加えたドロネー三角形分割
+                %     tmpid=tri(logical(sum(tri==1,2)),:); % 1 つまり自機体を含む三角形だけを取り出す．
+                %     tmpid=unique(tmpid(tmpid~=1))-1; % tmpid = 隣接エージェントのインデックス （neighbor_rpos内のインデックス番号）
+                %     neighbor_rpos=neighbor_rpos(:,tmpid); % 隣接エージェントの相対位置
+                %     neighbor.pos=neighbor.pos(:,tmpid); % 隣接エージェントの位置
+                %     neighbor.weight=sensor_obj.output.neighbor.weight(tmpid); % neighbor weight
+                %     neighbor.mass=sensor_obj.output.neighbor.mass(tmpid); % neighbor mass
+%                 Vn=voronoi_region([[0;0;0],(neighbor_rpos)],[R,R;-R,R;-R,-R;R,-R],1:size(neighbor,2)+1);% neighborsとのみボロノイ分割（相対座標）
+            else % 通信範囲にエージェントがいない場合
+%                 Vn=voronoi_region([0;0;0],[R,R;-R,R;-R,-R;R,-R],1);
+            end
+            
+            obj.result.state.p = (state.p+sensor.neighbor(:,1)+sensor.neighbor(:,2)+sensor.neighbor(:,3))/4;%(state.p+sensor.neighbor(:,1)+sensor.neighbor(:,2)+sensor.neighbor(:,3))/N;%.*[1;1;0]; % 重心位置（絶対座標）
+%             obj.result.state.p(3) = 1; % リファレンス高さは１ｍ
+            result = obj.result;
+        end
+        function show(obj,param)
+        end
     end
 end
