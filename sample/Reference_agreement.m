8eb2acb5a0962cea966d60f00f0fb601181141b3
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2021-06-11 19:10:31 +0900

    Update Reference_agreement.m
    機体数に応じたオフセット距離の導入

--- sample/Reference_agreement.m
+++ sample/Reference_agreement.m
@@ -1,10 +1,18 @@
-function Reference = Reference_agreement(agent)
+function Reference = Reference_agreement(agent,N)
     %% reference class demo
     % reference property をReference classのインスタンス配列として定義
     clear Reference
     Reference.type=["consensus_agreement"];
     Reference.name=["agreement"];
-    Reference.param.void=0.3;
-    if isfield(agent.sensor,'rdensity'); Reference.param.r = agent.sensor.rdensity.r;  end
-    if isfield(agent.sensor,'rpos'); Reference.param.R = agent.sensor.rpos.r;  end
+    Reference.param.N=N;
+    if N==3
+    Reference.param.offset=3*[-2*cos(pi/6),2*cos(pi/6),0.0;
+                              -2*sin(pi/6),-2*sin(pi/6),2.0;
+                               0.0,0.0,0.0,];
+    end
+    if N==4
+    Reference.param.offset=4*[-5.0,-5.0,5.0,5.0;
+                              -5.0,5.0,-5.0,5.0;
+                               0.0,0.0,0.0,0.0];
+    end
 end
