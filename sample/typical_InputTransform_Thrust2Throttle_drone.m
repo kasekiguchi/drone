2eb57508bda1c81886b89a39d08834fd062b3a60
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2021-07-21 20:32:39 +0900

    Update typical_InputTransform_Thrust2Throttle_drone.m
    共通プログラムではメインでfor文が回るのでその部分を削除

--- sample/typical_InputTransform_Thrust2Throttle_drone.m
+++ sample/typical_InputTransform_Thrust2Throttle_drone.m
@@ -1,9 +1,6 @@
-function typical_InputTransform_Thrust2Throttle_drone(agent,varargin)
+function u_trans_param=typical_InputTransform_Thrust2Throttle_drone(varargin)
 % typical input transformation from thrust force to throttle level for
 % drone Prop. input
-for i = 1:length(agent)
-    agent(i).input_transform=[];
-end
 u_trans_param.type="Thrust2Throttle_drone";
 u_trans_param.name="t2t";
 u_trans_param.param.gain = 0.5*[3*180/pi;3*180/pi;180/pi;50];%[500/400*180/pi;0];
@@ -11,7 +8,4 @@ u_trans_param.param.th_offset = 1021;
 u_trans_param.param.roll_offset = 1101;
 u_trans_param.param.pitch_offset = 1101;
 u_trans_param.param.yaw_offset = 1101;
-for i = 1:length(agent)
-    agent(i).set_input_transform(u_trans_param); 
-end
 end
