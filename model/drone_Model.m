ae9e524732c3c335694410ae96648b8da70f38e8
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2021-07-21 20:18:41 +0900

    Update drone_Model.m
    他のモデルクラスに合わせた名前に修正

--- model/drone_Model.m
+++ model/drone_Model.m
@@ -1,12 +1,12 @@
-classdef drone_Model < MODEL_CLASS
+classdef Drone_Model < MODEL_CLASS
     % Discrete time model
 %     properties
 %         Property1
 %     end
     
     methods
-        function obj = drone_Model(args)
-            obj= obj@MODEL_CLASS("drone_Model",args);
+        function obj = Drone_Model(args)
+            obj= obj@MODEL_CLASS("Drone_Model",args);
             %obj.id = self.id
             % 
             %   Ú×à–¾‚ð‚±‚±‚É‹Lq
