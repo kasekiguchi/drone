3bec456f344bc31a386bb7f5d82f324342d38e35
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2021-07-21 20:25:37 +0900

    Create Controller_tracebirds.m
    害鳥用コントローラのサンプルファイル

---
+++ sample/Controller_tracebirds.m
@@ -0,0 +1,8 @@
+function Controller  = Controller_tracebirds(N)
+%% controller class demo (1) : construct
+% controller property をController classのインスタンス配列として定義
+Controller.type="pestbirdsController";
+Controller.name="pestbirds";
+Controller.param=N;
+
+end
