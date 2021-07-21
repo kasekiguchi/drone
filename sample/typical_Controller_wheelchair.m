5c23676e12df4429e4cc437460960be7fd2d3b18
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2021-07-21 20:30:24 +0900

    Delete typical_Controller_wheelchair.m
    名前修正したのを作成したので消去

--- sample/typical_Controller_wheelchair.m
+++
@@ -1,9 +0,0 @@
-function Controller  = typical_Controller_wheelchair(N)
-%% controller class demo (1) : construct
-% controller property をController classのインスタンス配列として定義
-Controller.type="wheelchairController";
-Controller.name="wheelchair";
-Controller.param={""};
-
-
-end
