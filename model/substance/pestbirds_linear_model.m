e21de1bb0953d4a3f83c852a99aa1b8cefdf769b
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2021-08-14 20:24:32 +0900

    Update pestbirds_linear_model.m
    変換式のイメージ説明文

--- model/substance/pestbirds_linear_model.m
+++ model/substance/pestbirds_linear_model.m
@@ -1,4 +1,13 @@
 function X = pestbirds_linear_model(x,u,param)
     u(1) = u(1)*param.K;
-    X = [u(1)*cos(x(3));u(1)*sin(x(3));u(3)];
+%     [roll pitch yaw] = f(q)
+    X = [u(1)*cos(u(3));u(1)*sin(u(3));u(2);u(4);u(5);u(6);u(7)];
+%     [q0 q1 q2 q3] = f(X'(roll pitch yaw ))
+%     X = X'[x y z]+[q0 q1 q2 q3]
+%     A = [1 1;1 1]
+%     b = [0;1]
+%     u = [u1]
+%     x = [x1;x2]
+%     X = A*x+bu
+%     X = [x1+x2;x1+x2+u1]
 end
