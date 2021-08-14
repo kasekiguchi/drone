d6dfa0c929e5efb8f45519bf6e5520744251c5cf
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2021-08-14 20:29:40 +0900

    Update Reference_tracebirds.m
    メインで様々な値を与えることにしたので，リファレンスクラス内では何も渡さないように変更

--- sample/Reference_tracebirds.m
+++ sample/Reference_tracebirds.m
@@ -1,4 +1,4 @@
-function Reference = Reference_tracebirds(id,Nb,tex,N,fp,want_falm,position,distance)
+function Reference = Reference_tracebirds(id,Nb,tex)
 %% reference class demo
 % reference property をReference classのインスタンス配列として定義
 %1機目はwaypoint，2機目以降はボロノイとか
@@ -7,23 +7,13 @@ if tex==1
             clear Reference
             Reference.type=["trace_birds_drone"];
             Reference.name=["trace_drone"];
-            Reference.param{1}=N;
-            Reference.param{2}=Nb;
-            Reference.param{3}=distance;
-            Reference.param{4}=fp;
-            Reference.param{5}=want_falm(id);
-            Reference.param{6}=position;
+            Reference.param={""};
 %             agent(i).set_reference(Reference);
         else
             clear Reference
             Reference.type=["trace_birds_pestbirds"];
             Reference.name=["trace_pestbirds"];
-            Reference.param{1}=N;
-            Reference.param{2}=Nb;
-            Reference.param{3}=distance;
-            Reference.param{4}=fp;
-            Reference.param{5}=want_falm(id);
-            Reference.param{6}=position;
+            Reference.param={""};
 %             agent(i).set_reference(Reference);
         end
 elseif tex==2
@@ -31,23 +21,13 @@ elseif tex==2
             clear Reference
             Reference.type=["trace_birds_drone"];
             Reference.name=["trace_drone"];
-            Reference.param{1}=N;
-            Reference.param{2}=Nb;
-            Reference.param{3}=distance;
-            Reference.param{4}=fp;
-            Reference.param{5}=want_falm(id);
-            Reference.param{6}=position;
+            Reference.param={""};
 %             agent(i).set_reference(Reference);
         else
             clear Reference
             Reference.type=["trace_birds_pestbirds_Dis"];
             Reference.name=["trace_pestbirds"];
-            Reference.param{1}=N;
-            Reference.param{2}=Nb;
-            Reference.param{3}=distance;
-            Reference.param{4}=fp;
-            Reference.param{5}=want_falm(id);
-            Reference.param{6}=position;
+            Reference.param={""};
 %             agent(i).set_reference(Reference);
         end
 elseif tex==3
@@ -55,23 +35,13 @@ elseif tex==3
             clear Reference
             Reference.type=["trace_birds_drone"];
             Reference.name=["trace_drone"];
-            Reference.param{1}=N;
-            Reference.param{2}=Nb;
-            Reference.param{3}=distance;
-            Reference.param{4}=fp;
-            Reference.param{5}=want_falm(id);
-            Reference.param{6}=position;
+            Reference.param={""};
 %             agent(i).set_reference(Reference);
         else
             clear Reference
             Reference.type=["wheelchair_trace_birds_pestbirds_Dis"];
             Reference.name=["trace_pestbirds"];
-            Reference.param{1}=N;
-            Reference.param{2}=Nb;
-            Reference.param{3}=distance;
-            Reference.param{4}=fp;
-            Reference.param{5}=want_falm(id);
-            Reference.param{6}=position;
+            Reference.param={""};
 %             agent(i).set_reference(Reference);
         end
 end
