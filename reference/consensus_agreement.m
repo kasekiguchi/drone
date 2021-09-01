e70a24b19b8d3938132df0316e18fafeafb1ff44
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2021-09-01 18:11:17 +0900

    Update consensus_agreement.m
    コメント追加
    計算式の修正

--- reference/consensus_agreement.m
+++ reference/consensus_agreement.m
@@ -1,6 +1,6 @@
 classdef consensus_agreement < REFERENCE_CLASS
-    % 合意重心を算出するクラス
-    %   詳細説明をここに記述
+    % 合意重心を指定して隊列を形成するクラス
+    % 指定すれば各時刻における合意重心の算出も可能
     
     properties
         param
@@ -22,12 +22,13 @@ classdef consensus_agreement < REFERENCE_CLASS
             sensor = obj.self.sensor.result;%Param{1}.result;　%　他の機体の位置
             state = obj.self.estimator.result.state;%Param{2}.state; % 自己位置
 
-            ni = size(sensor.neighbor,2);
-            if ni==0
+            ni = size(sensor.neighbor,2);%センサレンジ内にある他機体の位置情報
+            consensus_point = [0;0;0]; %合意重心
+            if ni==0 %近くに他の機体がいない
                 obj.result.state.p = state.p;
             else
-%                 obj.result.state.p = [1;0;0]+obj.offset(:,obj.self.id);
-                obj.result.state.p = (state.p+(ni+1)*(obj.offset(:,obj.self.id))+sum(sensor.neighbor,2))/(ni+1); %逐次合意点を算出
+                obj.result.state.p = consensus_point+obj.offset(:,obj.self.id); %合意重心を設定して隊列を形成
+%                 obj.result.state.p = (state.p+(ni+1)*(obj.offset(:,obj.self.id))+sum(sensor.neighbor,2))/(ni+1); %逐次合意重心を算出
             end
             result = obj.result;
         end
