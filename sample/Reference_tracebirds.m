8189b1e075fd2cb934b9e63555638fdb9b7574fc
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2021-09-02 12:37:41 +0900

    Update Reference_tracebirds.m
    不要な分岐を修正
    それに伴い不要な引数も削除

--- sample/Reference_tracebirds.m
+++ sample/Reference_tracebirds.m
@@ -1,34 +1,15 @@
-function Reference = Reference_tracebirds(id,Nb,tex)
+function Reference = Reference_tracebirds(id,Nb)
 %% reference class demo
 % reference property をReference classのインスタンス配列として定義
-%1機目はwaypoint，2機目以降はボロノイとか
-if tex==1
-        if id>=Nb+1%最終機体は犬になる予定なのでsheep_refはない
-            clear Reference
-            Reference.type=["trace_birds_drone"];
-            Reference.name=["trace_drone"];
-            Reference.param={""};
-%             agent(i).set_reference(Reference);
-        else
-            clear Reference
-            Reference.type=["trace_birds_pestbirds_Dis"];
-            Reference.name=["trace_pestbirds"];
-            Reference.param={""};
-%             agent(i).set_reference(Reference);
-        end
-elseif tex==2
-        if id>=Nb+1%最終機体は犬になる予定なのでsheep_refはない
-            clear Reference
-            Reference.type=["trace_birds_drone"];
-            Reference.name=["trace_drone"];
-            Reference.param={""};
-%             agent(i).set_reference(Reference);
-        else
-            clear Reference
-            Reference.type=["wheelchair_trace_birds_pestbirds_Dis"];
-            Reference.name=["trace_pestbirds"];
-            Reference.param={""};
-%             agent(i).set_reference(Reference);
-        end
-end
+    if id>=Nb+1%ここはドローンの分岐
+                clear Reference
+                Reference.type=["trace_birds_drone"];
+                Reference.name=["trace_drone"];
+                Reference.param={""};
+    else%ここは害鳥の分岐
+                clear Reference
+                Reference.type=["wheelchair_trace_birds_pestbirds_Dis"];
+                Reference.name=["trace_pestbirds"];
+                Reference.param={""};
+    end
 end
