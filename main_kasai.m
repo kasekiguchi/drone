9016271dfd9051b986fda75d751b4a3ce8d1e975
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2021-09-13 16:24:21 +0900

    笠井
    ユニット数変更

--- main_kasai.m
+++ main_kasai.m
@@ -9,7 +9,7 @@ close all hidden; clear all; clc;
 userpath('clear');
 % warning('off', 'all');
 %% general setting
-N = 1; %number of agents（if bird_trace system else number of all units）
+N = 10; %number of agents（if bird_trace system else number of all units）
 Nb = 8; %number of pestbirds
 Na = N - Nb; %number of agents
 fp = farm_create(5);%畑のエリア生成．（）内の数は畑の数(1～9)．害鳥追跡用
