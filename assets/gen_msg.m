0a58b937d8b33d626a87f1e5b2086a5b23881eb6
kasekiguchi <kasekiguchi@gmail.com>
2021-12-04 08:31:52 +0900

    modified

--- assets/gen_msg.m
+++ assets/gen_msg.m
@@ -1,4 +1,7 @@
 function msg = gen_msg(input)
+% Function : generate msg to send through the transmitter
+% How to use : gen_msg([500 500 0 500 0 0 0 0])
+% The values correspond roll pitch throttle yaw aux1 aux2 aux3 aux4 channel
 for j = 1:1:8
     pw(1, j + 0)   = fix(input(1, j) / 100);
     pw(1, j + 8)   = rem(input(1, j),  100);
