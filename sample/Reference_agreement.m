9c1f0d5a31895870094f083c3d0ccfcd31811ea8
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2022-01-07 16:30:37 +0900

    kasai
    合意制御の隊列
    ・実機実験に合わせた隊列半径に修正

--- sample/Reference_agreement.m
+++ sample/Reference_agreement.m
@@ -1,38 +1,15 @@
-function Reference = Reference_agreement(N)
+function Reference = Reference_Agreement(N) 
     %% reference class demo
     % 合意制御の目標隊列
     % reference property をReference classのインスタンス配列として定義
     % 返し値は配置したい隊列
     clear Reference
-    Reference.type=["consensus_agreement"];
+    Reference.type=["ConsensusAgreement"];
     Reference.name=["agreement"];
     pos = linspace(0,2*pi,N); %円形に配置
     Reference.param = zeros(3,N);
-    r = 0.5; %隊列の半径
+    r = 1; %隊列の半径
     for i=1:N
         Reference.param(:,i)=[r*cos(pos(i));r*sin(pos(i));0.0];
     end
-%     l = 5;
-%     pos = linspace(0,l,N); %左向きV字
-%     Reference.param = zeros(3,N);
-%     theta = pi/6;
-%     Reference.param(:,1) = [-pos(1)*cos(theta);pos(1)*sin(theta);0.0];
-%     for i=2:N
-%         if mod(i,2)==0
-%             Reference.param(:,i)=[-pos(i)*cos(theta);pos(i)*sin(theta);0.0];
-%         else
-%             Reference.param(:,i)=[-pos(i-1)*cos(theta);pos(i-1)*-sin(theta);0.0];
-%         end
-%     end
-%     l = 5;
-%     pos = linspace(0,l,N); %右向きV字
-%     Reference.param = zeros(3,N);
-%     theta = pi/6;
-%     for i=1:N
-%         if mod(i,2)==0
-%             Reference.param(:,i)=[pos(i)*cos(theta);pos(i)*sin(theta);0.0];
-%         else
-%             Reference.param(:,i)=[pos(i)*cos(theta);pos(i)*-sin(theta);0.0];
-%         end
-%     end
 end
