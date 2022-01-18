232fd1ee339ca9c0d6cd63ab220467cba7831391
eikyuwataru <g1712017@tcu.ac.jp>
2022-01-18 22:49:51 +0900

    Update Suspended-Load system
    ・拡張したモデル・コントローラに変更
    →getParameter_withload.mの値を変更すれば拡張前と同じモデルにできる
    ・オイラー角の牽引モデル削除
    →不要になったため

--- model/get_model_name.m
+++ model/get_model_name.m
@@ -13,8 +13,6 @@ function model_full_name = get_model_name(sn)
         case "R 18"
             model_full_name = "rotation_matrix_thrust_force_physical_parameter_model";
         case "Load"
-            model_full_name = "with_load_model";
-        case "Load_Euler"
             model_full_name = "euler_with_load_model";
 %         "euler","euler_angle_model",12;
 %         "rodrigues","rodrigues_parameter_model",12;
--- sample/Model_Suspended_Load.m
+++ sample/Model_Suspended_Load.m
@@ -5,7 +5,8 @@ if ~isempty(varargin)
 end
 Model.type="Suspended_Load_Model"; % class name
 Model.name="load"; % print name
-Setting.dim=[25,4,16];
+Setting.projection = @(x)[x(1:18);x(19:21)/norm(x(19:21));x(22:24)];
+Setting.dim=[24,4,19];
 Setting.input_channel = ["f","M"];
 Setting.method = get_model_name("Load"); % model dynamicsの実体名
 Setting.state_list =  ["p","q","v","w","pL","vL","pT","wL"];
@@ -14,11 +15,12 @@ Setting.initial.vL = [0;0;0];
 Setting.initial.pT = [0;0;-1];
 Setting.initial.wL = [0;0;0];
 %Setting.initial.pL = Setting.initial.p+getParameter_withload("L")*Setting.initial.pT;
-Setting.initial.pL = Setting.initial.p+0.5*Setting.initial.pT;
-Setting.num_list = [3,4,3,3,3,3,3,3];
+% Setting.initial.pL = Setting.initial.p+0.5*Setting.initial.pT;
+Setting.num_list = [3,3,3,3,3,3,3,3];
 % Setting.type="compact"; % unit quaternionr
 Setting.dt = dt;
 Setting.param = getParameter_withload; % モデルの物理パラメータ設定
+Setting.initial.pL = Setting.initial.p+Setting.param(16)*Setting.initial.pT+[Setting.param(17);Setting.param(18);-Setting.param(19)];
 if strcmp(type,"plant")
         Model.id = id;
         Setting.param = getParameter_withload("Plant"); % モデルの物理パラメータ設定
