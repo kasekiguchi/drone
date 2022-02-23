27bc3d24f0199f07be28a701c2f68535eea00d1d
sawada <g1812060@tcu.ac.jp>
2022-02-23 22:39:36 +0900

    プログラム更新（卒論記述範囲内）
    変更点
    ・消火活動導入のためのグラフ強連結化
    ・保存したLoggerデータからマップの作成と被害セル数の算出

--- multi-agent/MA_main.m
+++ multi-agent/MA_main.m
@@ -31,7 +31,8 @@ nx = 130; % x axis grid number
 ny = 130; % y axis grid number
 N = nx*ny; % total grid number
 Il = 30; % length of I
-
+nx_app = 100; % 見かけ上のx辺
+ny_app = 100; % 見かけ上のy辺
 %% environment definition
 %[E,W] = make_grid_graph(nx,ny,@(x,y)0.1*ones(size(x))); % flat weight
 %[E,W] = make_grid_graph(nx,ny,@(x,y)0.01*[1:size(x,2)]'.^2.*ones(size(x)),1); % flat weight
@@ -61,21 +62,21 @@ end
 %% 消火しない場合の燃え広がり方 (h = 0)
 % 200ステップで端に行かない程度の重みがAstar でやる場合適切
 % Directでやる場合はもっと早い燃え広がりでも対応可能
-ke = 250; % シミュレーションステップ
+ken = 200; % シミュレーションステップ ke_natural
 fFPosition = 6; % flag fire position
 h = 0; % extinction probability
+map_extra = nx * ny - nx_app * ny_app ;     %見えない部分の総セル数
 W_vec = reshape(W,N,1);
 map = model_init(N,Il,h,nx,ny,fFPosition,W_vec);
 %%
 clear logger
-logger.k=zeros(1,ke);
+logger.k=zeros(1,ken);
 logger.S(:,1) = map.S(:);
 logger.I(:,1) = map.I(:);
 logger.R(:,1) = map.R(:);
 logger.U(:,1) = map.U(:);
 
-
-for k = 1:ke
+for k = 1:ken
     map.next_step_func(0,E);% map 更新
     % log
     k
@@ -85,108 +86,126 @@ for k = 1:ke
     logger.R(:,k) = map.R(:);
     logger.U(:,k) = map.U(:);
     %手動設定[飛び火＆長距離延焼]
-    if k == 23      %11:21
-        tbh1 = nx*56 + 48;
-        map.I(tbh1) =1 ;
-    end
-    if k == 42      %11:58
-        tbh2 = nx*34 + 56;
-        map.I(tbh2) =1 ;
-    end
-    if k == 50      %12:14
-        tbh3 = nx*30 + 73;
-        map.I(tbh3) =1 ;
-    end
-    if k == 68      %12:49
-        tbh4 = nx*70 + 48;
-        map.I(tbh4) =1 ;
-    end
-    if k == 74      %13:00
-        tbh5 = nx*69 + 89;
-        map.I(tbh5) =1 ;
-    end
-    if k == 87      %13:26
-        tbh6 = nx*78 + 58;
-        map.I(tbh6) =1 ;
-    end
-    if k == 89      %13:30  道路を跨いだ長距離延焼
-        ens = nx*33 + 90;
-        map.I(ens) =1 ;
-    end
-    if k == 97      %13:45
-        tbh7 = nx*39 + 47;
-        map.I(tbh7) =1 ;
-    end
-    if k == 108      %14:07
-        tbh8 = nx*81 + 50;
-        map.I(tbh8) =1 ;
-    end
-    if k == 116      %14:24
-        tbh9 = nx*24 + 95;
-        map.I(tbh9) =1 ;
-    end
-    if k == 151      %15:33
-        tbh10 = nx*25 + 85;
-        map.I(tbh10) =1 ;
-    end
-    if k == 78      %13:07
-        tbh11 = nx*54 + 56;
-        map.I(tbh11) =1 ;
-    end
-    if k == 79      %13:08
-        tbh12 = nx*64 + 64;
-        map.I(tbh12) =1 ;
-    end
-    if k == 90      %13:31
-        tbh13 = nx*71 + 70;
-        map.I(tbh13) =1 ;
-    end
-    if k == 90      %13:31
-        tbh14 = nx*70 + 81;
-        map.I(tbh14) =1 ;
-    end
-    if k == 97      %13:44
-        tbh15 = nx*78 + 65;
-        map.I(tbh15) =1 ;
-    end
-    %手動設定[消火活動]
-%     if k == 12      %11:00
-%         syk1 = nx*56 + 48;
-%         map.I(tbh1) = 0 ;
+%     if k == 23      %11:21
+%         tbh1 = nx*56 + 48;
+%         map.I(tbh1) =1 ;
+%     end
+%     if k == 42      %11:58
+%         tbh2 = nx*34 + 56;
+%         map.I(tbh2) =1 ;
+%     end
+%     if k == 50      %12:14
+%         tbh3 = nx*30 + 73;
+%         map.I(tbh3) =1 ;
+%     end
+%     if k == 68      %12:49
+%         tbh4 = nx*70 + 48;
+%         map.I(tbh4) =1 ;
+%     end
+%     if k == 74      %13:00
+%         tbh5 = nx*69 + 89;
+%         map.I(tbh5) =1 ;
+%     end
+%     if k == 87      %13:26
+%         tbh6 = nx*78 + 58;
+%         map.I(tbh6) =1 ;
+%     end
+%     if k == 89      %13:30  道路を跨いだ長距離延焼
+%         ens = nx*33 + 90;
+%         map.I(ens) =1 ;
+%     end
+%     if k == 97      %13:45
+%         tbh7 = nx*39 + 47;
+%         map.I(tbh7) =1 ;
+%     end
+%     if k == 108      %14:07
+%         tbh8 = nx*81 + 50;
+%         map.I(tbh8) =1 ;
+%     end
+%     if k == 116      %14:24
+%         tbh9 = nx*24 + 95;
+%         map.I(tbh9) =1 ;
+%     end
+%     if k == 151      %15:33
+%         tbh10 = nx*25 + 85;
+%         map.I(tbh10) =1 ;
+%     end
+%     if k == 78      %13:07
+%         tbh11 = nx*54 + 56;
+%         map.I(tbh11) =1 ;
+%     end
+%     if k == 79      %13:08
+%         tbh12 = nx*64 + 64;
+%         map.I(tbh12) =1 ;
+%     end
+%     if k == 90      %13:31
+%         tbh13 = nx*71 + 70;
+%         map.I(tbh13) =1 ;
+%     end
+%     if k == 90      %13:31
+%         tbh14 = nx*70 + 81;
+%         map.I(tbh14) =1 ;
+%     end
+%     if k == 97      %13:44
+%         tbh15 = nx*78 + 65;
+%         map.I(tbh15) =1 ;
 %     end
 end
 %% graphs
-%map.draw_state(nx,ny,map.loggerk(logger,1));
-% nx = 100; %実際のマップ上での表示範囲
-% ny = 100; %実際のマップ上での表示範囲
-map.draw_state(nx,ny,W);
+% map.draw_state(nx,ny,map.loggerk(logger,10));  %loggerk内の数字は何ステップ目の状態化を表す
+% nx=100, ny=100 でマップ上に表示
+map.draw_state(nx,ny,W)
 %% animations
-
-map.draw_movie(logger,nx,ny,1,"2022Jan11_自然延焼_手動飛び火_マップ更新_2");    %natural_expansion 
+% map.draw_movie(logger,nx,ny,1);
+map.draw_movie(logger,nx,ny,1,"2022Feb23_Natural_Log44");    %natural_expansion 
 %map.draw_movie(logger,nx,ny,1,"Extinct_alt_page_rank_random");
 %M=map.draw_movie(logger,nx,ny,2);
 %map.draw_movie(logger,nx,ny,1,"Extinct_APR_Astar_BiasRandom");
-%map.draw_movie(logger,nx,ny,1,"Extinct_Weight_Astar_BiasRandom");
-
+%%
+% map.save('Logger_Feb24_Natural_Log1.mat',logger); %logger保存
+%% plot
+tmp1=[];
+logger.I2 = logical (logger.I)
+for i = 1:ken
+    tmpS(i)=sum(logger.S(:,i))-map_extra; %logger(i)内にあるRの200s時(ken)の値の合計
+    tmpI(i)=sum(logger.I2(:,i)); %logger(i)内にあるRの200s時(ken)の値の合計
+    tmpR(i)=sum(logger.R(:,i)); %logger(i)内にあるRの200s時(ken)の値の合計
+end
+damage_cell = tmpR(ken)+tmpI(ken)   %被害セル数
+%%
+figure(1)
+plot(1:ken,tmpS,'-g','LineWidth',2);
+hold on 
+plot(1:ken,tmpI,'-b','LineWidth',2);
+plot(1:ken,tmpR,'-r','LineWidth',2);
+legend('S','I','R');
+xlabel('\sl Time step k','FontSize',20);
+ylabel('\sl Number','FontSize',20);
+set(gca,'FontSize',10);
+ax = gca;
+ax.Box = 'on';
+xlim([0 ken]);
+ylim([0 nx_app * ny_app]);
+hold off
 %% Monte-Carlo simulation
-unum = 10; % 消火点の数（10機のUAV）
-ke = 250; % シミュレーションステップ
-kn = 100;% number of Monte-Carlo simulation
+unum = 15; % 消火点の数（10機のUAV）
+ke = 200; % シミュレーションステップ
+kn = 1;% number of Monte-Carlo simulation
 % 手法選択
 fMethod = "WAPR"; % Weighted Alt Page Rank
-%fMethod = "APR"; % Alt Page Rank
-%fMethod = "Weight"; % 重み行列
-fDynamics = "Astar"; % 消火方法：A star or Direct
-%fDynamics = "Direct"; % 消火方法：A star or Direct
+% fMethod = "APR"; % Alt Page Rank
+% fMethod = "Weight"; % 重み行列
+% fDynamics = "Astar"; % 消火方法：A star or Direct
+fDynamics = "Direct"; % 消火方法：A star or Direct
 vi = 5; % 消火の必要がない部分を飛ばす距離
 %map.draw_state(nx,ny,map.loggerk(logger,ke));
-fFPosition = 0;
-h = 0.9;
+fFPosition = 6;
+h = 0.1; % extinction probability
 clear Logger
 
 if fMethod=="WAPR"
     % 重み拡張したもの
-    L = speye(size(E)) - E/eigs(E,1);
+    L = speye(size(E)) - E/eigs(E,1);   %size(E)=(n,m)のときEはn行m列 / eigs(E,1)はEの最も大きい固有値1個
     [V,Eig,Flag]=eigs(L',1,'smallestabs','Tolerance',1e-20); % V : alt page rank
     %[V2,Eig2,Flag2]=eigs(E'/eigs(E,1),1,'largestreal','Tolerance',1e-20); % V : alt page rank
     %V=V2;
@@ -204,85 +223,71 @@ elseif fMethod == "APR"
     X = V;
     Xm = min(0,min(V));% 最低のrank
     XM = max(V);% 最高のrank        
-else
+elseif fMethod == "Weight"
     % 重みに従う場合
     X = W;
     Xm = min(W,[],'all');
     XM = max(W,[],'all');
+else
+    disp("Method Error");
 end
 w2 = 1/((XM-Xm)/max(nx,ny))
-%% MC simulation
+V_mat = reshape(V,[nx,ny]);     %重みAPRグラフ表示のための正方行列化
+map.draw_state(nx,ny,V_mat);    %重みAPRのグラフ表示
+% map.draw_state(nx,ny,reshape(V,[nx,ny]))
+%% MC simulation 時間かかる
 for k = 1:kn
-    k;
+    k
     map = model_init(N,Il,h,nx,ny,fFPosition,W_vec);% initialize
     [K(k),Logger(k)] = Astar_SIR(N,ke,nx,ny,map,unum,E,X,Xm,Il,vi,1,w2,fDynamics); % simulation
-    K(k)
+    K(k);
 end
-%%
-tmp=[];
-for i = 1:100
-tmp(i)=sum(Logger(i).R(:,end));
+%% %%% 違法増築1 Logデータの保存
+map.save('Logger_Feb23_WAPR_Direct_unum15_vi5_h0.1_Log1.mat',Logger);
+% map.save('K_Feb22_WAPR_Direct_unum15_vi5_h0.1_Log2.mat',K);
+%% %%% 違法増築2 動画の生成と保存
+% Logger2=map.load('Logger_APR_Astar_100_30_09_004_10_5.mat');
+% K2 = map.load('K_APR_Astar_100_30_09_004_10_5.mat');
+% md = map.draw_state(nx,ny,map.loggerk(Logger,10));    %nステップ目を画像出力
+% M = map.draw_movie(Logger(1),nx,ny,1);    %動画を出力
+% M=map.draw_movie(Logger(1),nx,ny,1,"2022Feb22_WAPR_Direct_unum15_kn30_vi5_h0.1_Log64");  %名前を付けて動画を出力
+%% %%% 違法増築3 Logデータの読み込み
+Logger=map.load('Logger_Feb22_WAPR_Direct_unum15_vi5_h0.1_Log40.mat');
+%% plot 2
+kre = K-1;
+% kre = 200;    %Kの値は手動入力
+tmpS2 = 0;
+tmpI2 = 0;
+tmpR2 = 0;
+tmp2=[];
+I2.I = logical (Logger.I)
+for i = 1:kre
+    tmpS2(i)=sum(Logger.S(:,i))-map_extra; %logger(i)内にあるRの200s時(ke)の値の合計
+    tmpI2(i)=sum(I2.I(:,i)); %logger(i)内にあるRの200s時(ke)の値の合計
+    tmpR2(i)=sum(Logger.R(:,i)); %logger(i)内にあるRの200s時(ke)の値の合計
 end
-sum(tmp)/100
-sum(K>300)
-%% MCの平均時間遷移グラフ
-log = Logger3;
-logk = K3;
-ke = 300;
-for i = 1:ke
-    I = find(logk>i);
-    II = find(logk<=i);
-    tmpS(i) = 0;
-    tmpR(i) = 0;
-    tmpU(i) = 0;
-    for j = II
-        log(j).S(:,i) = log(j).S(:,i-1);
-        log(j).R(:,i) = log(j).R(:,i-1);
-        log(j).U(:,i) = log(j).U(:,i-1);
-    end
-    for j = 1:100
-        tmpS(i)=tmpS(i)+sum(log(j).S(:,i));
-        tmpR(i)=tmpR(i)+sum(log(j).R(:,i));
-        tmpU(i)=tmpU(i)+sum(log(j).U(:,i));
-    end
-    tmpS(i) = tmpS(i)/100;
-    tmpR(i) = tmpR(i)/100;
-    tmpU(i) = tmpU(i)/100;
-    tmpI(i)=10000-tmpS(i)-tmpR(i);
-end
+damage_cell = tmpR2(kre)+tmpI2(kre)
 %%
-% tmpS= sum(logger.S);
-% tmpR= sum(logger.R);
-% tmpI= 10000-tmpS-tmpR;
-%%
-plot(1:ke,tmpS,'--g','LineWidth',5);
+figure(2)
+plot(1:kre,tmpS2,'-g','LineWidth',2);
 hold on 
-plot(1:ke,tmpI,'-.r','LineWidth',5);
-plot(1:ke,tmpR,'Color',[0.2 0.2 0.2],'LineWidth',5);
-legend('N','F','E');
- xlabel('\sl Time step k','FontSize',25);
-            ylabel('\sl Number','FontSize',25);
-            set(gca,'FontSize',20);
-            ax = gca;
-            ax.Box = 'on';
+plot(1:kre,tmpI2,'-b','LineWidth',2);
+plot(1:kre,tmpR2,'-r','LineWidth',2);
+legend('S','I','R');
+xlabel('\sl Time step k','FontSize',20);
+ylabel('\sl Number','FontSize',20);
+set(gca,'FontSize',10);
+ax = gca;
+ax.Box = 'on';
+xlim([0 ke]);
+ylim([0 nx_app * ny_app]);
 hold off
+
+disp('Plot ended')
+%% END
+%　ここまでが卒論mainプログラム
+%
 %%
-sum(K>300)
-SIR_model.save('Logger_APR_ver0_Astar_100_30_09_004_10_5_1_w2.mat',Logger);
-SIR_model.save('K_APR_ver0_Astar_100_30_09_004_10_5_1_w2.mat',K);
-% SIR_model.save('Logger_APR_Astar_100_30_09_004_10_5.mat',Logger);
-% SIR_model.save('K_APR_Astar_100_30_09_004_10_5.mat',K);
-%%
-Logger1=SIR_model.load('Logger_WAPR_ver0_Astar_100_30_09_004_10_5_1_w2.mat');
-K1 = SIR_model.load('K_WAPR_ver0_Astar_100_30_09_004_10_5_1_w2.mat');
-Logger2=SIR_model.load('Logger_W_ver0_Astar_100_30_09_004_10_5_1_w2.mat');
-K2 = SIR_model.load('K_W_ver0_Astar_100_30_09_004_10_5_1_w2.mat');
-Logger3=SIR_model.load('Logger_APR_ver0_Astar_100_30_09_004_10_5_1_w2.mat');
-K3 = SIR_model.load('K_APR_ver0_Astar_100_30_09_004_10_5_1_w2.mat');
-%M=SIR_model.draw_movie(Logger(2),nx,ny,2);
-%tt=SIR_model.load('Logger_APR_Astar_100_30_09_004_10_5.mat')
-%%
-map.draw_state(nx,ny,reshape(V,[nx,ny]))
 %% local functions
 %% model init
 function map= model_init(N,Il,h,nx,ny,fFPosition,W_vec)
@@ -408,8 +413,8 @@ while (k <= ke) && sum(find(map.I))
         
         % A star アルゴリズム (上のfor文とくっつけられるが分けたほうが見やすそう)
         for i = 1:tunum
-            path = aster(p(i),t(pt(i)),E1,@(c,t)grid_distance(c,t,nx,ny),@(Z) performance(X,Xm,map.I,Z),w1,w2);
-            % path : A starで求めたp(i)からt(pt(i))へのpathのインデックス配列
+            c=p(i);
+            path = aster(c,t(pt(i)),E1,@(c,t)grid_distance(c,t,nx,ny),@(Z) performance(X,Xm,map.I,Z),w1,w2);    %cにp(i)
             logger.P(:,k) = logger.P(:,k) | sparse(path,1,1,N,1);
             u(i) = path(1);% 実際の消化点
             if length(path)>1
