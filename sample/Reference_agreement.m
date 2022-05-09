09d5d536d14f34de6ccdd202c147e7ac56357024
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2022-05-09 16:43:27 +0900

    Update Reference_agreement.m
    kasai

--- sample/Reference_agreement.m
+++ sample/Reference_agreement.m
@@ -4,8 +4,8 @@ function Reference = Reference_Agreement(N)
     % reference property をReference classのインスタンス配列として定義
     % 返し値は配置したい隊列
     clear Reference
-    Reference.type=["ConsensusAgreement"];
-    Reference.name=["agreement"];
+    Reference.type="ConsensusAgreement";
+    Reference.name="agreement";
     pos = linspace(0,2*pi,N); %円形に配置
     Reference.param = zeros(3,N);
     r = 1; %隊列の半径
