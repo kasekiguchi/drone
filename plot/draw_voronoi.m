f454cb6c1fd32974fa80d502350b94e87f2721c8
kasekiguchi <kasekiguchi@gmail.com>
2022-05-13 16:52:12 +0900

    tmp modify
    とりあえずエラー無く回るという状態
    正常には動作していない．

--- plot/draw_voronoi.m
+++ plot/draw_voronoi.m
@@ -1,23 +1,30 @@
-function draw_voronoi(V,span,pos,varargin)
+function draw_voronoi(V,span,pos,region,text)
 % 【Input】V : polyshapeセル配列， span : 対象インデックス，
 %  pos : span毎に"r+","bx","go" で表示
-%   varargin : region : 描画領域,  text = {pos, text} 
+%  region : 描画領域,
+%  text = {pos, text}　で　posの位置に <=text を表示
 %% 描画
+arguments
+    V
+    span
+    pos
+    region = []
+    text = []
+end
 daspect([1 1 1]);
-if isfield(varargin,"range")
-    region=varargin.range;
-xlim([min(region(:,1)),max(region(:,1))]);
-ylim([min(region(:,2)),max(region(:,2))]);
+if ~isempty(region)
+    xlim([min(region(:,1)),max(region(:,1))]);
+    ylim([min(region(:,2)),max(region(:,2))]);
 end
 hold on
 for i = span
- if isa(V{i},'polyshape')
-     plot(V{i});
- else
-    %if area(V{i}) > 0
+    if isa(V{i},'polyshape')
+        plot(V{i});
+    else
+        %if area(V{i}) > 0
         plot(polyshape(V{i}));
-    %end
- end
+        %end
+    end
 end
 num=length(span);
 lt = ["r+","bx","go"];
@@ -25,15 +32,15 @@ if iscell(pos)
     pos = [pos{1,:}];
 end
 for i =1: round(size(pos,2)/size(span,2),0)
-plot(pos(1,num*(i-1)+1:num*i),pos(2,num*(i-1)+1:num*i),lt(i))
+    plot(pos(1,1:num:end),pos(1,2:num:end),lt(i))
 end
-if isfield(varargin,"text")
-    tvar=varargin.text;
+if ~isempty(text)
+    tvar=text;
     tpos=tvar{1};
     tval=tvar{2};
-for i =span
-    text(tpos(1,i),tpos(2,i),strcat('\leftarrow ',num2str(tval{i})));
-end
+    for i =span
+        text(tpos(1,i),tpos(2,i),strcat('\leftarrow ',num2str(tval{i})));
+    end
 end
 hold off
 end
