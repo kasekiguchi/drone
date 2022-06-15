0a316d99fd88d602cea05423978398feb88cb9aa
sekiguchi <kasekiguchi@gmail.com>
2022-06-15 14:30:16 +0900

    ESPr update

--- experiment/arduino/ESPr_PWM/ESPr_PWM.ino
+++ experiment/arduino/ESPr_PWM/ESPr_PWM.ino
@@ -1,40 +1,115 @@
-#define LED  16
-#define AIN1 13  // Right Motor Blue
-#define AIN2 15  // Right Motor Yellow
+// arduino の設定は
+// http://trac.switch-science.com/wiki/esp_dev_arduino_ide
+// CPU Frequency を 160 MHzにしないとFCにPPMを送れない．
+// PPM は　Down pulse
+#include <Arduino.h>
+#include <ESP8266WiFi.h> //https://github.com/esp8266/Arduino
+#include <WiFiUDP.h>
+//#define CPU_FRE 160 // CPUクロック周波数 [MHz]
+
+
+// PWM
 #define PWMA 2
-#define STBY 12
- 
-// PWN
-#define PWN_FREQ 150000 // PWM frequency: 1000Hz(1kHz)
-#define PWN_RANGE 100 // PWN range: 100
-#define SPEED 30  
- 
-void setup() {
-  pinMode(LED, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-   
-  // Initialize GPIO mode
-  pinMode(AIN1, OUTPUT);
-  pinMode(AIN2, OUTPUT);
-  pinMode(STBY, OUTPUT);
- 
-  // Initialize PWN
-  analogWriteFreq(PWN_FREQ);
-  analogWriteRange(PWN_RANGE);
- 
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, LOW);
-  analogWrite(PWMA, SPEED);
-   
-  digitalWrite(STBY, LOW);
-   
+#define PWM_FREQ 144000 // PWM frequency: 1000Hz(1kHz)
+#define PWM_RANGE 100 // PWM range: 100
+uint16_t SPEED = 0; //33;// arming のduty比
+
+char packetBuffer[1];
+
+unsigned int droneNumber = 31; //機体番号を入力
+
+#define OUTPUT_PIN 2 // PPM出力のピン番号 加速度使うなら０
+
+const char *ssid = "ACSLexperimentWiFi";//"acsl-mse-arl-YAMAHA";
+const char *password = "wifi-acsl-mse";
+
+/////////////////// WiFi関係 ////////////////////
+WiFiUDP udp;
+
+// ESPrのIPアドレスの設定
+IPAddress myIP(192, 168, 50, 100 + droneNumber); // 機体により下番号変更
+IPAddress gateway(192, 168, 50, 1);// PCが接続されているネットワークのゲートウェイのIPアドレスを入力する（MATLABのPCのIP）
+const int my_udp_port = 8000 + droneNumber;        //開放する自ポート
+IPAddress subnet(255, 255, 255, 0);
+
+
+//boolean connected = false;
+uint8_t old_receive_data = 0;
+boolean isReceive_Data_Updated = false;
+
+unsigned long last_received_time;
+
+
+//*********** local functions  *************************//
+void connectToWiFi()// ---- setup connection to wifi
+{
+  WiFi.begin(ssid, password);
+  WiFi.config(myIP, gateway, subnet);
+  Serial.println("start_connect");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("CONNECTED!");
+
+  Serial.println("WiFi connected");
+  Serial.print("Local IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println(my_udp_port);
 }
- 
+
+
+void receiveUDP()// ---------- loop function : receive signal by UDP
+{
+  int packetSize = udp.parsePacket();
+  if (packetSize)
+  {
+    int len = udp.read(packetBuffer, packetSize);
+    if (len > 0)
+    {
+      //SPEED = ;
+      SPEED = 70 * min(int(packetBuffer[0]), 100) / 100; //33 + 37*min(int(packetBuffer[0]),100)/100; // FC からのPWM波形を計測した結果
+      Serial.print("Received duty rate:[");
+      Serial.print(" ");
+      Serial.print(SPEED);
+      Serial.print(" ");
+      Serial.print("]\r\n");
+      //Serial.printf("Received %d bytes from %s, port %d\n", packetSize, udp.remoteIP().toString().c_str(), udp.remotePort());
+
+      last_received_time = ESP.getCycleCount();
+      isReceive_Data_Updated = true;
+      analogWrite(PWMA, SPEED);
+    }
+    /*
+     else if (ESP.getCycleCount() - last_received_time >= 0.500 * CPU_FRE * 1000000L)// Stop propellers after 0.5s signal lost.
+    {
+      Serial.print("Set :[");
+      Serial.print(" ");
+      Serial.print(0);
+      Serial.print(" ");
+      Serial.print("]\r\n");
+      analogWrite(PWMA, 0);
+    }*/
+  }
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  connectToWiFi();
+
+  // Initialize PWM
+  analogWriteFreq(PWM_FREQ);
+  analogWriteRange(PWM_RANGE);
+
+  analogWrite(PWMA, 0);
+
+  udp.begin(my_udp_port);
+  unsigned long last_received_time = ESP.getCycleCount();
+}
+
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-                                    // but actually the LED is on; this is because 
-                                    // it is active low on the ESP-01)
-  delay(1000);                      // Wait for a second
-  digitalWrite(LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+  receiveUDP();
 }
