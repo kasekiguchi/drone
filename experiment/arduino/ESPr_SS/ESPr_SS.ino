97b761f90f01a5eb577e467c65c4af23545cdcdd
kasekiguchi <kasekiguchi@gmail.com>
2022-06-20 10:32:22 +0900

    ブロアファン実験用
    張り付き水平移動成功
    uint8([40])

--- experiment/arduino/ESPr_SS/ESPr_SS.ino
+++ experiment/arduino/ESPr_SS/ESPr_SS.ino
@@ -1,18 +1,18 @@
 // arduino の設定は
 // http://trac.switch-science.com/wiki/esp_dev_arduino_ide
 // CPU Frequency を 160 MHzにしないとFCにPPMを送れない．
+// Cristal frequency 26 MHz
 // PPM は　Down pulse
 #include <Arduino.h>
-#include <ESP8266WiFi.h> //https://github.com/esp8266/Arduino
-#include <WiFiUDP.h>
 #define CPU_FRE 160 // CPUクロック周波数 [MHz]
 
+/////////////////// WiFi関係 ////////////////////
+#include <ESP8266WiFi.h> //https://github.com/esp8266/Arduino
+#include <WiFiUDP.h>
 char packetBuffer[1];
-unsigned int droneNumber = 31; //機体番号を入力
+unsigned int droneNumber = 33; //機体番号を入力
 const char *ssid = "ACSLexperimentWiFi";//"acsl-mse-arl-YAMAHA";
 const char *password = "wifi-acsl-mse";
-
-/////////////////// WiFi関係 ////////////////////
 WiFiUDP udp;
 
 // ESPrのIPアドレスの設定
@@ -26,15 +26,15 @@ boolean isReceive_Data_Updated = false;
 
 unsigned long last_received_time;
 
+uint16_t msg;
 
+////////////////// Software Serial /////////////////////////
+#include <SoftwareSerial.h>
+#define TX (12)
+#define RX (14)
 
-////////////// PWM ////////////////////
-#define PWMA 2
-#define PWM_FREQ 24000 // PWM frequency: 1000Hz(1kHz)
-#define PWM_RANGE 100 // PWM range: 100
-uint16_t SPEED = 0; //33;// arming のduty比
-
-
+SoftwareSerial SSerial(RX,TX,false);//(14, 12, false, 256);
+//#define SSerial SoftwareSerial
 //*********** local functions  *************************//
 void connectToWiFi()// ---- setup connection to wifi
 {
@@ -63,26 +63,23 @@ void receiveUDP()// ---------- loop function : receive signal by UDP
     int len = udp.read(packetBuffer, packetSize);
     if (len > 0)
     {
-      SPEED = 70 * min(int(packetBuffer[0]), 100) / 100; 
-      Serial.print("Received duty rate:[");
+      msg = int(packetBuffer[0]); 
+      Serial.print("Received message ");
       Serial.print(" ");
-      Serial.print(SPEED);
+      Serial.print(msg);
       Serial.print(" ");
-      Serial.print("]\r\n");
+      Serial.print("\r\n");
       //Serial.printf("Received %d bytes from %s, port %d\n", packetSize, udp.remoteIP().toString().c_str(), udp.remotePort());
+      SSerial.println(msg);
 
       last_received_time = ESP.getCycleCount();
       isReceive_Data_Updated = true;
-      analogWrite(PWMA, SPEED);
     }
      else if (ESP.getCycleCount() - last_received_time >= 0.500 * CPU_FRE * 1000000L)// Stop propellers after 0.5s signal lost.
     {
-      Serial.print("Set :[");
-      Serial.print(" ");
+      Serial.print("Set :[ ");
       Serial.print(0);
-      Serial.print(" ");
-      Serial.print("]\r\n");
-      analogWrite(PWMA, 0);
+      Serial.print(" ]\r\n");
     }
   }
 }
@@ -90,19 +87,18 @@ void receiveUDP()// ---------- loop function : receive signal by UDP
 
 void setup() {
   Serial.begin(115200);
+  SSerial.begin(9600);
   connectToWiFi();
-
-  // Initialize PWM
-  analogWriteFreq(PWM_FREQ);
-  analogWriteRange(PWM_RANGE);
-
-  analogWrite(PWMA, 1);
-
   udp.begin(my_udp_port);
   unsigned long last_received_time = ESP.getCycleCount();
+
+  Serial.println("\nSoftware serial test started");
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  receiveUDP();
+  receiveUDP();    
+  while (SSerial.available() > 0) {
+    Serial.println("Received from SS : " + SSerial.readStringUntil('\n'));
+    yield();
+  }
 }
