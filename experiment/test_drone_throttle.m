079307ad8c1fcd06f8e460cd39c63d5d5f3729e3
kasekiguchi <kasekiguchi@gmail.com>
2022-07-11 18:50:39 +0900

    whill control

--- experiment/test_drone_throttle.m
+++ experiment/test_drone_throttle.m
@@ -3,28 +3,27 @@ agent = DRONE(Model_Drone_Exp(0.025,[0;0;0], "udp", [50,132]),DRONE_PARAM("DIATO
 pause(1);
     agent.plant.connector.sendData(gen_msg([500,500,0,500,0,0,0,0]));% arming
 %%
-disp("roll")
-for i = 0:10:500
-agent.plant.connector.sendData(gen_msg([i,500,0,500,0,0,0,0]));% arming
-pause(0.1);
-end
-disp("pitch")
-for i = 0:10:500
-agent.plant.connector.sendData(gen_msg([500,i,0,500,0,0,0,0]));% arming
-pause(0.1);
-end
-disp("yaw")
-for i = 0:10:500
-agent.plant.connector.sendData(gen_msg([500,500,0,i,0,0,0,0]));% arming
-pause(0.1);
-end
-disp("throttle")
-for i = 0:10:500
-agent.plant.connector.sendData(gen_msg([500,500,i,500,0,0,0,0]));% arming
-pause(0.1);
-end
-%%
-agent.plant.connector.sendData(gen_msg([0,500,0,500,0,0,0,0]));% arming
+% disp("roll")
+% for i = 0:10:500
+% agent.plant.connector.sendData(gen_msg([i,500,0,500,0,0,0,0]));% 
+% pause(0.1);
+% end
+% disp("pitch")
+% for i = 0:10:500
+% agent.plant.connector.sendData(gen_msg([500,i,0,500,0,0,0,0]));% 
+% pause(0.1);
+% end
+% disp("yaw")
+% for i = 0:10:500
+% agent.plant.connector.sendData(gen_msg([500,500,0,i,0,0,0,0]));% 
+% pause(0.1);
+% end
+% disp("throttle")
+% for i = 0:10:500
+% agent.plant.connector.sendData(gen_msg([500,500,i,500,0,0,0,0]));% 
+% pause(0.1);
+% end
+% agent.plant.connector.sendData(gen_msg([500,500,0,500,0,0,0,0]));% stop
 
 %%
 FH = figure('position', [0 0 eps eps], 'menubar', 'none');
