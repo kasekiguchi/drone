4436e48445ba59c1ec869eaa20272643a6812344
g1812100 <85143997+g1812100@users.noreply.github.com>
2022-07-15 17:34:52 +0900

    サーボ移設
    サーボ移設

--- main.m
+++ main.m
@@ -8,6 +8,13 @@ cellfun(@(xx) addpath(xx), activeFile, 'UniformOutput', false);
 close all hidden; clear all; clc;
 userpath('clear');
 % warning('off', 'all');
+
+%% general setting
+N = 1; % number of agents
+fExp = 1 % 1：実機　それ以外：シミュレーション
+fMotive = 1 % Motiveを使うかどうか
+fOffline = 0; % offline verification with experiment data
+
 run("main1_setting.m");
 run("main2_agent_setup.m");
 %agent.set_model_error("ly",0.02);
@@ -21,6 +28,7 @@ LogAgentData = [% 下のLOGGER コンストラクタで設定している対象a
 logger = LOGGER(1:N, size(ts:dt:te, 2), fExp, LogData, LogAgentData);
 %% main loop
 run("main3_loop_setup.m");
+xf = [0;0;0];
 
 try
     while round(time.t, 5) <= te
@@ -65,10 +73,11 @@ end
 
             % reference
             param(i).reference.covering = [];
-            param(i).reference.point = {FH, [0; 0; 0], time.t};
-            param(i).reference.timeVarying = {time};
+            param(i).reference.point = {FH, [2; 2; 1], time.t};
+            param(i).reference.timeVarying = {time,FH};
             param(i).reference.tvLoad = {time};
             param(i).reference.wall = {1};
+            param(i).reference.tbug = {};
             param(i).reference.agreement = {logger, N, time.t};
             for j = 1:length(agent(i).reference.name)
                 param(i).reference.list{j} = param(i).reference.(agent(i).reference.name(j));
@@ -78,7 +87,7 @@ end
 
             % controller
             param(i).controller.hlc = {time.t, HLParam};
-            param(i).controller.ftcontroller = {time.t, HLParam};
+            param(i).controller.pd = {};
             for j = 1:length(agent(i).controller.name)
                 param(i).controller.list{j} = param(i).controller.(agent(i).controller.name(j));
             end
@@ -96,6 +105,14 @@ end
             model_param.FH = FH;
             agent(i).do_model(model_param); % 算出した入力と推定した状態を元に状態の1ステップ予測を計算
 
+%             if time.t <= 10
+%                 agent(i).input = agent(i).input - [0.5;0.01;0;0]; % 定常外乱
+%             else
+%                 agent(i).input = agent(i).input - [0.8;0.01;0;0];
+%             end
+%                 agent(i).input = agent(i).input - [0+(5/10);0.01;0;0];
+            agent(i).input = agent(i).input;
+                
             model_param.param = agent(i).plant.param;
             agent(i).do_plant(model_param);
         end
@@ -153,8 +170,21 @@ end
 close all
 clc
 % plot 
-logger.plot({1,"p","er"});
-% agent(1).reference.timeVarying.show(logger)
+%logger.plot({1,"p","per"},{1,"controller.result.z",""},{1,"input",""});
+%logger.plot({1,"p","per"},{1,"input",""});
+logger.plot({1,"p1","pr"},"time",[0 50],"fig_num",1);
+legend("off")
+xlabel('Time[s]','FontSize',25)
+ylabel('Position[m]','FontSize',25)
+logger.plot({1,"p2","pr"},"fig_num",2);
+legend("off")
+xlabel('Time[s]','FontSize',25)
+ylabel('Position[m]','FontSize',25)
+logger.plot({1,"p3","pr"},"time",[0 50],"fig_num",3);
+legend("off")
+xlabel('Time[s]','FontSize',25)
+ylabel('Position[m]','FontSize',25)
+%agent(1).reference.timeVarying.show(logger)
 
 %% animation
 %VORONOI_BARYCENTER.draw_movie(logger, N, Env,1:N)
--- main2_agent_setup.m
+++ main2_agent_setup.m
@@ -1,27 +1,33 @@
+%% generate environment
+%Env = DensityMap_sim(Env_2DCoverage); % 重要度マップ設定
+Env = [];
+
 for i = 1:N
     %% generate Drone instance
     % DRONE classのobjectをinstance化する．制御対象を表すplant property（Model classのインスタンス）をコンストラクタで定義する．
     if fExp
-        %agent(i) = DRONE(Model_Drone_Exp(dt,initial(i),"udp",[25]),DRONE_PARAM("DIATONE")); % for exp % 機体番号（ESPrのIP）
-        agent(i) = DRONE(Model_Drone_Exp(dt,initial(i), "serial", COMs(i)),DRONE_PARAM("DIATONE")); % for exp % 機体番号（ArduinoのCOM番号）
-        %agent(i) = DRONE(Model_Drone_Exp(dt,initial(i), "serial", "COM31"),DRONE_PARAM("DIATONE")); % for exp % 機体番号（ArduinoのCOM番号）
-        %agent(i) = Whill(Model_Whill_Exp(dt,initial(i),"ros",[21]),DRONE_PARAM("DIATONE")); % for exp % 機体番号（ESPrのIP）
+        %agent(i) = DRONE(Model_Drone_Exp(dt,initial_state(i),"udp",[50,110]),DRONE_PARAM("DIATONE")); % for exp % 機体番号（ESPrのIP）
+        agent(i) = DRONE(Model_Drone_Exp(dt,initial_state(i),"udp",[25]),DRONE_PARAM("DIATONE")); % for exp % 機体番号（ESPrのIP）
+        %agent(i) = DRONE(Model_Drone_Exp(dt,initial_state(i), "serial", COMs(i)),DRONE_PARAM("DIATONE")); % for exp % 機体番号（ArduinoのCOM番号）
+        %agent(i) = DRONE(Model_Drone_Exp(dt,initial_state(i), "serial", "COM31"),DRONE_PARAM("DIATONE")); % for exp % 機体番号（ArduinoのCOM番号）
+        %agent(i) = Whill(Model_Whill_Exp(dt,initial_state(i),"ros",[21]),DRONE_PARAM("DIATONE")); % for exp % 機体番号（ESPrのIP）
         agent(i).input = [0; 0; 0; 0];
     else
-%         agent(i) = DRONE(Model_Quat13(dt,initial(i),i),DRONE_PARAM("DIATONE")); % unit quaternionのプラントモデル : for sim
-        agent(i) = DRONE(Model_EulerAngle(dt,initial(i), i),DRONE_PARAM("DIATONE"));                % euler angleのプラントモデル : for sim
-        %agent(i) = DRONE(Model_Suspended_Load(dt,'plant',initial(i),i)); % 牽引物込みのプラントモデル : for sim
-        %agent(i) = DRONE(Model_Discrete0(dt,initial(i),i),DRONE_PARAM("DIATONE")); % 離散時間質点モデル（次時刻位置＝入力） : Direct controller（入力＝目標位置） を想定
-        %agent(i) = DRONE(Model_Discrete(dt,initial(i),i),DRONE_PARAM("DIATONE")); % 離散時間質点モデル : PD controller などを想定
+        %agent(i) = DRONE(Model_Quat13(dt,initial_state(i),i),DRONE_PARAM("DIATONE")); % unit quaternionのプラントモデル : for sim
+        agent(i) = DRONE(Model_EulerAngle(dt,initial_state(i), i),DRONE_PARAM("DIATONE"));                % euler angleのプラントモデル : for sim
+        %agent(i) = DRONE(Model_Suspended_Load(dt,'plant',initial_state(i),i)); % 牽引物込みのプラントモデル : for sim
+        %agent(i) = DRONE(Model_Discrete0(dt,initial_state(i),i),DRONE_PARAM("DIATONE")); % 離散時間質点モデル（次時刻位置＝入力） : Direct controller（入力＝目標位置） を想定
+        %[M,P]=Model_Discrete(dt,initial_state(i),i);
+        %agent(i) = DRONE(M,P); % 離散時間質点モデル : PD controller などを想定
     end
 
     %% model
     % set control model
-    agent(i).set_model(Model_EulerAngle(dt,initial(i), i)); % オイラー角モデル
-    %agent(i).set_model(Model_Quat13(dt,initial(i),i)); % オイラーパラメータ（unit quaternion）モデル
-    %agent(i).set_model(Model_Suspended_Load(dt,'model',initial(i),i)); %牽引物込みモデル
-    %agent(i).set_model(Model_Discrete0(dt,initial(i),i)) % 離散時間モデル（次時刻位置＝入力） : Direct controller（入力＝目標位置） を想定 : plantが４入力モデルの時はInputTransform_REFtoHL_droneを有効にする
-    %agent(i).set_model(Model_Discrete(dt,initial(i),i)) % 離散時間質点モデル : plantが４入力モデルの時はInputTransform_toHL_droneを有効にする
+    agent(i).set_model(Model_EulerAngle(dt,initial_state(i), i)); % オイラー角モデル
+    %agent(i).set_model(Model_Quat13(dt,initial_state(i),i)); % オイラーパラメータ（unit quaternion）モデル
+    %agent(i).set_model(Model_Suspended_Load(dt,'model',initial_state(i),i)); %牽引物込みモデル
+    %agent(i).set_model(Model_Discrete0(dt,initial_state(i),i)) % 離散時間モデル（次時刻位置＝入力） : Direct controller（入力＝目標位置） を想定 : plantが４入力モデルの時はInputTransform_REFtoHL_droneを有効にする
+    %agent(i).set_model(Model_Discrete(dt,initial_state(i),i)) % 離散時間質点モデル : plantが４入力モデルの時はInputTransform_toHL_droneを有効にする
     close all
     %% set input_transform property
     if fExp                                                                               % isa(agent(i).plant,"Lizard_exp")
@@ -46,46 +52,50 @@ for i = 1:N
     end
 
     %agent(i).set_property("sensor", Sensor_ROS(struct('ROSHostIP', '192.168.50.21')));
-    %agent(i).set_property("sensor",Sensor_Direct()); % 状態真値(plant.state)　：simのみ
+    %agent(i).set_property("sensor",Sensor_Direct(0.0)); % 状態真値(plant.state)　：simのみ % 入力はノイズの大きさ
     %agent(i).set_property("sensor",Sensor_RangePos(i,'r',3)); % 半径r (第二引数) 内の他エージェントの位置を計測 : sim のみ
     %agent(i).set_property("sensor",Sensor_RangeD('r',3)); %  半径r (第二引数) 内の重要度を計測 : sim のみ
-    %agent(i).set_property("sensor",struct("type","LiDAR_sim","name","lrf","param",[]));
+    %agent(i).set_property("sensor",Sensor_LiDAR(i));
     %% set estimator property
     agent(i).estimator = [];
     %agent(i).set_property("estimator",Estimator_LPF(agent(i))); % lowpass filter
     %agent(i).set_property("estimator",Estimator_AD()); % 後退差分近似で速度，角速度を推定　シミュレーションこっち
     %agent(i).set_property("estimator",Estimator_feature_based_EKF(agent(i),["p","q"],[1e-5,1e-8])); % 特徴点ベースEKF
     %agent(i).set_property("estimator",Estimator_PDAF(agent(i),["p","q"],[1e-5,1e-8])); % 特徴点ベースPDAF
-    agent(i).set_estimator("estimator", Estimator_EKF(agent(i), ["p", "q"], [1e-5, 1e-8])); % （剛体ベース）EKF
+    agent(i).set_property("estimator",Estimator_EKF(agent(i), ["p", "q"], [1e-5, 1e-8])); % （剛体ベース）EKF
+    %agent(i).set_property("estimator",Estimator_KF(agent(i), ["p","v","q"], "Q",1e-5,"R",1e-3)); % （質点）EKF
     %agent(i).set_property("estimator",Estimator_Direct()); % Directセンサーと組み合わせて真値を利用する　：sim のみ
     %agent(i).set_property("estimator",Estimator_Suspended_Load([i,i+N])); %
     %agent(i).set_property("estimator",Estimator_EKF(agent(i),["p","q","pL","pT"],[1e-5,1e-5,1e-5,1e-7])); % （剛体ベース）EKF
     %agent(i).set_property("estimator",struct('type',"MAP_UPDATE",'name','map','param',Env)); % map 更新用 重要度などのmapを時間更新する
     %% set reference property
     agent(i).reference = [];
+    agent(i).set_property("reference",Reference_Time_Varying("line_ref",[0,0,1]));%line
     %agent(i).set_property("reference",Reference_2DCoverage(agent(i),Env,'void',0.1)); % Voronoi重心
-    %agent(i).set_property("reference",Reference_Time_Varying("gen_ref_saddle",{5,[0;0;1.5],[2,2,1]})); % 時変な目標状態
-    agent(i).set_property("reference",Reference_Time_Varying("Case_study_trajectory",[1;0;1])); % ハート形[x;y;z]永久
+    %agent(i).set_property("reference",Reference_Time_Varying("gen_ref_saddle",{5,[0;0;1],[2,2,0.5]})); % 時変な目標状態
+    %agent(i).set_property("reference",Reference_Time_Varying("Case_study_trajectory",[1;0;1])); % ハート形[x;y;z]永久
     %agent(i).set_property("reference",Reference_Time_Varying_Suspended_Load("Case_study_trajectory",[1;0;1])); % ハート形[x;y;z]永久
     %agent(i).set_property("reference",Reference_Wall_observation()); %
-%     agent(i).set_property("reference",Reference_Agreement(N)); % Voronoi重心
+    %agent(i).set_property("reference",Reference_Agreement(N)); % Voronoi重心
+    %agent(i).set_property("reference",struct("type","TWOD_TANBUG","name","tbug","param",[])); % ハート形[x;y;z]永久
     % 以下は常に有効にしておくこと "t" : take off, "f" : flight , "l" : landing
     agent(i).set_property("reference", Reference_Point_FH());                              % 目標状態を指定 ：上で別のreferenceを設定しているとそちらでxdが上書きされる  : sim, exp 共通
     %% set controller property
     agent(i).controller = [];
-    agent(i).set_property("controller",Controller_FT(dt)); % 有限時間整定制御
-%     agent(i).set_property("controller", Controller_HL(dt));                                % 階層型線形化
-    HLControlSetting = Controller_HL(dt);
-    HLParam = HLControlSetting.param;
+    %agent(i).set_property("controller",Controller_FT(dt)); % 有限時間整定制御
+    %agent(i).set_property("controller", Controller_HL(dt));                                % 階層型線形化
+    %agent(i).set_property("controller", Controller_FHL(dt));                                % 階層型線形化
+    agent(i).set_property("controller", Controller_FHL_Servo(dt));                                % 階層型線形化
+    %HLControlSetting = Controller_HL(dt);
+    HLParam = agent(i).controller.hlc.param;%HLControlSetting.param;
 
-    
     %agent(i).set_property("controller",Controller_HL_Suspended_Load(dt)); % 階層型線形化
     %agent(i).set_property("controller",Controller_MEC()); % 実入力へのモデル誤差補償器
-    % agent(i).set_property("controller",Controller_HL_MEC(dt);% 階層型線形化＋MEC
+    %agent(i).set_property("controller",Controller_HL_MEC(dt);% 階層型線形化＋MEC
     %agent(i).set_property("controller",Controller_HL_ATMEC(dt));%階層型線形化+AT-MEC
     %agent(i).set_property("controller",struct("type","MPC_controller","name","mpc","param",{agent(i)}));
     %agent(i).set_property("controller",struct("type","DirectController","name","direct","param",[]));% 次時刻に入力の位置に移動するモデル用：目標位置を直接入力とする
-    %agent(i).set_property("controller",struct("type","PDController","name","pd","param",struct("P",-1*diag([1,1,3]),"D",-1*diag([1,1,3]))));% 次時刻に入力の位置に移動するモデル用：目標位置を直接入力とする
+    %agent(i).set_property("controller",struct("type","PDController","name","pd","param",struct("P",-0.9178*diag([1,1,3]),"D",-1.6364*diag([1,1,3]),"Q",-1)));% 次時刻に入力の位置に移動するモデル用：目標位置を直接入力とする
     %% 必要か？実験で確認 : TODO
     param(i).sensor.list = cell(1, length(agent(i).sensor.name));
     param(i).reference.list = cell(1, length(agent(i).reference.name));
