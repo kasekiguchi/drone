89a034303888d4a18727ea56d02db63863b3a760
1912012 <104184509+1912012@users.noreply.github.com>
2022-07-18 10:00:16 +0900

    rosとの通信(試し)

---
+++ ir_ros/ros1.m
@@ -0,0 +1,19 @@
+clear all
+clc
+test_1 = ros2node("/test_1",30);
+test_2 = ros2node("/test_2",30);
+pause(3)
+subscan = ros2subscriber(test_1,"/scan")
+% subsensor = ros2subscriber(test_2,"/sensor_state")
+% global pos
+% global orient
+% pause(3)
+% disp(pos)
+% disp(orient)
+% scandata = receive(subscan,2)
+ros2("node","list","DomainID",30)
+ros2("topic","list","DomainID",30)
+Pubcmd_vel = ros2publisher(test_2,"/cmd_vel","geometry_msgs/Twist")
+cmd_msg = ros2message("geometry_msgs/Twist");
+cmd_msg.angular.z = 0.1;
+send(Pubcmd_vel,cmd_msg)
