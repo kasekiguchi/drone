da3c9f1ef58ff38d79f8fd39e0c0114e2548ad9f
1912063 <104186106+1912063@users.noreply.github.com>
2022-09-08 15:07:24 +0900

    入力ゲイン調整

--- sample/InputTransform_Thrust2Throttle_drone.m
+++ sample/InputTransform_Thrust2Throttle_drone.m
@@ -16,7 +16,7 @@ function u_trans_param = InputTransform_Thrust2Throttle_drone(varargin)
 %     u_trans_param.param.gain = [600; 600; 600; 150]; % serial : 500 = neutral  udp : 1100
 %     u_trans_param.param.th_offset = 1000;             % check throttle at hovering    
 
-    u_trans_param.param.gain = [600; 600; 600; 80]; % serial : 500 = neutral  udp : 1100
+    u_trans_param.param.gain = [600; 600; 600; 150]; % serial : 500 = neutral  udp : 1100
     u_trans_param.param.th_offset = 300;             % check throttle at hovering    
 %   u_trans_param.param.gain =[200;200;200;84]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
 %   u_trans_param.param.th_offset = 360;         % offset 3s[1021] 4s[900]　発掘[926]
