3e50eaf2c1388694ccc0cf70f10078331a3f56ad
sekiguchi <kasekiguchi@gmail.com>
2022-09-19 10:10:06 +0900

    modify getobjectivefimeval

--- ws_development/ws_controller/MPCcodegen/GetobjectiveFimEval.m
+++ ws_development/ws_controller/MPCcodegen/GetobjectiveFimEval.m
@@ -25,23 +25,23 @@ for j = 1:params.H
         ObFim = ObFim + RangeLogic(i) * FIM_Observe(X(1,j), X(2,j), X(3,j), params.dis(i), params.alpha(i), params.phi(i));
     end
     Fim = (1/(2*NoiseR))*Fim;
-    ObFim = (1/(2*NoiseR))*([ObFim + [1e-2,1e-2,1e-2;1e-2,1e-2,1e-2;1e-2,1e-2,1e-2]]);
+    ObFim = (1/(NoiseR))*(obFim + 1e-3*eye(3));%[ObFim + [1e-2,1e-2,1e-2;1e-2,1e-2,1e-2;1e-2,1e-2,1e-2]]);
     InvFim = [Fim(2,2) -Fim(1,2); -Fim(2,1), Fim(1,1)]/(det(Fim));
     InvObFim = inv(ObFim);
 %     evFim(1,j) = max(eig(InvFim));
-evFim(1,j) = trace(InvFim);
-evObFim(1,j) = trace(InvObFim);
+evFim(1,j) = trace(InvObFim)*trace(InvFim);
+%evObFim(1,j) = trace(InvObFim);
 end
 %-- 状態及び入力のステージコストを計算
 stageState = arrayfun(@(L) tildeX(:, L)' * params.Q * tildeX(:, L), 1:params.H);
 stageInput = arrayfun(@(L) tildeU(:, L)' * params.R * tildeU(:, L), 1:params.H);
 % stageSlack = arrayfun(@(L) S(:, L)' * params.WoS * S(:,L), 1:params.H);
-eval.stageevFim = evFim* params.T * evFim';
+eval.stageevFim = evFim* params.T *evFim';
 %-- 状態の終端コストを計算
 eval.terminalState = tildeX(:, end)' * params.Qf * tildeX(:, end);
 %-- 評価値計算
 eval.StageState = sum(stageState);
 eval.StageInput = sum(stageInput);
 % eval.StageSlack = sum(stageSlack);
-eval.stageeObFim = evObFim* params.T * evObFim';
+eval.stageeObFim = 1;%evObFim* params.T * evObFim';
 end
