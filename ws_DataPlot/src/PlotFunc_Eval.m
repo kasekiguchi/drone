66105d4b819897a55501fceef7e6dbc72a263af4
kasekiguchi <kasekiguchi@gmail.com>
2022-09-20 17:14:49 +0900

    Update PlotFunc_Eval.m

--- ws_DataPlot/src/PlotFunc_Eval.m
+++ ws_DataPlot/src/PlotFunc_Eval.m
@@ -43,7 +43,7 @@ if vFlag
     ax.FontSize = obj.FontSize;
     ax.FontName = obj.FontName;
     ax.FontWeight = obj.FontWeight;
-    pbaspect([3 1 1]);
+    pbaspect([2 1 1]);
     %ylim([0,100]);
     hold off
     exportgraphics(ax,strcat(prename,'Eval','.pdf'));
