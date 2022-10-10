3717c619c284b7659d2afa97731eb282ab60211d
1912012 <g1912012@gmail.com>
2022-10-10 11:19:43 +0900

    up

--- main.m
+++ main.m
@@ -12,7 +12,7 @@ userpath('clear');
 %% general setting
 N = 1; % number of agents
 fExp = 0 % 1：実機　それ以外：シミュレーション
-fMotive = 1 % Motiveを使うかどうか
+fMotive = 0 % Motiveを使うかどうか
 fOffline = 0; % offline verification with experiment data
 fDebug = 0;
 
