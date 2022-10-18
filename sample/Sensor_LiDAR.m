100bcd3e240d46a7cda61b7a449efec2f1215135
AnzaiYuta <104184284+AnzaiYuta@users.noreply.github.com>
2022-10-18 12:45:01 +0900

    Update Sensor_LiDAR.m

--- sample/Sensor_LiDAR.m
+++ sample/Sensor_LiDAR.m
@@ -9,7 +9,7 @@ end
 %% sensor class demo : constructor
 % sensor property 2D LiDAR sensor
 Sensor.name = ["lrf"];
-Sensor.type = ["L2node('/test1')iDAR_SIM"];
+Sensor.type = ["LiDAR_SIM"];
 Sensor.param.radius = param.radius;
 Sensor.param.angle_range = param.angle_range;
 Sensor.param.seed = param.seed;
