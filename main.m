5ed9f7433e805983d0025fae96f84b5eb481f4bf
g1912077 <104176400+g1912077@users.noreply.github.com>
2022-12-09 12:56:46 +0900

    Revert "Ros2_rpm"
    This reverts commit 0ddf735276d687b81a19dbf04dc7addb1bc65f2d.

--- main.m
+++ main.m
@@ -11,7 +11,7 @@ userpath('clear');
 
 %% general setting
 N = 1; % number of agents
-fExp = 0 % 1：実機　それ以外：シミュレーション
+fExp = 1 % 1：実機　それ以外：シミュレーション
 fMotive = 1 % Motiveを使うかどうか
 fOffline = 0; % offline verification with experiment data
 
@@ -76,7 +76,6 @@ try
             param(i).sensor.direct = {};
             param(i).sensor.rdensity = {Env};
             param(i).sensor.lrf = Env;
-            param(i).sensor.tokyu = {};
             for j = 1:length(agent(i).sensor.name)
                 param(i).sensor.list{j} = param(i).sensor.(agent(i).sensor.name(j));
             end
