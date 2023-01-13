8991c72bb323347a95a2a18306d6802cb5aa6b26
1912063 <104186106+1912063@users.noreply.github.com>
2023-01-13 14:40:10 +0900

    Update Connector_Natnet.m

--- sample/Connector_Natnet.m
+++ sample/Connector_Natnet.m
@@ -17,7 +17,7 @@ function motive = Connector_Natnet(natnet_param)
 arguments
   natnet_param.ClientIP char = '192.168.1.9' % 9 : 関口デスクトップ
   natnet_param.HostIP char = '192.168.1.6'
-end
 
+end
  motive=NATNET_CONNECTOR("HostIP",natnet_param.HostIP,"ClientIP",natnet_param.ClientIP);
 end
