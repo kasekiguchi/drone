b36c11791b3fbb91ec2479c803856dcb3502b870
Yuki-ISOBE-0707 <isobe_yuuki_0707@gmail.com>
2023-01-26 17:49:58 +0900

    複数回mainを実行するrun_minManyTime.mを作成

---
+++ run_mainManyTime.m
@@ -0,0 +1,41 @@
+%% run_mainManyTime.m
+%  指定した回数だけ共通プログラムのmainをブン回す
+% !!! 実行する前に共通プログラムmain.m 内の Initialize setting に含まれている clear all をコメントアウトすること !!!
+
+clear;clc;
+
+%% 初期設定
+% 実行する回数を指定
+HowManyRun = 10;
+
+% rand シード値
+seed = double('T');
+% seed = double('C');
+% seed = double('U');
+% seed = double('G');
+rng(seed);
+
+% データ保存先フォルダ名
+Foldername = 'Data\simData_KoopmanApproach';
+% データ保存先ファイル名
+% "ファイル名"_[番号].m で保存される
+FileName = 'simtest';
+
+%データ保存用,現在のファイルパスを取得,保存先を指定
+activeFile = matlab.desktop.editor.getActive;
+nowFolder = fileparts(activeFile.Filename);
+targetpath = append(nowFolder,'\',Foldername,'\',FileName);
+
+%必要な構造体を一旦定義
+agent = struct; logger = struct;
+
+%% mainを複数回実行
+for runCount = 1:HowManyRun
+   clearvars -except HowManyRun targetpath FileName Foldername seed runCount nowFolder% 変数 A 以外の変数を削除  
+
+    run('main.m')
+
+    targetFileNumber = append(targetpath,'_',num2str(runCount),'.m');
+    save(targetFileNumber,'logger');    % 実行結果を記録
+end
+
