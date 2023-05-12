210f47da8e55f415f76d4f56970f64a9c6c4fee3
1912063 <104186106+1912063@users.noreply.github.com>
2023-05-12 18:43:41 +0900

    exp t265

--- sample/InputTransform_Thrust2Throttle_drone.m
+++ sample/InputTransform_Thrust2Throttle_drone.m
@@ -11,8 +11,8 @@ function u_trans_param = InputTransform_Thrust2Throttle_drone(varargin)
 %     u_trans_param.param.th_offset = 960;         % offset 3s[1021] 4s[900]　発掘[926]
 %     u_trans_param.param.gain_SuspendedLoad =[500;500;500;150]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
 %     u_trans_param.param.th_offset_SuspendedLoad = 1150;         % offset 3s[1021] 4s[900]　発掘[926]
-    u_trans_param.param.gain =[600;600;600;150]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
-    u_trans_param.param.th_offset = 360;         % offset 3s[1021] 4s[900]　発掘[926]
+    u_trans_param.param.gain =[800;800;800;150]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
+    u_trans_param.param.th_offset = 460;         % offset 3s[1021] 4s[900]　発掘[926]
     u_trans_param.param.gain_SuspendedLoad =[500;500;500;100]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
     u_trans_param.param.th_offset_SuspendedLoad = 1110;         % offset 3s[1021] 4s[900]　発掘[926]
 
