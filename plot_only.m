551e59275fe862571ea5c474f885cafa8f931891
2012100 <g2012100tcu@gmail.com>
2023-05-18 18:35:03 +0900

    energy

--- plot_only.m
+++ plot_only.m
@@ -2,14 +2,16 @@
 figure(6)
 hold on
 for plot_i = 1:logger.k%変数を格納
-    if obs_pos(2).p(2)>logger.Data.agent.estimator.result{1, plot_i}.state.p(2)&&logger.Data.agent.estimator.result{1, plot_i}.state.p(2)<obs_pos(3).p(2)
-        distance_wall(plot_i,1) = norm(obs_x-logger.Data.agent.estimator.result{1, plot_i}.state.p(1));%壁との垂直の距離
-    else
-        distance_a(1) = norm(obs_pos(2).p(1:2)-logger.Data.agent.estimator.result{1, plot_i}.state.p(1:2));%端点aとの距離
-        distance_a(2) = norm(obs_pos(3).p(1:2)-logger.Data.agent.estimator.result{1, plot_i}.state.p(1:2));%端点bとの距離
-        distance_wall(plot_i,1) = min(distance_a);%a or b　近いほうを選出
-    end
-    distance_sensor(plot_i,1) = logger.Data.agent.sensor.result{1, plot_i}.distance.teraranger;%テラレンジャーの距離
+    sensor_ofset_z = logger.Data.agent.sensor.result{1,plot_i}.state.p(3) - logger.Data.agent.sensor.result{1,plot_i}.posion(3);
+    logger.Data.agent.sensor.result{1,plot_i}.posion(3) = sensor_ofset_z;
+%     if obs_pos(2).p(2)>logger.Data.agent.estimator.result{1, plot_i}.state.p(2)&&logger.Data.agent.estimator.result{1, plot_i}.state.p(2)<obs_pos(3).p(2)
+%         distance_wall(plot_i,1) = norm(obs_x-logger.Data.agent.estimator.result{1, plot_i}.state.p(1));%壁との垂直の距離
+%     else
+%         distance_a(1) = norm(obs_pos(2).p(1:2)-logger.Data.agent.estimator.result{1, plot_i}.state.p(1:2));%端点aとの距離
+%         distance_a(2) = norm(obs_pos(3).p(1:2)-logger.Data.agent.estimator.result{1, plot_i}.state.p(1:2));%端点bとの距離
+%         distance_wall(plot_i,1) = min(distance_a);%a or b　近いほうを選出
+%     end
+%     distance_sensor(plot_i,1) = logger.Data.agent.sensor.result{1, plot_i}.distance.teraranger;%テラレンジャーの距離
 end
 
 plot(T,distance_wall,'LineWidth',1)
