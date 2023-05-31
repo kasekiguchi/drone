921f607b535438e4c2e7f0499dfe448c9d7fae48
sekiguchi <kasekiguchi@gmail.com>
2023-06-01 06:19:13 +0900

    at Home

--- mainGUI.m
+++ mainGUI.m
@@ -1,3 +1,12 @@
+%% Initialize settings
+% set path
+tmp = matlab.desktop.editor.getActive;
+cd(fileparts(tmp.Filename));
+[~, tmp] = regexp(genpath('.'), '\.\\\.git.*?;', 'match', 'split');
+cellfun(@(xx) addpath(xx), tmp, 'UniformOutput', false);
+close all hidden; clear all; clc;
+userpath('clear');
+%%
 SimBaseMode = ["","SimHL","SimVoronoi2D"];
 ExpBaseMode = ["","ExpHL","ExpVoronoi2D"];
 gui = SimExp;
