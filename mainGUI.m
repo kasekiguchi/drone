b86dd68b818966658ff709222dac78cbaba7d57b
kasekiguchi <kasekiguchi@gmail.com>
2023-06-02 19:00:45 +0900

    at LAB

--- mainGUI.m
+++ mainGUI.m
@@ -8,14 +8,6 @@ close all hidden; clear all; clc;
 userpath('clear');
 %%
 clc
-SimBaseMode = ["","SimHL","SimVoronoi2D"];
+SimBaseMode = ["SimHL","SimVoronoi2D"];
 ExpBaseMode = ["","ExpHL","ExpVoronoi2D"];
-gui = SimExp;
-%%
-gui.PlantLabel.Text = ["Plant ",""];
-gui.Lamp.Color = [1 0 0];
-gui.LampLabel.Text = "Arming";
-gui.RunmodeListBox.Value = "Simulation";
-
-%%
-hoge = readcell("test.csv");
+gui = SimExp(0);
