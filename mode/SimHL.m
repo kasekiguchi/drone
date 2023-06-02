f5dc59703c1954f3f3d7ca134fb48a3748ef1b2b
kasekiguchi <kasekiguchi@gmail.com>
2023-06-02 12:24:02 +0900

    at Lab1

--- mode/SimHL.m
+++ mode/SimHL.m
@@ -26,6 +26,6 @@ agent.set_model(Model_EulerAngle(dt, initial_state, 1), DRONE_PARAM("DIATONE", "
 agent.set_property("sensor", Sensor_Motive(1,0, motive)); % motive情報 : sim exp 共通 % 引数はmotive上の剛体番号ではない点に注意
 agent.set_property("estimator", Estimator_EKF(agent, ["p", "q"]));                                                                    % （剛体ベース）EKF
 agent.set_property("reference",Reference_Time_Varying("gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[2,2,0.5]})); % 時変な目標状態
-agent.set_property("reference", Reference_Point_FH());                                                                                   % 目標状態を指定 ：上で別のreferenceを設定しているとそちらでxdが上書きされる  : sim, exp 共通
+%agent.set_property("reference", Reference_Point_FH());                                                                                   % 目標状態を指定 ：上で別のreferenceを設定しているとそちらでxdが上書きされる  : sim, exp 共通
 agent.set_property("controller", Controller_HL(dt));                                                                                     % 階層型線形化
 
