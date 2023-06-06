f6d0456f1df383ca99b84b928bb72c462d6024a3
2012100 <g2012100tcu@gmail.com>
2023-06-06 14:03:08 +0900

    plot

--- plot_only.m
+++ plot_only.m
@@ -7,6 +7,7 @@ x_wide = 0.1;%壁の厚さ
 
 figure(6)
 hold on
+
 for plot_i = 1:logger.k%変数を格納
     distance_sensor = logger.Data.agent.sensor.result{1,plot_i}.state.p(3) - logger.Data.agent.sensor.result{1,plot_i}.posion(3);
     sensor_T265(plot_i,1) = logger.Data.agent.sensor.result{1,plot_i}.posion(1);
@@ -26,6 +27,24 @@ end
 
 plot(T,sensor_T265,'LineWidth',1)
 plot(T,prime_sensor,'LineWidth',1)
+
+txt = {''};
+
+if length([find(logger.Data.phase == 116, 1), find(logger.Data.phase == 116, 1, 'last')]) == 2
+    Square_coloring(logger.Data.t([find(logger.Data.phase == 97, 1), find(logger.Data.phase == 116, 1, 'last')])); % take off phase
+    %                        txt = {txt{:},'{\color{yellow}■} :Take off phase'};
+    txt = {txt{:}, '{\color[rgb]{1.0,1.0,0.9}■} :Take off phase'};
+end
+
+if length([find(logger.Data.phase == 102, 1), find(logger.Data.phase == 102, 1, 'last')]) == 2
+    Square_coloring(logger.Data.t([find(logger.Data.phase == 116, 1), find(logger.Data.phase == 102, 1, 'last')]), [0.9 1.0 1.0]); % flight phase
+    txt = {txt{:}, '{\color[rgb]{0.9,1.0,1.0}■} :Flight phase'};
+end
+
+if length([find(logger.Data.phase == 108, 1), find(logger.Data.phase == 108, 1, 'last')]) == 2
+    Square_coloring(logger.Data.t([find(logger.Data.phase == 102, 1), find(logger.Data.phase == 108, 1, 'last')]), [1.0 0.9 1.0]); % landing phase
+    txt = {txt{:}, '{\color[rgb]{1.0,0.9,1.0}■} :Landing phase'};
+end
 
 xlabel('Time [s]','FontSize',16);
 ylabel('Position [m]','FontSize',16);
