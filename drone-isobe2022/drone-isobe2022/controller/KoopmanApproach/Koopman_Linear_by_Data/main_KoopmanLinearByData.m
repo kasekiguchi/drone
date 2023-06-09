0425aa43db7645bfd58368233c5e0e9d3474ccec
g2012035 <g2012035@tcu.ac.jp>
2023-06-09 12:21:37 +0900

    6月9日
    6月8日の実験データを使ったクープマン線形化

--- drone-isobe2022/drone-isobe2022/controller/KoopmanApproach/Koopman_Linear_by_Data/main_KoopmanLinearByData.m
+++ drone-isobe2022/drone-isobe2022/controller/KoopmanApproach/Koopman_Linear_by_Data/main_KoopmanLinearByData.m
@@ -8,14 +8,16 @@ close all
 flg.bilinear = 0; %1:双線形モデルへの切り替え
 
 %データ保存先ファイル名(逐次変更する)
-delete controller\KoopmanApproach\Koopman_Linear_by_Data\EstimationResult_12state_6_6_normal_experiment.mat; %同じファイル名を使うときはコメントイン
-FileName = 'EstimationResult_12state_6_6_normal_experiment.mat';
+% delete controller\KoopmanApproach\Koopman_Linear_by_Data\EstimationResult_12state_6_9_normal_experiment_vertical.mat; %同じファイル名を使うときはコメントイン
+FileName = 'EstimationResult_12state_6_9_Input_experiment_alldata.mat';
 
 % 読み込むデータファイル名(run_mainManyTime.mのファイル名と一致させる)
 % loading_filename = 'sim_rndP_12state';
 % loading_filename = 'sim_rndP4';
 % loading_filename = '2023_0529_1804_logger_cricle_LS.mat';
-loading_filename = '6_08_experiment';
+% loading_filename = 'experiment_circle_6_8';
+loading_filename = 'experiment_vertical_6_8';
+% loading_filename = '6_8_experiment';
 
 %データ保存用,現在のファイルパスを取得,保存先を指定
 activeFile = matlab.desktop.editor.getActive;
@@ -26,11 +28,11 @@ targetpath=append(nowFolder,'\',FileName);
 % クープマン作用素を定義
 % F@(X) Xを与える関数ハンドルとして定義
 % DroneSimulation
-F = @(x) [x;1]; % 状態そのまま
+% F = @(x) [x;1]; % 状態そのまま
 % F = @quaternionParameter; % クォータニオンを含む13状態の観測量
 % F = @eulerAngleParameter; % 姿勢角をオイラー角モデルの状態方程式からdq/dt部分を抜き出した観測量
 % F = @eulerAngleParameter_withinConst; % eulerAngleParameter+慣性行列を含む部分(dvdt)を含む観測量
-% F = @eulerAngleParameter_InputAndConst; % eulerAngleParameter_withinConst+入力にかかる係数行列の項を含む観測量(動作確認済み)
+F = @eulerAngleParameter_InputAndConst; % eulerAngleParameter_withinConst+入力にかかる係数行列の項を含む観測量(動作確認済み)アップデート中
 % F = @quaternions; % 状態+クォータニオンの1乗2乗3乗 オイラー角パラメータ用(動作確認済み)
 % F = @quaternions_13state; % 状態+クォータニオンの1乗2乗3乗 クォータニオンパラメータ用
 % F = @eulerAngleParameter_withoutP;
@@ -44,7 +46,7 @@ F = @(x) [x;1]; % 状態そのまま
 % 使用するデータセットの数を指定
 % 23/01/26 run_mainManyTime.m で得たデータを合成
 disp('now loading data set')
-Data.HowmanyDataset = 4; %読み込むデータ数に応じて変更
+Data.HowmanyDataset = 2; %読み込むデータ数に応じて変更
 
 for i= 1: Data.HowmanyDataset
     if contains(loading_filename,'.mat')
--- drone-isobe2022/drone-isobe2022/run_mainManyTime.m
+++ drone-isobe2022/drone-isobe2022/run_mainManyTime.m
@@ -17,8 +17,8 @@ rng(seed);
 
 % データ保存先フォルダ名
 % rmdir Data\simData_KoopmanApproach_2023_6_1_data200 s;%フォルダの削除 フォルダ名を変更せずに回す場合はコメントイン 
-mkdir Data\simData_KoopmanApproach_2023_6_6_experiment; %新規フォルダの作成(以下のフォルダ名と一致させるように)
-Foldername = 'Data\simData_KoopmanApproach_2023_6_6_experiment'; %ここの名前を変えないとフォルダがどんどん上書きされてしまう
+mkdir Data\simData_KoopmanApproach_2023_6_9_experiment_alldata; %新規フォルダの作成(以下のフォルダ名と一致させるように)
+Foldername = 'Data\simData_KoopmanApproach_2023_6_6_experiment_circle'; %ここの名前を変えないとフォルダがどんどん上書きされてしまう
 % データ保存先ファイル
 % "ファイル名"_[番号].mat で保存される
 FileName = '6_1_isobe'; %線形化で読み込むファイルはこれで判別してる
