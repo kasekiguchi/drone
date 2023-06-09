082bf617a29fafb09af3eca6a4a148ff05468a3f
YoshikiK-tcu <gg1912037@gmail.com>
2023-06-09 17:47:47 +0900

    add kensyo

--- practice1.m
+++ practice1.m
@@ -1,6 +1,6 @@
 clear
 clc
-
+%%
 %dos1,2,3,4のqiがqihatに変更
 %ro追加 omegahat^2(oh02)を変更
 syms v0 [3 1] real;
@@ -8,6 +8,7 @@ syms ddx0 [3 1] real ; syms dol0 [3 1] real;
 syms A [3,3] real; syms B [3,3] real; syms C [3,3] real
 syms P1 [3,1] real; syms P2 [3,1] real
 
+syms m0 real; syms m1 real; syms m2 real; syms m3 real; syms m4 real;
 syms q1 [3,1] real; syms q2 [3,1] real; syms q3 [3,1] real; syms q4 [3,1] real %リンクの方向ベクトル
 syms ol0 [3,1] real; %ペイロードの角速度
 syms ol1 [3,1] real;syms ol2 [3,1] real;syms ol3 [3,1] real;syms ol4 [3,1] real; %機体の角速度
@@ -18,6 +19,8 @@ syms R0 [3,3] real; %ペイロードを軸にした回転行列
 syms R1 [3,3] real; syms R2 [3,3] real; syms R3 [3,3] real; syms R4 [3,3] real; %機体回転行列
 syms f1 real; syms f2 real; syms f3 real; syms f4 real; %機体推力
 syms qt0 [4,1] real; syms qt1 [4,1] real; syms qt2 [4,1] real; syms qt3 [4,1] real; syms qt4 [4,1] real; %クォータニオン(姿勢)
+
+e3 = [0,0,1]';
 
 [Rb0,L0] = RodriguesQuaternion(qt0);   % Rotation matrix
 [Rb1,L1] = RodriguesQuaternion(qt1);   % Rotation matrix
@@ -31,22 +34,24 @@ dqt2 = L2'*ol2/2;
 dqt3 = L3'*ol3/2;
 dqt4 = L4'*ol4/2; %ドローンの姿勢
 
+
 % ro1 = [0.05; 0.05; 0.05];
 % ro2 = [-0.05; 0.05; 0.05];
 % ro3 = [-0.05; -0.05; 0.05];
 % ro4 = [0.05; -0.05; 0.05]; %ペイロードの重心とリングが接地している点までの長さ
 
-ro1 = [1; 1; 1];
-ro2 = [-1; 1; 1];
-ro3 = [-1; -1; 1];
-ro4 = [1; -1; 1]; %ペイロードの重心とリングが接地している点までの長さ
+
+ro1 = [1; 1;-1];
+ro2 = [1;-1;-1];
+ro3 = [-1;-1;-1];
+ro4 = [-1;1;-1]; %ペイロードの重心とリングが接地している点までの長さ
 
 
-m0 = 1;
-m1 = 2;
-m2 = 2;
-m3 = 2;
-m4 = 2;
+% m0 = 100;
+% m1 = 2;
+% m2 = 2;
+% m3 = 2;
+% m4 = 2;
 
 ge3 = [0;0;9.81]; 
 
@@ -54,7 +59,7 @@ ge3 = [0;0;9.81];
 jx = 0.005;
 jy = 0.005;
 jz = 0.005;
-J0=diag([0.01, 0.01, 0.01]);
+J0=diag([0.1, 0.1, 0.1]);
 J1=diag([jx,jy,jz]);
 J2=diag([jx,jy,jz]);
 J3=diag([jx,jy,jz]);
@@ -67,6 +72,7 @@ l3 = 0.5;
 l4 = 0.5; %リンクの長さ
 
 syms u1 [3,1] real; syms u2 [3,1] real; syms u3 [3,1] real; syms u4 [3,1] real; %入力
+u1 = -f1 * Rb1 * e3; u2 = -f2 * Rb2 * e3; u3 = -f3 * Rb3 * e3; u4 = -f4 * Rb4 * e3;
 u1p = (q1*q1')*u1; u2p = (q2*q2')*u2; u3p = (q3*q3')*u3; u4p = (q4*q4')*u4;
 u1v = (eye(3)-(q1*q1'))*u1; u2v = (eye(3)-(q2*q2'))*u2; u3v = (eye(3)-(q3*q3'))*u3; u4v = (eye(3)-(q4*q4'))*u4;
 
@@ -137,18 +143,6 @@ os4hat = [0, -os4(3,1), os4(2,1);
 
 mq = m0*eye(3) + m1*(q1*q1') + m2*(q2*q2') + m3*(q3*q3') + m4*(q4*q4');
 
-
-%%
-% enq1 = ddx0 == A*dol0 + P1;
-% enq2 = B*dol0 == -C*ddx0 + P2;
-
-% ddx0 = A*dol0 + P1;
-%%
-A0=[1,1,1;1,1,1;1,1,1];
-B0=[1,1,1;1,1,1;1,1,1];
-C0=[1,1,1;1,1,1;1,1,1];
-P10=[1;2;2];
-P20=[1;2;1];
 
 %%
 ddx0 = simplifyFraction((A*C+B)\(B*P1 + A*P2));
@@ -251,31 +245,37 @@ dR4 = R4*ol4hat;
 %%
 % subs(ddx0_new,dol0,[0,0,0])
 % subs(ddx0_new,data,dataset)
-data = [q1;q2;q3;q4;ol0;ol1;ol2;ol3;ol4;os1;os2;os3;os4;u1;u2;u3;u4;qt0];
+P = [m0,m1,m2,m3,m4]';
+data = [q1;q2;q3;q4;ol0;ol1;ol2;ol3;ol4;os1;os2;os3;os4;f1;f2;f3;f4;M1;M2;M3;M4;qt0;qt1;qt2;qt3;qt4;P];
 % q_dataset=[[1/(3)^(1/2);1/(3)^(1/2);1/(3)^(1/2)];[1/(3)^(1/2);1/(3)^(1/2);1/(3)^(1/2)];[1/(3)^(1/2);1/(3)^(1/2);1/(3)^(1/2)];[1/(3)^(1/2);1/(3)^(1/2);1/(3)^(1/2)]];
-q_dataset=[[0;1/sqrt(2);1/sqrt(2)];[0;1/sqrt(2);1/sqrt(2)];[0;1/sqrt(2);1/sqrt(2)];[0;1/sqrt(2);1/sqrt(2)]];
-% q_dataset=[[0;0;1];[0;0;1];[0;0;1];[0;0;1]];
+% q_dataset=[[0;1/sqrt(2);1/sqrt(2)];[0;1/sqrt(2);1/sqrt(2)];[0;1/sqrt(2);1/sqrt(2)];[0;1/sqrt(2);1/sqrt(2
+q_dataset1=[0;-1;1];
+q_dataset2=[0;-1;1];
+q_dataset3=[0;-1;1];
+q_dataset4=[0;-1;1];
+q_dataset=[q_dataset1/vecnorm(q_dataset1);q_dataset2/vecnorm(q_dataset2);q_dataset3/vecnorm(q_dataset3);q_dataset4/vecnorm(q_dataset4)];
 
 ol0_dataset=[0;0;0];
 ol_dataset=[[0;0;0];[0;0;0];[0;0;0];[0;0;0]];
 os_dataset=[[0;0;0];[0;0;0];[0;0;0];[0;0;0]];
-u_dataset = [[0;0;-1];[0;0;-1];[0;0;-5];[0;0;-1]];
-
-% P1dataset=subs(P1_new,data,dataset);
-% P2dataset=subs(P2_new,data,dataset);
-
-% data2 = [q1;q2;q3;q4;ol0;ol1;ol2;ol3;ol4;os1;os2;os3;os4;u1;u2;u3;u4;qt0;P1;P2];
-
-alfa=pi/180*90;
-beta=pi/180*0;
-gunma=pi/180*60;
-R0_matrix = [cos(alfa)*cos(beta)*cos(gunma)-sin(alfa)*sin(gunma),-cos(alfa)*cos(beta)*sin(gunma)-sin(alfa)*cos(gunma),cos(alfa)*sin(beta);
-            sin(alfa)*cos(beta)*cos(gunma)-cos(alfa)*sin(gunma),-sin(alfa)*cos(beta)*sin(gunma)-cos(alfa)*sin(gunma),sin(alfa)*sin(gunma);
-            -sin(beta)*cos(gunma),sin(beta)*sin(gunma),cos(beta)];
+u_dataset = [[0;0;1];[0;0;1];[0;0;1];[0;0;1]];
+f_dataset=[1;1;1;1];
+M_dataset = [[0;0;0];[0;0;0];[0;0;0];[0;0;0]];
 
 qt0_dataset=[1;0;0;0];
+qt1_dataset=[1;0;0;0];
+qt2_dataset=[1;0;0;0];
+qt3_dataset=[1;0;0;0];
+qt4_dataset=[1;0;0;0];
 
-dataset = [q_dataset;ol0_dataset;ol_dataset;os_dataset;u_dataset;qt0_dataset];
+m0_dataset = 1;
+m1_dataset = 2;
+m2_dataset = 2;
+m3_dataset = 2;
+m4_dataset = 2;
+P_dataset = [m0_dataset,m1_dataset,m2_dataset,m3_dataset,m4_dataset]';
+
+dataset = [q_dataset;ol0_dataset;ol_dataset;os_dataset;f_dataset;M_dataset;qt0_dataset;qt1_dataset;qt2_dataset;qt3_dataset;qt4_dataset;P_dataset];
 % dataset2 = [q_dataset;ol0_dataset;ol_dataset;os_dataset;u_dataset;qt0_dataset;P1dataset;P2dataset];
 
 % ddx0_new2 = subs(ddx0_new,[P1,P2],[P1_new,P2_new]);
@@ -287,6 +287,7 @@ dataset = [q_dataset;ol0_dataset;ol_dataset;os_dataset;u_dataset;qt0_dataset];
 % subs(dos2_new,data,dataset);
 % subs(dos3_new,data,dataset);
 % subs(dos4_new,data,dataset)]
+v0= [0;0;0];
 
 AA2 = subs(AA,data,dataset);
 BB2 = subs(BB,data,dataset);
@@ -301,9 +302,36 @@ dos2_out = subs((q2hat*(ddx0_out-ge3-Rb0*ro2hat*dol0_out+Rb0*ol0hat^2*ro2)/l2 - 
 dos3_out = subs((q3hat*(ddx0_out-ge3-Rb0*ro3hat*dol0_out+Rb0*ol0hat^2*ro3)/l3 - q3hat*u3p/(m3*l3)),data,dataset);
 dos4_out = subs((q4hat*(ddx0_out-ge3-Rb0*ro4hat*dol0_out+Rb0*ol0hat^2*ro4)/l4 - q3hat*u4p/(m4*l4)),data,dataset);
 
-ddx0_out2 = ddx0_out'
-dol0_out2 = dol0_out'
-dos1_out2 = dos1_out'
-dos2_out2 = dos2_out'
-dos3_out2 = dos3_out'
-dos4_out2 = dos4_out'
+dq1_out = subs(os1hat*q1,data,dataset);
+dq2_out = subs(os2hat*q2,data,dataset);
+dq3_out = subs(os3hat*q3,data,dataset);
+dq4_out = subs(os4hat*q4,data,dataset);
+
+
+% dR0_out = subs(R0*ol0hat,data,dataset);
+% dR1_out = subs(R1*ol1hat,data,dataset);
+% dR2_out = subs(R2*ol2hat,data,dataset);
+% dR3_out = subs(R3*ol3hat,data,dataset);
+% dR4_out = subs(R4*ol4hat,data,dataset);
+
+dqt0_out = subs(L0'*ol0/2,data,dataset); %ペイロードの姿勢
+dqt1_out = subs(L1'*ol1/2,data,dataset); %ドローンの姿勢
+dqt2_out = subs(L2'*ol2/2,data,dataset);
+dqt3_out = subs(L3'*ol3/2,data,dataset);
+dqt4_out = subs(L4'*ol4/2,data,dataset);
+
+dol1 = subs(J1\(M1-cross(ol1,J1*ol1)),data,dataset);
+dol2 = subs(J2\(M2-cross(ol2,J2*ol2)),data,dataset);
+dol3 = subs(J3\(M3-cross(ol3,J3*ol3)),data,dataset);
+dol4 = subs(J4\(M4-cross(ol4,J4*ol4)),data,dataset);
+
+out = [v0;dqt0_out;ddx0_out;dol0_out;dq1_out;dq2_out;dq3_out;dq4_out;dos1_out;dos2_out;dos3_out;dos4_out;dqt1_out;dqt2_out;dqt3_out;dqt4_out;dol1;dol2;dol3;dol4]
+out_2 = reshape(double(out),[5,13])'
+%[ 3*1;dr0 4*1;ddx0 3*1;do0 3*1;dqi 3*N;dwi 3*N;dri 4*N;doi 3*N]
+
+% ddx0_out2 = ddx0_out'
+% dol0_out2 = dol0_out'
+% dos1_out2 = dos1_out'
+% dos2_out2 = dos2_out'
+% dos3_out2 = dos3_out'
+% dos4_out2 = dos4_out'
