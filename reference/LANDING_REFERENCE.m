f715099677cd154682f64a60bc0ca0b2ebf03aa7
1912063 <104186106+1912063@users.noreply.github.com>
2023-06-15 18:34:48 +0900

    at LAB experiment success

--- reference/LANDING_REFERENCE.m
+++ reference/LANDING_REFERENCE.m
@@ -7,6 +7,7 @@ classdef LANDING_REFERENCE < handle
     result
     base_state
     base_time
+    te = 3
   end
 
   methods
@@ -19,7 +20,7 @@ classdef LANDING_REFERENCE < handle
     end
     function  result= do(obj,varargin)
       % [Input] time,cha,logger,env
-      if isempty( obj.result.state.xd) % first take
+      if isempty(obj.result.state.xd) % first take
         obj.base_time=varargin{1}.t;
         obj.base_state = [obj.self.estimator.result.state.p(1:2);obj.result.state.p(3)]; % x,y : current position, z : reference using at flight phase
         obj.result.state.xd = [obj.base_state;zeros(17,1)];
@@ -44,9 +45,9 @@ classdef LANDING_REFERENCE < handle
       %% Variable set
       Xd  = zeros( 20, 1);
       %% Set Xd
-      if t<=te
+      if t<=obj.te
         Zd = curve_interpolation_9order(t,obj.te,obj.base_state(3),0,0,0);
-      elseif t> te
+      elseif t> obj.te
         Zd = zeros(1,5);
       end
       Xd(1:3,1) = obj.base_state(1:3);
