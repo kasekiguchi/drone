f0b50882b57b42e0a17af7cee8f3838ca7886087
g1812100 <85143997+g1812100@users.noreply.github.com>
2023-06-21 12:20:46 +0900

    add controller

--- controller/GEOMETRIC_CONTROLLER.m
+++ controller/GEOMETRIC_CONTROLLER.m
@@ -15,12 +15,23 @@ classdef GEOMETRIC_CONTROLLER < handle
     end
 
     function result = do(obj,varargin)
+      rho1 = [rho1_1;rho1_2;rho1_3];
+      rho2 = [rho2_1;rho2_2;rho2_3];
+      rho3 = [rho3_1;rho3_2;rho3_3];
+      rho4 = [rho4_1;rho4_2;rho4_3];
+      rho_hat = [Skew(rho1),Skew(rho2),Skew(rho3),Skew(rho4)];
+      P_mat = [eye(3),eye(3),eye(3),eye(3);rho_hat]; 
+
+      e3 = [0;0;1]
+      J0 = diag([obj.param.P(3),obj.param.P(4),obj.param.P(5)]);
       model = obj.self.estimator.result;
       ref = obj.self.reference.result;
       pd = ref.state.p;
       Qd = ref.state.Q;
       vd = ref.state.v;
       Od = ref.state.O;
+      ddxd = ref.state.ddx;
+      dOd = ref.state.dO; %ペイロード角加速度
       P = obj.param.P;
       xd=0;
 
@@ -28,7 +39,7 @@ classdef GEOMETRIC_CONTROLLER < handle
       Rbd = RodriguesQuaternion(Qd);
       kx0 = obj.param.F1; %Controller_Cooperative_Load(dt)から係数を持ってくる
       kdx0 = obj.param.F2;
-      kR02 = obj.param.F3;
+      kR0 = obj.param.F3;
       kO0 = obj.param.F4; 
 %       xd=[xd;zeros(20-size(xd,1),1)];% 足りない分は０で埋める．
 
@@ -36,11 +47,19 @@ classdef GEOMETRIC_CONTROLLER < handle
       % TODO : 本質的にはx-xdを受け付ける関数にして，x-xdの状態で2pi問題を解決すれば良い．
       ex0 = model.p - pd;
       edx0 = model.v - vd;
-      eQ0 = 1/2*vee(Rbd'*Rb0 - Rb0'*Rbd);
+      eQ0 = 1/2*Vee(Rbd'*Rb0 - Rb0'*Rbd);
 %       ev0 = model.v - vd;
       eO0 = model.O - Rb0'*Rbd*Od;
       
-      Fd = param.P(2)*(-kx0*ex0 - kdx0*edx0 + )
+      Fd = param.P(2)*(-kx0*ex0 - kdx0*edx0 + ddxd - param.(1)*e3);
+      R0Fd = Rb0'*Fd;
+      Md = -kR0*eR0 - kO0*eO0 + Skew(Rb0'*Rbd*Od)*J0*Rb0'*Rbd*Od + J0*Rb0'*Rbd*dOd;
+      con_mat = [R0Fd;Md];
+
+      img_mu = diag(Rb0,Rb0,Rb0,Rb0)*P_mat'\(P_mat*P_mat')*con_mat;
+      
+
+
 
 
 %       Rb0 = RodriguesQuaternion(Eul2Quat([0;0;xd(4)]));
