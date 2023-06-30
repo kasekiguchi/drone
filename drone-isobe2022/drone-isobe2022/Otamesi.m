7d165589aa0f70c7f5490855c7237585e5a3de80
g2012035 <g2012035@tcu.ac.jp>
2023-06-30 11:42:34 +0900

    change

--- drone-isobe2022/drone-isobe2022/Otamesi.m
+++ drone-isobe2022/drone-isobe2022/Otamesi.m
@@ -3,4 +3,12 @@ close all hidden;
 clear all;
 clc;
 
-disp(['a:' num2str(1) 'b:' num2str(2)])
+%% データのインポート
+load("experiment_6_20_circle_estimaterdata.mat") %読み込むデータファイルの設定
+% load("circle_6_26_sim_Log(26-Jun-2023_22_10_30).mat")
+disp('load finished')
+
+for i = 1:find(log.Data.t,1,'last')
+    data.t(1,i) = log.Data.t(i,1);                                      %時間t
+    data.p(:,i) = log.Data.agent.estimator.result{i}.state.p(:,1);
+end
