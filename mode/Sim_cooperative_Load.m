aed03c1b2743ad6ca474f84a72e042b2a9014d84
kasekiguchi <kasekiguchi@gmail.com>
2023-07-06 18:52:16 +0900

    sekiguchi

--- mode/Sim_cooperative_Load.m
+++ mode/Sim_cooperative_Load.m
@@ -1,6 +1,6 @@
 clc
 ts = 0;
-dt = 0.025;
+dt = 0.01;
 te = 25;
 time = TIME(ts,dt,te);
 in_prog_func = @(app) dfunc(app);
@@ -12,9 +12,10 @@ N = 3;
 % qtype = "eul"; % "eul" : euler angle, "" : euler parameter
 qtype = "zup"; % "eul" : euler angle, "" : euler parameter
 % x = [p0 Q0 v0 O0 qi wi Qi Oi]
-initial_state.p = [-2;-2;1];
+%initial_state.p = [-2;-2;1];
+initial_state.p = [1;4.8;1.5];
 initial_state.v = [0;0;0];
-initial_state.O = [0;0;0];
+initial_state.O = [0;0;-1.0996];
 initial_state.wi = repmat([0;0;0],N,1);
 initial_state.Oi = repmat([0;0;0],N,1);
 
@@ -29,6 +30,7 @@ initial_state.Qi = repmat([0;pi/180;0],N,1);
 %initial_state.Qi = repmat([0;0;0],N,1);
 else
 initial_state.Q = [1;0;0;0];
+initial_state.Q = initial_state.Q/vecnorm(initial_state.Q);
 initial_state.Qi = repmat([1;0;0;0],N,1);
 %initial_state.Qi = repmat(Eul2Quat([pi/180;0;0]),N,1);
 end
@@ -40,19 +42,19 @@ agent.estimator = DIRECT_ESTIMATOR(agent,struct("model",MODEL_CLASS(agent,Model_
 %agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_Suspended_Cooperative_Load(dt, initial_state, 1,N)),["p","Q","v","O","qi","wi","Qi","Oi"]));
 
 agent.sensor = DIRECT_SENSOR(agent,0.0); % sensor to capture plant position : second arg is noise 
-% agent.reference = TIME_VARYING_REFERENCE(agent,Reference_Time_Varying("gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[2,2,0.5]}));
-% agent.reference = POINT_REFERENCE_COOPERATIVE_LOAD(agent,[1,1,1]);
-agent.reference = TIME_VARYING_REFERENCE_Cooporative(agent,Reference_Time_Varying_Cooprative_Load("gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[2,2,0.5]}));
+%agent.reference = TIME_VARYING_REFERENCE(agent,Reference_Time_Varying("gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[2,2,0.5]}));
+agent.reference = POINT_REFERENCE_COOPERATIVE_LOAD(agent,[1,1,1]);
+%agent.reference = TIME_VARYING_REFERENCE_Cooporative(agent,Reference_Time_Varying_Cooprative_Load("gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[2,2,0.5]}));
 %agent.controller = GEOMETRIC_CONTROLLER(agent,Controller_Cooperative_Load(dt));
 agent.controller = CSLC(agent,Controller_Cooperative_Load(dt,N));
 % agent.controller = GEOMETRIC_CONTROLLER_with_3_Drones(agent,Controller_Cooperative_Load(dt));
 run("ExpBase");
 
-%%
+%
 clc
 
-for i = 1:200
-  i
+for i = 1:1000
+if i< 20 || rem(i,10)==0, i, end
 agent(1).sensor.do(time,'f');
 agent(1).estimator.do(time,'f');
 agent(1).reference.do(time,'f');
@@ -66,14 +68,15 @@ time.t = time.t + time.dt;
 %pause(1)
 end
 %%
+logger.plot({1,"p","p"},{1, "plant.result.state.Q", "p"},{1,"plant.result.state.qi","p"},{1, "plant.result.state.Qi","p"})
+%%
 close all
 mov = DRAW_COOPERATIVE_DRONES(logger, "self",agent,"target", 1:N);
-mov.animation(logger,'target',1:N)
+mov.animation(logger,'target',1:N,"gif",true)
 %%
-logger.plot({1,"plant.result.state.qi","p"},{1,"p","e"},{1, "v", "p"},{1, "input", "p"},{1, "plant.result.state.Qi","p"})
 %%
 function dfunc(app)
 app.logger.plot({1, "p", "er"},"ax",app.UIAxes,"xrange",[app.time.ts,app.time.t]);
 app.logger.plot({1, "q", "e"},"ax",app.UIAxes2,"xrange",[app.time.ts,app.time.t]);
-app.logger.plot({1, "input", ""},"ax",app.UIAxes3,"xrange",[app.time.ts,app.time.t]);
+appb.logger.plot({1, "input", ""},"ax",app.UIAxes3,"xrange",[app.time.ts,app.time.t]);
 end
