afb97b296165bcca65b3ef2a8256d99ad4ffdd5e
g1812100 <85143997+g1812100@users.noreply.github.com>
2023-07-13 17:25:40 +0900

    change

--- reference/SuspendedLoad/gen_ref_sample_cooperative_load.m
+++ reference/SuspendedLoad/gen_ref_sample_cooperative_load.m
@@ -26,19 +26,20 @@ ly = scale(2); %3.5;
 ly_offset = origin(2);% 3.5;
 lz = scale(3);% 1;
 lz_offset=origin(3);% 1;
-w = 0.2%*pi/T; % T秒で一周
+w = 1; % T秒で一周
+% w = 1*pi/T; % T秒で一周
 
 % ref=@(t) [lx*cos(w*t + phase)+lx_offset; % x
 % ly*sin(w*t + phase)+ly_offset; % y
 % lz*sin(2*w*t + phase/2)+lz_offset; % z
 % 0];%
 
-% ref=@(t) [lx*cos(w*t + phase)+lx_offset; % x
-% ly*sin(w*t + phase)+ly_offset; % y
-% lz_offset]; % z
-% dx = diff(ref,t,1);
+ref=@(t) [lx*cos(w*t + phase)+lx_offset; % x
+ly*sin(w*t + phase)+ly_offset; % y
+lz_offset]; % z
+dx = diff(ref,t,1);
 
-ref=@(t)[1.2*sin(2*w*pi*t);4.2*cos(w*pi*t);2];
+% ref=@(t)[1.2*sin(2*w*pi*t);4.2*cos(w*pi*t);2];
 
 % fprintf("max ref acceleration = %f\n",subs(ddx(3),t,T/4));
 
