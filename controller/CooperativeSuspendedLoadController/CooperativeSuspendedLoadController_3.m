a81ffe941a611b64a55388c1e43497aea9278ad0
user2 <ksekiguc@tcu.ac.jp>
2023-08-21 08:39:50 +0900

    at home

--- controller/CooperativeSuspendedLoadController/CooperativeSuspendedLoadController_3.m
+++ controller/CooperativeSuspendedLoadController/CooperativeSuspendedLoadController_3.m
@@ -1,4 +1,4 @@
-function [U,tdqid,tddqid] = CooperativeSuspendedLoadController_3(x,qi,R0,Ri,R0d,xd,K,P,Pdagger,tdqid,tddqid)
+function [U,dqid,ddqid] = CooperativeSuspendedLoadController_3(x,qi,R0,Ri,R0d,xd,K,P,Pdagger,tdqid,tddqid)
  % x : state
  % qi : subpart of x 3xN
  % xd : reference 18x1 : =[x0d;dx0d;ddx0d;dddx0d;o0d;do0d] 
@@ -13,16 +13,12 @@ R0TFdMd = CSLC_3_R0TFdMd(x,xd,R0,R0d,P,K);
 muid = reshape(kron(eye(3),R0)*Pdagger*R0TFdMd,3,3); % 3xN
 mui = sum(muid.*qi,1).*qi; % 3xN
 qid = -muid./vecnorm(muid,2,1); % 3xN
-Ap = -49/51;% -2/3
-Bp = -100000/2601; % -250/9
-Dp = 1000/51; % 50/3
-tdqid = Ap*tdqid + Bp*qid;
-dqid = 0*(tdqid + Dp*qid);
-tddqid = Ap*tddqid + Bp*dqid;
-ddqid =  0*(tddqid + Dp*dqid); 
+dqid = cross(qid,qi);
+ddqid =  0*(dqid); 
 ui = CSLC_3_ui(x,xd,R0,R0d,P,K,Pdagger,mui,qid,dqid,ddqid);
 b3 = ui./vecnorm(ui,2,1); % 3xN
-b1 = xd(4:6) + 0.1*cell2mat(arrayfun(@(i) Ri(:,:,i)*[1;0;0],1:3,'UniformOutput',false)); % Caution : problem if dx0d = - 10*xi.
+b1 = repmat(xd(4:6),1,3);% + 0.1*cell2mat(arrayfun(@(i) Ri(:,:,i)*[1;0;0],1:3,'UniformOutput',false)); % Caution : problem if dx0d = - 10*xi.
+b1(:,vecnorm(b1,2,1)==0) = repmat([1;0;0],1,sum(vecnorm(b1,2,1)==0));
 b1 = b1./vecnorm(b1,2,1);
 b2 = cross(b3, b1); % 3xN
 si = vecnorm(b2,2,1);
