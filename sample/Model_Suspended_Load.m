2ff10025a5c20434263b05d50fcf1282cc60bd01
kasekiguchi <kasekiguchi@gmail.com>
2023-10-16 09:03:02 +0900

    整理４

--- sample/Model_Suspended_Load.m
+++ sample/Model_Suspended_Load.m
@@ -6,22 +6,20 @@ arguments
   agent = ""
 end
 Model.id = id;
-Model.type="Suspended_Load_Model"; % model name
 Model.name="load"; % print name
-
 
 %Setting.projection = @(x)[x(1:18);x(19:21)/norm(x(19:21));x(22:24)-dot(x(19:21)/norm(x(19:21)),x(22:24))*x(19:21)/norm(x(19:21))];
 if strcmp(agent ,"plant")
-Setting.method = get_model_name("Load"); % model dynamicsの実体名
-Setting.dim=[25,4,16];
-Setting.num_list = [3,4,3,3,3,3,3,3];
+  Setting.method = get_model_name("Load"); % KF未実装
+  Setting.dim=[25,4,16];
+  Setting.num_list = [3,4,3,3,3,3,3,3];
 else
-Setting.method = get_model_name("Load_HL"); % model dynamicsの実体名
-Setting.dim=[24,4,21];
-Setting.num_list = [3,3,3,3,3,3,3,3];
+  Setting.method = get_model_name("Load_HL"); % model dynamicsの実体名
+  Setting.dim=[24,4,21];
+  Setting.num_list = [3,3,3,3,3,3,3,3];
 end
 Setting.state_list =  ["p","q","v","w","pL","vL","pT","wL"];
-Setting.initial = initial; %struct('p',[0;0;0],'q',[1;0;0;0],'v',[0;0;0],'w',[0;0;0],"pL",[0;0;0],"vL",[0;0;0],"pT",[0;0;-1],"wL",[0;0;0]);
+Setting.initial = initial;
 Setting.initial.vL = [0;0;0];
 Setting.initial.pT = [0;0;-1];
 Setting.initial.wL = [0;0;0];
