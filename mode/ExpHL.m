e234a84990068e216705e9e472d7110fb7a51dd4
1912063 <104186106+1912063@users.noreply.github.com>
2023-10-20 16:52:53 +0900

    10月20日
    P2Pのx方向の実験データ7つ

--- mode/ExpHL.m
+++ mode/ExpHL.m
@@ -16,7 +16,7 @@ initial_state.v = [0; 0; 0];
 initial_state.w = [0; 0; 0];
 
 agent = DRONE;
-agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "udp", [100, 252]));
+agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "udp", [100, 251]));
 agent.parameter = DRONE_PARAM("DIATONE");
 agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)), ["p", "q"]));
 agent.sensor = MOTIVE(agent, Sensor_Motive(1,0, motive));
@@ -27,8 +27,7 @@ agent.input_transform = THRUST2THROTTLE_DRONE(agent,InputTransform_Thrust2Thrott
 %agent.reference = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",0,"orig",[0;0;1],"size",[0,0,0]},"HL"});
 
 % agent.reference = TIME_VARYING_REFERENCE(agent,{"Case_study_trajectory",{[0,0,0]},"HL"});
-agent.reference = MY_POINT_REFERENCE(agent,{struct("f",[1;0;1],"g",[-1;0;1]),5});
-
+agent.reference = MY_POINT_REFERENCE(agent,{struct("f",[1;0;1],"g",[-1;0;1],"h",[1;0;1],"j",[-1;0;1]),15});
 agent.controller = HLC(agent,Controller_HL(dt));
 
 run("ExpBase");
