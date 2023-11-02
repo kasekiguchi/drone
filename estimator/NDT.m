1773a683dbd403fc7795b152b5c71ed567f6a799
g2012101 <g2012101@tcu.ac.jp>
2023-11-02 18:34:59 +0900

    1102_result
    ・estimator.doで推定結果を保存するとき型のエラー
    ・plant.doのif文が意味不明
    ・NDTクラスに渡すsampleの名前をEstimater_NDTに変更

--- estimator/NDT.m
+++ estimator/NDT.m
@@ -5,6 +5,8 @@ classdef NDT < handle
         result
         self
         model
+
+        state
     end
 
     properties
@@ -18,7 +20,8 @@ classdef NDT < handle
         function obj = NDT(self,param)
             % コンストラクタ
             obj.self = self; % agent that 
-            obj.model = param.model;
+            model = param.model;
+            obj.model = model;
             param = param.param;
             obj.initialtform = param.initialtform;
             obj.fixedSeg = param.fixedSeg;
@@ -28,8 +31,11 @@ classdef NDT < handle
 
         function [result] = do(obj,~)
             obj.PCdata_use = obj.self.sensor.result;
-            obj.result.tform = pcregisterndt(obj.PCdata_use,obj.fixedSeg,0.5,"InitialTransform",obj.initialtform,"OutlierRatio",0.1,"Tolerance",[0.01 0.1]);%マッチング
-            
+            obj.result.tform = pcregisterndt(obj.PCdata_use,obj.fixedSeg.floormap,0.5,"InitialTransform",obj.initialtform,"OutlierRatio",0.1,"Tolerance",[0.01 0.1]);%マッチング
+
+            tmpvalue = obj.result.tform;
+            obj.result.state.set_state(tmpvalue);%%%%%推定結果
+            obj.model.state.set_state(tmpvalue);%%%%%%モデルの更新
         end
         
         % function result=do(obj,varargin)
