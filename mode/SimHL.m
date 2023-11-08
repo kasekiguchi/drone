01ddd7c5e52cd9a0f37992493154a326b5cb3d7f
1912063 <104186106+1912063@users.noreply.github.com>
2023-11-08 14:23:30 +0900

    11月8日
    実験、P2Pで斜めのデータ取得

--- mode/SimHL.m
+++ mode/SimHL.m
@@ -17,21 +17,21 @@ agent.parameter = DRONE_PARAM("DIATONE");
 agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)),["p", "q"]));
 agent.sensor = MOTIVE(agent, Sensor_Motive(1,0, motive));
 
-agent.reference = TIME_VARYING_REFERENCE(agent,{"Case_study_trajectory",{[0,0,1]},"HL"});
+% agent.reference = TIME_VARYING_REFERENCE(agent,{"Case_study_trajectory",{[0,0,1]},"HL"});
 % agent.reference = MY_POINT_REFERENCE(agent,{struct("f",[1;0;1],"g",[-1;0;1]),5});
 % agent.reference = MY_POINT_REFERENCE(agent,{struct("f",[1;0;1],"g",[-1;0;1],"h",[1;0;1],"j",[-1;0;1]),20});
 % agent.reference = POINT_REFERENCE(agent,{"PtoP",{[0,0,0]},"HL"});
 
 % agent.reference = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",10,"orig",[0;0;1],"size",[1,1,0.5]},"HL"});
 %agent.reference = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",0,"orig",[0;0;1],"size",[0,0,0]},"HL"});
-% agent.reference = MY_POINT_REFERENCE(agent,{struct("f",[0;0;0.5],"g",[0;0;1],"h",[0;0;0.5],"j",[0;0;1]),20});
+agent.reference = MY_POINT_REFERENCE(agent,{struct("f",[1;1;1],"g",[0;0;1],"h",[1;1;1],"j",[0;0;1]),20});
 agent.controller = HLC(agent,Controller_HL(dt));
 run("ExpBase");
 function dfunc(app)
 
-% app.logger.plot({1, "p1-p2", "e"},"ax",app.UIAxes,"xrange",[app.time.ts,app.time.te]);
+app.logger.plot({1, "p1-p2", "e"},"ax",app.UIAxes,"xrange",[app.time.ts,app.time.te]);
 % app.logger.plot({1, "p1-p2-p3", "e"},"ax",app.UIAxes,"xrange",[app.time.ts,app.time.te]);
-app.logger.plot({1, "p", "re"},"ax",app.UIAxes,"xrange",[app.time.ts,app.time.te]);
+% app.logger.plot({1, "p", "re"},"ax",app.UIAxes,"xrange",[app.time.ts,app.time.te]);
 app.logger.plot({1, "q", "se"},"ax",app.UIAxes2,"xrange",[app.time.ts,app.time.te]);
 app.logger.plot({1, "v", "er"},"ax",app.UIAxes3,"xrange",[app.time.ts,app.time.te]);
 app.logger.plot({1, "input", ""},"ax",app.UIAxes4,"xrange",[app.time.ts,app.time.t]);
