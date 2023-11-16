b2d143d96a7cf11f21b4c6706822f16db10dd4da
2012100 <g2012100tcu@gmail.com>
2023-11-16 16:57:13 +0900

    事例研更新...したいなぁ

--- jireiken_main.m
+++ jireiken_main.m
@@ -1,1 +1,33 @@
-distance = wall_distance_ref()
+%% ROS2 講座 
+% 実施日：2023/10/31
+% 作成者：緒方 翔一
+%% reset
+close all; clear; clc;
+%% 2.MATLABからのPublish
+node1=ros2node("/nodeP",23); % ノードをたてる
+[Pub, msg]=ros2publisher(node1,"/topic","geometry_msgs/Pose"); % トピック名とメッセージタイプを定義
+msg.position.x = 10; % メッセージに実際の値を代入
+send(Pub, msg); % メッセージを送信
+
+%% 3.MATLABでのSubscribe
+node2=ros2node("/nodeS",23); % ノードをたてる
+
+% % receive
+% Sub = ros2subscriber(node2, "/topic","geometry_msgs/Pose"); % トピック名とメッセージタイプを定義
+% message = receive(Sub); % receiveでメッセージを受信
+
+% % callback
+% ros2subscriber(node2, "/topic","geometry_msgs/Pose",@callback_topic); % トピック名とメッセージタイプとcallbackを定義
+% function callback_topic(message) % "messege"はルール上固定の名前
+% %バックグラウンドで回り続ける関数
+% x=message.position.x;
+% disp(x);
+% end
+
+%% 5. %265のmsgをsubscribe
+node3=ros2node("nodeT",23);
+subscriber = ros2subscriber(node3, "/camera/pose/sample", "nav_msgs/Odometry", @T265_callback,"Reliability","besteffort");
+function T265_callback(message)
+x=message.pose.pose.position;
+disp(x);
+end
