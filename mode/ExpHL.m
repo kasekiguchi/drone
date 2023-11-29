bdb459d5a0bf0c4a72e50ab6ccac2331d6bd5bf3
2012100 <g2012100tcu@gmail.com>
2023-11-29 17:21:30 +0900

    うまくいかなーい

--- mode/ExpHL.m
+++ mode/ExpHL.m
@@ -22,6 +22,9 @@ agent.parameter = DRONE_PARAM("DIATONE");
 agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)), ["p", "q"]));
 % agent.sensor = MOTIVE(agent, Sensor_Motive(1,0, motive));
 agent.sensor = VL53L1X(agent,Sensor_VL53L1X(agent));
+% agent.sensor.motive = MOTIVE(agent, Sensor_Motive(1,0, motive));
+% agent.sensor.vl = VL53L1X(agent,Sensor_VL53L1X(agent));
+% agent.sensor.do = @sensor.do;
 agent.input_transform = THRUST2THROTTLE_DRONE(agent,InputTransform_Thrust2Throttle_drone()); % 推力からスロットルに変換
 
 % agent.reference = TIME_VARYING_REFERENCE(agent,{"Case_study_trajectory",{"freq",10,"orig",[0;0;1],"size",[1,1,0]},"HL"});
