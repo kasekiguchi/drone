f56ec4c6811ffeb1741f5352d285eb851424ddea
g2012101 <g2012101@tcu.ac.jp>
2024-01-17 10:59:26 +0900

    Update ExpROS.m

--- mode/ExpROS.m
+++ mode/ExpROS.m
@@ -1,17 +1,23 @@
 % clc;
 % clear all
 % disp("clear node");
+clc;
+clear all
+disp("clear node");
 
 % mega rover
 ts = 0; % initial time
 dt = 0.25; % sampling period
 te = 200; % termina time
+dt = 0.025; % sampling period
+te = 30; % termina time
 time = TIME(ts,dt,te);
 in_prog_func = @(app) in_prog(app);
 post_func = @(app) post(app);
 logger = LOGGER(1, size(ts:dt:te, 2), 1, [],[]);
 
 initial_state.p = [0.0;0;0];
+initial_state.p = [0.0;1;0];
 initial_state.q = [0;0;0];
 initial_state.v = [0; 0; 0];
 initial_state.w = [0; 0; 0];
@@ -21,18 +27,19 @@ agent.plant = WHILL_EXP_MODEL(agent,Model_Whill_Exp(dt, initial_state, "ros2", 7
 % agent.plant = WHILL_EXP_MODEL(agent,Model_Whill_Exp(dt, initial_state, "ros",25));
 agent.parameter = VEHICLE_PARAM("VEHICLE3");
 % agent.sensor = ROS(agent, Sensor_ROS(struct('DomainID',25)));
-% agent.sensor = ROS2_LiDAR_PCD(agent, Sensor_LiDAR_ROS2(Expnode));
 agent.sensor = ROS2_SENSOR(agent, Sensor_Ros2_multi(agent));
 % agent.estimator = UKF2DSLAM(agent, Estimator_UKF2DSLAM_Vehicle(agent,dt,MODEL_CLASS(agent,Model_Vehicle45(dt, initial_state, 1)), ["p", "q"]));
 agent.estimator = NDT(agent,Estimator_NDT(agent,dt,MODEL_CLASS(agent,Model_Vehicle45(dt, initial_state, 1)),'experimentroom_map4'));
+agent.estimator = NDT(agent,Estimator_NDT(agent,dt,MODEL_CLASS(agent,Model_Vehicle45(dt, initial_state, 1))));
 % agent.reference = PATH_REFERENCE(agent,Reference_PathCenter(agent.sensor.lrf.radius));
 agent.reference = TIME_VARYING_REFERENCE(agent,{"Case_study_trajectory",{[0;0;0]}});
+% agent.reference = TIME_VARYING_REFERENCE(agent,{"Case_study_trajectory",{[0;0;0]}});
 agent.controller = APID_CONTROLLER(agent,Controller_APID(dt));
 
 run("ExpBase");
 
 %%
-clc
+% clc
 % for i = 1:time.te
 % %    if i < 20 || rem(i, 10) == 0, i, end
     % agent(1).sensor.do(time, 'f');
@@ -44,20 +51,20 @@ clc
 %     time.t = time.t + time.dt;
 %     %pause(1)
 % end
-% for i = 1:time.te
-%    % if i < 20 || rem(i, 10) == 0, i, end
-%     agent.sensor.do(time);
-%     agent.estimator.do(time);
-%     agent.reference.do(time,'f');
-%     agent.controller.do(time,'f');
-%     agent.plant.do(time, 'f');
-%     logger.logging(time, 'f', agent);
-%     time.t = time.t + time.dt;
-%     % disp(agent.estimator.result.state.p);
-%     % pause(1)
-% end
+for i = 1:time.te
+   % if i < 20 || rem(i, 10) == 0, i, end
+    agent.sensor.do(time);
+    agent.estimator.do(time);
+    agent.reference.do(time,'f');
+    agent.controller.do(time,'f');
+    agent.plant.do(time, 'f');
+    logger.logging(time, 'f', agent);
+    time.t = time.t + time.dt;
+    disp(agent.estimator.result.state.p);
+    pause(dt)
+end
 %%
-% 
+
 % for j = 1:100
 %     px(j) = logger.Data.agent.estimator.result{1,j}.state.p(1,1);
 %     py(j) = logger.Data.agent.estimator.result{1,j}.state.p(2,1);
