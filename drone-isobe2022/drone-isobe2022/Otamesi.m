87048aa05456d9e115f0f9b5f783a88b1a502f5e
g2012035 <g2012035@tcu.ac.jp>
2024-02-10 18:10:37 +0900

    change

--- drone-isobe2022/drone-isobe2022/Otamesi.m
+++ drone-isobe2022/drone-isobe2022/Otamesi.m
@@ -1,18 +1,18 @@
 clc
 clear all
-% close all
+close all
 % exp1 = load("EstimationResult_12state_2_4_Exp_sprine+zsprine+P2Pz_torque_incon.mat",'est');
-% exp1 = load("EstimationResult_12state_2_7_Exp_sprine+zsprine+P2Pz_torque_incon_150data_vzからz算出.mat",'est');
-exp1 = load("EstimationResult_12state_2_7_Exp_sprine+zsprine+P2Pz_torque_incon_150data.mat",'est');
+exp1 = load("EstimationResult_12state_2_7_Exp_sprine+zsprine+P2Pz_torque_incon_150data_vzからz算出.mat",'est');
+% exp1 = load("EstimationResult_12state_2_7_Exp_sprine+zsprine+P2Pz_torque_incon_150data.mat",'est');
 % exp1 = load("EstimationResult_12state_2_1_Exp_sprine100__torque_incon.mat",'est');
 % exp1 = load("EstimationResult_12state_12_17_ExpcirrevsadP2Pxydata_est=P2Pshape.mat",'est');
-% exp1 = load("test.mat");
+% exp1 = load("EstimationResult_12state_11_29_GUIsimdata_input=torque.mat",'est');
 % exp1 = load("EstimationResult_12state_1_29_Exp_sprineandall_est=P2Pshape_torque_incon.mat");
 %% 
 
 m = 0.5884;
 g = 9.81;
-u1 = m*g-100;
+u1 = -100;
 u2 = 0;
 u3 = 0;
 u4 = 0;
@@ -27,32 +27,52 @@ for i = 1:t-1
     x(:,i+1) = exp1.est.A*x(:,i) + exp1.est.B*u(:,i);
     % U(:,i) = exp1.est.B*u(:,i);
     % x(:,i+1) = exp1.est.A*x(:,i);
-    % u(1,i+1) = 0.5884*9.81+randi([-5 5])*0.1;
+    % u(1,i+1) = u(1,1)^t;
 end
 
 X = exp1.est.C*x;
+% X = [x0,X];
 
-size = figure;
-size.WindowState = 'maximized'; %表示するグラフを最大化
-subplot(2,3,1)
-plot(1:t,X(1,:),'LineWidth',1.2)
+figure(1)
+plot(1:t,X(3,:),'LineWidth',1.8)
 grid on
-subplot(2,3,2)
-plot(1:t,X(2,:),'LineWidth',1.2)
+xlabel('Number of Steps','FontSize',16)
+ylabel('Position z [m]','FontSize',18)
+
+figure(2)
+plot(1:t,x(9,:),'LineWidth',1.8)
 grid on
-subplot(2,3,3)
-plot(1:t,X(3,:),'LineWidth',1.2)
-grid on
-subplot(2,3,4)
-plot(1:t,u(1,:),'LineWidth',1.2)
-grid on
-hold on
-xlabel('入力')
-yline(u1,'Color','r','LineWidth',1.2)
-subplot(2,3,5)
-plot(1:t,X(9,:),'LineWidth',1.2)
-grid on
-xlabel('z速度')
+xlabel('Number of Steps','FontSize',16)
+ylabel('Velocity vz [m/s]','FontSize',18)
+
+% xlim([1 t])
+% ax = gca;
+% 
+% fontSize = 12; %軸の文字の大きさの設定
+% set(ax,'FontSize',fontSize); 
+
+% size = figure;
+% size.WindowState = 'maximized'; %表示するグラフを最大化
+% subplot(2,3,1)
+% plot(1:t,X(1,:),'LineWidth',1.2)
+% grid on
+% subplot(2,3,2)
+% plot(1:t,X(2,:),'LineWidth',1.2)
+% grid on
+% subplot(2,3,3)
+% plot(1:t,X(3,:),'LineWidth',1.2)
+% grid on
+% subplot(2,3,4)
+% plot(1:t,u(1,:),'LineWidth',1.2)
+% grid on
+% hold on
+% xlabel('入力')
+% yline(u1,'Color','r','LineWidth',1.2)
+% subplot(2,3,5)
+% plot(1:t,X(9,:),'LineWidth',1.2)
+% grid on
+% xlabel('z速度')
+
 % subplot(2,3,6)
 % plot(1:t,U(:,:),'LineWidth',1.2)
 % grid on
