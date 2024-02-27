90b7bae3c168115eb87431ef13e8c326d46753bc
1912063 <104186106+1912063@users.noreply.github.com>
2024-02-27 11:30:33 +0900

    2_27
    実機動作確認

--- controller/KMPC/Controller_MPC_Koopman.m
+++ controller/KMPC/Controller_MPC_Koopman.m
@@ -22,11 +22,11 @@ function Controller = Controller_MPC_Koopman(Agent)
     Controller_param.C = est.C;
 
     %% 重み
-    Controller_param.weight.P = diag([1; 1; 1]);    % 座標   1000 1000 10000
-    Controller_param.weight.V = diag([1; 1; 1]);    % 速度
+    Controller_param.weight.P = diag([20; 1; 30]);    % 座標   1000 1000 10000
+    Controller_param.weight.V = diag([30; 20; 1]);    % 速度
     Controller_param.weight.R = diag([1; 1; 1; 1]); % 入力
     Controller_param.weight.RP = 0 * diag([1; 1; 1; 1]);  % 1ステップ前の入力との差    0*(無効化)
-    Controller_param.weight.QW = diag([1; 1; 1; 1; 1; 1]);  % 姿勢角，角速度
+    Controller_param.weight.QW = diag([10; 1; 1; 1; 1; 1]);  % 姿勢角，角速度
 
     Controller_param.weight.Pf = Controller_param.weight.P; % 6
     Controller_param.weight.Vf = Controller_param.weight.V;
