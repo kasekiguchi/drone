e29add0a55d3e2836c4ff3ae1c9e924a64294613
Naoki-Yata <g2112093@tcu.ac.jp>
2024-05-15 10:26:17 +0900

    ArduinoPPM信号確認試験結果追加

--- experiment/main.m
+++ experiment/main.m
@@ -4,16 +4,17 @@ close all hidden; clear; clc;
 %%
 dt = 0.025;
 fExp = 1;
-agent = DRONE;
-agent.parameter = DRONE_PARAM("DIATONE");
+
 initial_state.p = [0; 0; 0];
 initial_state.q = [1;0; 0; 0];
 initial_state.v = [0; 0; 0];
 initial_state.w = [0; 0; 0];
-initial_state.Trs = [agent.parameter.mass*agent.parameter.gravity+0.1; 0];%重力を打ち消すため最初はTr=m*g
-
-agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial","COM3"));
-	% COM：機体番号（ArduinoのCOM番号）
-msg=gen_msg([500,500,0,500,0,0,0,0]);
-agent.plant.connector.sendData(msg);
-
+agent = DRONE;
+agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial", "COM4"));
+% COM：機体番号（ArduinoのCOM番号）
+    pause(0.75)
+    %途中で一時停止しないとArduinoが信号を送信しない，原因不明
+    msg=gen_msg([1000,500,0,1000,0,500,500,1000]); %([Roll,Pitch,Throttle,Yaw,arming])
+    pause(0.75)
+    %途中で一時停止しないとArduinoが信号を送信しない，原因不明
+    agent.plant.connector.sendData(msg)
