38160a434bd575846a73487c88152ec61baf5717
kemiyake <g2112086@tcu.ac.jp>
2024-05-17 15:55:55 +0900

    飛行実績ありの入力

--- reference/functions/Case_study_trajectory.m
+++ reference/functions/Case_study_trajectory.m
@@ -9,13 +9,16 @@ z_0 = X0{1}(3);
 
 s = X0{2}; % s = 2 → period = 4*pi (12 sec)ハート1周
 y_offset = 5;
-r = 0.1;
-% 
-% x = x_0+sin(t/s);%r*16*sin(t/s)^3;
-% y = y_0+cos(t/s);%r*(13*cos(t/s)-5*cos(2*t/s)-2*cos(3*t/s)-cos(4*t/s)-y_offset);
+%r = 0.1;
+r=0.07
+% ハート型
+% x = x_0+r*16*sin(t/s)^3;%r*sin(t/s);%
+% y = y_0+r*(13*cos(t/s)-5*cos(2*t/s)-2*cos(3*t/s)-cos(4*t/s)-y_offset);%r*cos(t/s);%
 % z = z_0;
-x = sin(t/2);
-y = cos(t/2);
+% 円旋回
+T=10;
+x = sin(2*pi*t/T);
+y = cos(2*pi*t/T);
 z = z_0;
 
 ref=@(t)[x;y;z;0];
