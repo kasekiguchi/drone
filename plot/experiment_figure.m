f016130df3453c900760c604650364651a501141
kemiyake <g2112086@tcu.ac.jp>
2024-05-21 11:27:03 +0900

    グラフ作成追加

--- plot/experiment_figure.m
+++ plot/experiment_figure.m
@@ -8,7 +8,8 @@ set(0,'defaultTextFontsize',15);
 set(0,'defaultLineLineWidth',1.5);
 set(0,'defaultLineMarkerSize',15);
 
-load("Data/Eikyu_0514_result/logger_Eikyu2022.mat");
+%load("Data/Eikyu_0514_result/logger_Eikyu2022.mat");
+load("Data/Eikyu_0514_result/momoseHL_miyake_0514.mat");%単純HL
 log = logger;
 %%
 figtype = 1;%1でグラフを1タブづつ，2で1タブにグラフを多数．
@@ -20,6 +21,7 @@ logt = log.Data.t(flight_start_idx:flight_finish_idx);
 
 % initialize data
 Est = zeros(12, flight_finish_idx-flight_start_idx+1);
+Road_est = zeros(9, flight_finish_idx-flight_start_idx+1);
 Ref = zeros(3,  flight_finish_idx-flight_start_idx+1);
 Input = zeros(4,flight_finish_idx-flight_start_idx+1);
 InnerInput = zeros(8, flight_finish_idx-flight_start_idx+1);
@@ -28,7 +30,9 @@ for i = flight_start_idx:flight_finish_idx
                 Agent.estimator.result{i}.state.q;
                 Agent.estimator.result{i}.state.v;
                 Agent.estimator.result{i}.state.w];
-
+    Road_est(:,i-flight_start_idx+1) = [Agent.estimator.result{i}.state.pL;
+                Agent.estimator.result{i}.state.vL;
+                Agent.estimator.result{i}.state.wL];
     Ref(:,i-flight_start_idx+1) = [Agent.reference.result{i}.state.p];
 
     Input(:,i-flight_start_idx+1) = Agent.input{i};
@@ -40,7 +44,7 @@ m = 3; n = 3;
 if figtype == 1
     % Title = strcat('LandingFreeFall', '-N', num2str(data.param.Maxparticle_num), '-', num2str(te), 's-', datestr(datetime('now'), 'HHMMSS'));
     figure(1); plot(logt, Est(1:3,:)); hold on; plot(logt, Ref(1:3,:), '--'); hold off;
-    xlabel("Time [s]"); ylabel("Position [m]"); legend("x.state", "y.state", "z.state", "x.reference", "y.reference", "z.reference",  "Location","northwest");
+    xlabel("Time [s]",'Interpreter','latex'); ylabel("Position [m]",'Interpreter','latex'); legend("x.state", "y.state", "z.state", "x.reference", "y.reference", "z.reference",  "Location","northwest",'Interpreter','latex');
     grid on; xlim([logt(1), logt(end)]); ylim([-inf inf]);
     % title("Time change of Position"); 
     % attitude
@@ -59,14 +63,28 @@ if figtype == 1
     grid on; xlim([logt(1), logt(end)]); ylim([-inf inf]);
     ytickformat('%.1f');
     % virtual input
-    figure(5); plot(logt, InnerInput);legend("u.1", "u.2", "u.3", "u.4");%キャプション間違ってるかも
+    figure(5); plot(logt, InnerInput);legend("z","roll", "pitch", "yaw");%キャプション間違ってるかも
     grid on; xlim([logt(1), logt(end)]); ylim([-inf inf]);
     ytickformat('%.1f');
     %以下三宅整備中1/2
-    figure(6); plot(Est(1,:), Est(2,:)); hold on; plot(Ref(1,:), Ref(2,:), '--'); hold off;
-    xlabel("x [m]"); ylabel("y [m]"); %legend("x.state", "y.state");
-    grid on; xlim([-3, 3]); ylim([-3 3]);
-    ytickformat('%.1f');
+    figure(6); plot(Road_est(1,:), Road_est(2,:)); hold on; plot(Ref(1,:), Ref(2,:), '--'); hold off;
+    xlabel('x [m]','Interpreter','latex'); ylabel("y [m]",'Interpreter','latex'); legend("estimeter", "reference",'Interpreter','latex');
+    grid on; xlim([-3.0, 3.0]); ylim([-3.0, 3.0]);
+    %ytickformat('%.1f');
+    % Title = 
+    %荷物のプロット↓
+    figure(7); plot(logt, Road_est(1:3,:)); hold on; plot(logt, Ref(1:3,:), '--'); hold off;
+    xlabel("Time [s]",'Interpreter','latex'); ylabel("Load position [m]",'Interpreter','latex'); legend("x.state", "y.state", "z.state", "x.reference", "y.reference", "z.reference",  "Location","northwest",'Interpreter','latex');
+    grid on; xlim([logt(1), logt(end)]); ylim([-inf inf]);
+    %機体と荷物のxy重ねて表示↓
+    figure(8); plot(Est(1,:), Est(2,:)); hold on; plot(Road_est(1,:), Road_est(2,:), '--'); hold off;
+    xlabel('$$x$$ [m]','Interpreter','latex'); ylabel("$$y$$ [m]",'Interpreter','latex'); legend("Drone", "Load",'Interpreter','latex');
+    grid on; xlim([-3.0, 3.0]); ylim([-3.0, 3.0]);
+    %目標軌道と機体と荷物のxy重ねて表示↓
+    figure(9); plot(Est(1,:), Est(2,:)); hold on; plot(Road_est(1,:), Road_est(2,:), '--');plot(Ref(1,:), Ref(2,:), '--'), hold off;
+    % xlabel("x [m]"); ylabel("y [m]"); legend("Drone", "Load","Reference of Load");
+    grid on; xlim([-3.0, 3.0]); ylim([-3.0, 3.0]);
+    xlabel('$$x$$ [m]','Interpreter','latex'); ylabel("$$y$$ [m]",'Interpreter','latex'); legend("Drone", "Load","Reference of Load",'Interpreter','latex');
 elseif figtype == 2
     % Title = strcat('LandingFreeFall', '-N', num2str(data.param.Maxparticle_num), '-', num2str(te), 's-', datestr(datetime('now'), 'HHMMSS'));
     subplot(m,n,1); plot(logt, Est(1:3,:)); hold on; plot(logt, Ref(1:3, :), '--'); hold off;
@@ -90,7 +108,7 @@ elseif figtype == 2
     ytickformat('%.1f');
     % virtual input
     subplot(m,n,5); plot(logt, InnerInput); 
-    xlabel("Time [s]"); ylabel("Inner input");legend("u.1", "u.2", "u.3", "u.4");%キャプション間違ってるかも
+    xlabel("Time [s]"); ylabel("Inner input");legend("z","roll", "pitch", "yaw");%キャプション間違ってるかも
     grid on; xlim([logt(1), logt(end)]); ylim([-inf inf]);
     %↓三宅整備中2/2
     subplot(m,n,6); plot(Est(1,:), Est(2,:)); hold on; plot(Ref(1,:), Ref(2,:), '--'); hold off;
@@ -100,7 +118,7 @@ elseif figtype == 2
 end
 %%
 set(gca,'FontSize',Fontsize);  grid on; title("");
-xlabel("Time [s]");
+%xlabel("Time [s]");%x軸が勝手にTimeになっていたので消した
 
 set(gcf, "WindowState", "maximized");
 set(gcf, "Position", [960 0 960 1000])
