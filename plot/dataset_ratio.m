af500847bcca2ab86cba05e0285f7dee25ba3e47
YoshikiK-tcu <gg1912037@gmail.com>
2024-05-24 17:42:52 +0900

    データセットが適切か論じるためのプログラム

--- plot/dataset_ratio.m
+++ plot/dataset_ratio.m
@@ -1,26 +1,28 @@
 %% Importing data
 % 結合されたデータを読み込む
+clear
 load('Koopman_Linearization\Integration_Dataset\Kiyama_Exp_Dataset.mat');
 X = Data.X;
 Y = Data.Y;
 U = Data.U;
 
 %% データセットがどういう割合のデータなのか算出する
+close all
 T = 1:size(Data.X,2);
 figure(1);
-plot(T, Data.X(1:3,:));
+plot(T, Data.X(1:3,:)); ylabel("Position")
+legend("$$x$$", "$$y$$", "$$z$$", "Interpreter", "latex")
 
-%% データの傾向
-M = median(Data.X(1:3,:),2); % 中央値
-
-%% 正負の数を取得する
+%% 正負に分割　⇒　中央値を求める
 Xplus_idx = find(X(1,:)>=0); Xminus_idx = find(X(1,:)<0);
 Yplus_idx = find(X(2,:)>=0); Yminus_idx = find(X(2,:)<0);
 
-Xplus_median = median(X(1,Xplus_idx));
-Xminus_median = median(X(1,Xminus_idx));
-Yplus_median = median(X(2,Yplus_idx));
-Yminus_median = median(X(2,Yminus_idx));
+Xplus_median = median(X(1,Xplus_idx)); Xminus_median = median(X(1,Xminus_idx));
+Yplus_median = median(X(2,Yplus_idx)); Yminus_median = median(X(2,Yminus_idx));
 
 result_median = [Xplus_median Xminus_median Yplus_median Yminus_median]
+
+%% 座標の総移動距離の比較
+error = Y(1:3,:) - X(1:3,:);
+all_dis = sum(error,2)'
 
