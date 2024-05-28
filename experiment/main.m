1b6a1bd9203e9ddbb4fbd8207ba470e4fa857304
Naoki-Yata <g2112093@tcu.ac.jp>
2024-05-28 13:16:14 +0900

    フラコン通信後・通信の値がおかしい

--- experiment/main.m
+++ experiment/main.m
@@ -10,11 +10,10 @@ initial_state.q = [1;0; 0; 0];
 initial_state.v = [0; 0; 0];
 initial_state.w = [0; 0; 0];
 agent = DRONE;
-agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial", "COM4"));
+agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial", "COM8"));
 % COM：機体番号（ArduinoのCOM番号）
-    pause(0.75)
-    %途中で一時停止しないとArduinoが信号を送信しない，原因不明
-    msg=gen_msg([1000,500,0,1000,0,500,500,1000]); %([Roll,Pitch,Throttle,Yaw,arming])
-    pause(0.75)
-    %途中で一時停止しないとArduinoが信号を送信しない，原因不明
+    pause(1.45)
+    %途中で一時停止しないとArduinoが信号を送信しない，原因:前の文章でCOMを指定しているので，それが読み込めないうちに進むと信号が送れない
+    %pause(1.45)当たりを境に信号が送信できなくなる
+    msg=gen_msg([1000,1000,0,1000,0,0,0,0]); %([Roll,Pitch,Throttle,Yaw,arming])
     agent.plant.connector.sendData(msg)
