1cefb323201048b5629a06cd5d624c026e355677
1912063 <104186106+1912063@users.noreply.github.com>
2024-06-06 18:42:58 +0900

    0606_実験。mex化で飛ぶか未検証。
    プロペラガードの不調により中止

--- sample/InputTransform_Thrust2Throttle_drone_Koopman.m
+++ sample/InputTransform_Thrust2Throttle_drone_Koopman.m
@@ -3,9 +3,10 @@ function u_trans_param = InputTransform_Thrust2Throttle_drone_Koopman(varargin)
     % drone Prop. input
 
     %% transmitter system
+    % gain_tl
     u_trans_param.gain =[600;600;600;20]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
     u_trans_param.th_offset = 340;         % offset 3s[1021] 4s[900]　発掘[926]
-
+    % gain_f
     u_trans_param.gain2 =[1000;1000;1000;1000]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
     u_trans_param.th_offset2 = 350;  
 
