874b0a833d9276359e17514336937a064241cc37
YoshikiK-tcu <gg1912037@gmail.com>
2024-06-07 11:21:20 +0900

    データセットをcellにまとめたやつ

--- Koopman_Linearization/Asset_file/dataset_each_analys.m
+++ Koopman_Linearization/Asset_file/dataset_each_analys.m
@@ -1,37 +1,38 @@
 %% 各データのちらばりを見る
-% Data.HowmanyDataset = 155;
-% loading_filename = 'Exp_0529';
-% Data.X = {};
-% for i = 1:Data.HowmanyDataset
-%     if contains(loading_filename,'.mat')
-%         Dataset = ImportFromExpData_tutorial(loading_filename); %ImportFromExpData_tutorial:データセットをくっつけるための関数
-%     else
-%         if i == 1 %66 ~ 78はコマンドウィンドウから入力するのに必要(クープマン線形化には関係ない)
-%             setting = 1;
-%             Dataset = ImportFromExpData_tutorial(append(loading_filename,'_',num2str(i),'.mat'),setting);
-%             datarange = Dataset.datarange;
-%             range = Dataset.range;
-%             IDX = Dataset.IDX;
-%             phase2 = Dataset.phase2;
-%             vz_z = Dataset.vz_z;
-%             fprintf('\n')
-%         else
-%             setting = 0;
-%             Dataset = ImportFromExpData_tutorial(append(loading_filename,'_',num2str(i),'.mat'),setting,datarange,range,IDX,phase2,vz_z);
-%         end
-%     end
-%     % if i==1
-%     %     Data.X = [Dataset.X];
-%         % Data.U = [Dataset.U];
-%         % Data.Y = [Dataset.Y];        
-%     % else
-%         % Data.X = [Data.X; Dataset.X];
-%         Data.X{1,i} = Dataset.X;
-%         % Data.U = [Data.U, Dataset.U];
-%         % Data.Y = [Data.Y, Dataset.Y];
-%     % end
-%     disp(append('loading data number: ',num2str(i),', now data:',num2str(Dataset.N),', all data: ',num2str(size(Data.X,2))))
-% end
+Data.HowmanyDataset = 170;
+loading_filename = 'Exp_0604_15data_add';
+Data.X = {};
+for i = 1:Data.HowmanyDataset
+    if contains(loading_filename,'.mat')
+        Dataset = ImportFromExpData_tutorial(loading_filename); %ImportFromExpData_tutorial:データセットをくっつけるための関数
+    else
+        if i == 1 %66 ~ 78はコマンドウィンドウから入力するのに必要(クープマン線形化には関係ない)
+            setting = 1;
+            Dataset = ImportFromExpData_tutorial(append(loading_filename,'_',num2str(i),'.mat'),setting);
+            datarange = Dataset.datarange;
+            range = Dataset.range;
+            IDX = Dataset.IDX;
+            phase2 = Dataset.phase2;
+            vz_z = Dataset.vz_z;
+            fprintf('\n')
+        else
+            setting = 0;
+            Dataset = ImportFromExpData_tutorial(append(loading_filename,'_',num2str(i),'.mat'),setting,datarange,range,IDX,phase2,vz_z);
+        end
+    end
+    % if i==1
+    %     Data.X = [Dataset.X];
+        % Data.U = [Dataset.U];
+        % Data.Y = [Dataset.Y];        
+    % else
+        % Data.X = [Data.X; Dataset.X];
+        Data.X{1,i} = Dataset.X;
+        % Data.U = [Data.U, Dataset.U];
+        % Data.Y = [Data.Y, Dataset.Y];
+    % end
+    disp(append('loading data number: ',num2str(i),', now data:',num2str(Dataset.N),', all data: ',num2str(size(Data.X,2))))
+end
+save("Koopman_Linearization\Data_cell_Xdirection_add", "Data", "-mat");
 
 %%
 clear
@@ -51,7 +52,7 @@ close all
 % set(0,'DefaultAxesFontSize',18);
 set(groot,'DefaultTextFontSize', 18);
 variable = {'x', 'y', 'z', 'q_r', 'q_p', 'q_y', 'v_x', 'v_y', 'v_z', 'omega_r', 'omega_p', 'omega_y'};
-
+flg.savefig = 0;
 
 % all x
 % figure(1); sgtitle(strcat('$$', variable(1), '$$'), 'Interpreter','latex', 'FontSize', 20);
@@ -69,7 +70,7 @@ for m = 1:12
     histogram(data.pd(2,:,m),20); title('\sigma')
 
     % save
-    saveas(m, variable{m}, 'png');
+    if flg.savefig; saveas(m, variable{m}, 'png'); end
 end
 
 
