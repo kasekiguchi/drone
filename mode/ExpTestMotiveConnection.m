68f326b95789db7cc5833a6fccd4fab81fd862c2
YoshikiK-tcu <gg1912037@gmail.com>
2024-06-18 18:24:05 +0900

    IP変更

--- mode/ExpTestMotiveConnection.m
+++ mode/ExpTestMotiveConnection.m
@@ -6,7 +6,7 @@ in_prog_func = @(app) [];
 post_func = @(app) [];
 logger = LOGGER(1, size(ts:dt:te, 2), 0, [],[]);
 
-motive = Connector_Natnet('192.168.1.4'); % connect to Motive
+motive = Connector_Natnet('192.168.1.2'); % connect to Motive
 motive.getData([], []); % get data from Motive
 rigid_ids = [1]; % rigid-body number on Motive
 sstate = motive.result.rigid(rigid_ids);
