cdf063030daa37070a99245ff0ccd48b69fd15e9
kemiyake <g2112086@tcu.ac.jp>
2024-06-27 17:17:53 +0900

    guiおちたとき用計測タイム

--- plot/calculate_time_ave_max.m
+++ plot/calculate_time_ave_max.m
@@ -1,9 +1,17 @@
-fads=diff(log.Data.t(1000:3600))
-ave=mean(fads);
+%%
+%正常に保存できた時 
+ave=mean(gui.time.keisoku_Time(4:end));
 
-saidai=max(diff(log.Data.t(1000:3600)));
-[max_value, max_index] = max(diff(log.Data.t(1000:3600)));
-
+saidai=max(gui.time.keisoku_Time(4:end));
+[max_value, max_index] = max(gui.time.keisoku_Time(4:end));
+%%
+% %GUI落ちたとき
+% fads=diff(log.Data.t(1000:3600))
+% ave=mean(fads);
+% 
+% saidai=max(diff(log.Data.t(1000:3600)));
+% [max_value, max_index] = max(diff(log.Data.t(1000:3600)));
+%%
 % 最大値の位置を計算
 [row, col] = ind2sub(size(diff(log.Data.t(4:end))), max_index);
 col=col+3;
