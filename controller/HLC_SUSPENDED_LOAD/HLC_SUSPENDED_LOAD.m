008232cbbe5fff40a184ddaa380288847eea9889
yumamomose <104126005+yumamomose@users.noreply.github.com>
2024-07-12 22:52:16 +0900

    cbf修正

--- controller/HLC_SUSPENDED_LOAD/HLC_SUSPENDED_LOAD.m
+++ controller/HLC_SUSPENDED_LOAD/HLC_SUSPENDED_LOAD.m
@@ -24,7 +24,12 @@ classdef HLC_SUSPENDED_LOAD < handle
             model = obj.self.estimator.result;
             ref = obj.self.reference.result;
             x = [model.state.getq('compact');model.state.w;model.state.pL;model.state.vL;model.state.pT;model.state.wL]; % [q, w ,pL, vL, pT, wL]に並べ替え
-            xq    = [model.state.getq('4');x(4:end)]; % [q, w ,pL, vL, pT, wL]に並べ替え
+            % pT = model.state.pT
+            % wL = model.state.wL
+            % theta = acos(-[0,0,1]*pT)*180/pi
+            % h0=-pT(3)-cos(10*pi/180)
+            % h1 = wL(2)*pT(1)-wL(1)*pT(2)+1*h0
+            % xq    = [model.state.getq('4');x(4:end)]; % [q, w ,pL, vL, pT, wL]に並べ替え
             if isprop(ref.state,'xd')
                 xd = ref.state.xd; % 20次元の目標値に対応するよう
             else
@@ -72,13 +77,36 @@ classdef HLC_SUSPENDED_LOAD < handle
 
             tmp = uf + us;
             % control barrier funciton
-            % fun = @(u_opt) (u_opt - tmp)'*(u_opt - tmp);
-            % [A,b] = obj.conic_cfb(xq,P,[10;1],10*pi/180);%deg
-            % tmp = fmincon(fun,obj.u_opt0,A,b,[],[],[],[],[],obj.fmc_options);
-            % obj.u_opt0 = tmp;
-            obj.result.input = tmp;
-            % obj.result.input = [max(0,min(20,tmp(1)));max(-1,min(1,tmp(2)));max(-1,min(1,tmp(3)));max(-1,min(1,tmp(4)))];
+                fun = @(u_opt) sqrt((u_opt - tmp)'*(u_opt - tmp));
+                a=[100;10];
+                C=11;%deg
+                [A,b] = conic_cfb(x,P,a,C*pi/180);
+                tmp_opt = fmincon(fun,obj.u_opt0,A,b,[],[],[],[],[],obj.fmc_options);
+    
+                % fun = @(u_opt) sqrt((u_opt - tmp)'*(u_opt - tmp));
+                % [A,b] = conic_cfb_eul(x,P,[10;10],2*pi/180)%deg
+                % tmp = fmincon(fun,obj.u_opt0,A,b,[],[],[],[],[],obj.fmc_options);
+    
+                obj.u_opt0 = tmp_opt;
+    
+                % obj.result.input = tmp_opt;
+                tmp = tmp_opt;
+
+            % obj.result.input = tmp;
+            obj.result.input = [max(0,min(20,tmp(1)));max(-1,min(1,tmp(2)));max(-1,min(1,tmp(3)));max(-1,min(1,tmp(4)))];
             result = obj.result;
+
+            %チェック用
+            pT = model.state.pT;
+            % wL = model.state.wL
+            theta = acos(-[0,0,1]*pT)*180/pi
+            % h0=-pT(3)-cos(C*pi/180)
+            % h1 = wL(2)*pT(1)-wL(1)*pT(2)+1*h0
+            [h1,h2]=conic_cfb_h1h2(x,tmp_opt,P,a,C*pi/180)
+            % h1
+            % dh1 = h2 - h1/a(2)
+            % h2
+            
         end
         function show(obj)
             obj.result
