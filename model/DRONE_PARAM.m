2c7c4a0beb1e1f66629eb2c906e03895c0ab2c2f
Funsai <85128064+ACSL-yamak@users.noreply.github.com>
2024-07-17 15:05:09 +0900

    ドローンパラム調整

--- model/DRONE_PARAM.m
+++ model/DRONE_PARAM.m
@@ -47,7 +47,7 @@ classdef DRONE_PARAM < PARAMETER_CLASS
                 % param.jy = 0.03;
                 % param.jz = 0.03;
                 % param.mass = 0.699;
-                param.mass = 0.810;
+                param.mass = 0.730;
                 param.Lx = 0.16;
                 param.Ly = 0.16;
                 param.lx = 0.16/2;%0.05;
