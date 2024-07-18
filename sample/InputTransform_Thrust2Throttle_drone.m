69ba1381ca4b9aec9653fb397bb86addd1302923
YoshikiK-tcu <gg1912037@gmail.com>
2024-07-18 18:09:31 +0900

    新型機調整中

--- sample/InputTransform_Thrust2Throttle_drone.m
+++ sample/InputTransform_Thrust2Throttle_drone.m
@@ -15,7 +15,7 @@ function u_trans_param = InputTransform_Thrust2Throttle_drone(varargin)
 
      %6 cells(EL)バッテリーとの兼ね合いで調整が必要
     u_trans_param.gain =[600;600;600;20];%[600;600;600;20]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
-    u_trans_param.gain =[650;650;650;20];%EL
+    u_trans_param.gain =[700;700;700;20];%EL
     % u_trans_param.th_offset = 230;         % offset 3s[1021] 4s[900]　発掘[926]
     u_trans_param.th_offset = 330;   %iflight用
 end
