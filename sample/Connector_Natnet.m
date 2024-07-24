c754aa6dc1483c22a69fcdaf6ca59062f6f16bf5
Funsai <85128064+ACSL-yamak@users.noreply.github.com>
2024-07-24 19:00:11 +0900

    IP変更

--- sample/Connector_Natnet.m
+++ sample/Connector_Natnet.m
@@ -6,6 +6,7 @@ end
 [~,hostname] = system('hostname');
 hostname = string(strtrim(hostname));
 ClientIP = resolvehost(hostname,"address"); % client ip
-ClientIP = '192.168.1.3';
+% ClientIP = '192.168.1.3';%なぶちゃん
+ClientIP = '192.168.1.6';%粉砕
 motive=NATNET_CONNECTOR("HostIP",HostIP,"ClientIP",ClientIP);
 end
