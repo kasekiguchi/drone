6d7c25261ffb47eb65dcb4c7701e085e8a5598d7
Naoki-Yata <g2112093@tcu.ac.jp>
2024-09-13 17:39:04 +0900

    main.m更新

--- experiment/main.m
+++ experiment/main.m
@@ -11,12 +11,12 @@ initial_state.v = [0; 0; 0];
 initial_state.w = [0; 0; 0];
 agent = DRONE;
 %agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial", "COM22"));
-agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial", "COM8"));
+agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial", "COM23"));
 % COM：機体番号（ArduinoのCOM番号）
-    % pause(1.45)
+    %pause(1.45)
     % % 途中で一時停止しないとArduinoが信号を送信しない，原因:前の文章でCOMを指定しているので，それが読み込めないうちに進むと信号が送れない
     % % pause(1.45) 当たりを境に信号が送信できなくなる
-    % msg=gen_msg([500,500,0,500,0,0,0,0]); %([Roll,Pitch,Throttle,Yaw,arming])
+    %msg=gen_msg([500,500,0,500,0,0,0,0]); %([Roll,Pitch,Throttle,Yaw,arming])
     % msg=gen_msg([0,0,1000,0,1000,1000,1000,1000]);
     % msg=gen_msg([1000,1000,500,1000,500,500,500,500]);
     % msg=gen_msg([500,500,551,500,70,50,0,0]);
@@ -36,12 +36,25 @@ agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial",
     %agent.plant.connector.sendData(msg)
 
 
+pause(1.45)
+    Command = 0;
+    sy = 0;
+    while Command <= 1000000
+        pause(0.005)
+        msg=gen_msg([500,500,551,500,70,50,0,0]); %([Roll,Pitch,Throttle,Yaw,arming])
+        agent.plant.connector.sendData(msg)
+
+        Command = Command + 1;
+
+    end
+    
+
 % pause(1.45)
 %     Command = 0;
 %     sy = 0;
-%     while Command <= 10000
+%     while Command <= 1000
 %         pause(0.005)
-%         msg=gen_msg([1000,1000,500,1000,500,500,500,500]); %([Roll,Pitch,Throttle,Yaw,arming])
+%         msg=gen_msg([Command,Command,Command,Command,Command,Command,Command,Command]); %([Roll,Pitch,Throttle,Yaw,arming])
 %         agent.plant.connector.sendData(msg)
 %         sy = sy +1;
 %         if(sy == 5)
@@ -49,20 +62,6 @@ agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial",
 %         sy = 0;
 %         end
 %     end
-
-pause(1.45)
-    Command = 0;
-    sy = 0;
-    while Command <= 1000
-        pause(0.005)
-        msg=gen_msg([Command,Command,Command,Command,Command,Command,Command,Command]); %([Roll,Pitch,Throttle,Yaw,arming])
-        agent.plant.connector.sendData(msg)
-        sy = sy +1;
-        if(sy == 5)
-        Command = Command + 1;
-        sy = 0;
-        end
-    end
 
 % pause(1.45)
 %     Command = 1000;
