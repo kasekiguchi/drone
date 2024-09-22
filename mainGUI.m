f98794d8ec521fefef7b237e43384d297c38b733
Naoki-Yata <g2112093@tcu.ac.jp>
2024-09-22 14:38:25 +0900

    変更前保存

--- mainGUI.m
+++ mainGUI.m
@@ -1,5 +1,5 @@
 %% Initialize settings
-% set path
+% set path 共通プログラムのパスを通す
 cf = pwd;
 if contains(mfilename('fullpath'),"mainGUI")
   cd(fileparts(mfilename('fullpath')));
@@ -13,9 +13,9 @@ close all hidden; clear ; clc;
 userpath('clear');
 %%
 clc
-SimBaseMode = ["SimHL","SimFHL","SimFHL_Servo","SimLiDAR","SimFT","SimEL","SimVoronoi"];
-ExpBaseMode = ["ExpTestMotiveConnection","ExpHL","ExpFHL","ExpFHL_Servo","ExpFT","ExpEL"];
-fExp = 0;
-fDebug = 0; % 1: active : for debug function
-PInterval = 0.6; % sec : poling interval for emergency stop
-gui = SimExp(fExp,fDebug,PInterval);
+SimBaseMode = ["SimHL","SimFHL","SimFHL_Servo","SimLiDAR","SimFT","SimEL","SimVoronoi"]; %シミュレーションの種類
+ExpBaseMode = ["ExpTestMotiveConnection","ExpHL","ExpFHL","ExpFHL_Servo","ExpFT","ExpEL"]; %実機実験の種類
+fExp = 0; %fEXpとは何？
+fDebug = 0; % 1: active : for debug function　デバッグファンクションとは何？
+PInterval = 0.6; % sec : poling interval for emergency stop　緊急停止の間隔？
+gui = SimExp(fExp,fDebug,PInterval); %上の3つを考慮する　
--- mode/ExpHL.m
+++ mode/ExpHL.m
@@ -1,23 +1,23 @@
-ts = 0; % initial time
-dt = 0.025; % sampling period
-te = 10000; % termina time
-time = TIME(ts,dt,te);
-in_prog_func = @(app) in_prog(app);
-post_func = @(app) post(app);
-logger = LOGGER(1, size(ts:dt:te, 2), 1, [],[]);
+ts = 0; % initial time　開始時間
+dt = 0.025; % sampling period　サンプリング間隔
+te = 10000; % termina time　終了時間
+time = TIME(ts,dt,te); %上の3つの時間をまとめる．
+in_prog_func = @(app) in_prog(app); %43行目にある
+post_func = @(app) post(app); %35行目にある
+logger = LOGGER(1, size(ts:dt:te, 2), 1, [],[]); %データをまとめている？
 
-motive = Connector_Natnet('192.168.1.4'); % connect to Motive
-motive.getData([], []); % get data from Motive
-rigid_ids = [1]; % rigid-body number on Motive
-sstate = motive.result.rigid(rigid_ids);
-initial_state.p = sstate.p;
-initial_state.q = sstate.q;
-initial_state.v = [0; 0; 0];
-initial_state.w = [0; 0; 0];
+motive = Connector_Natnet('192.168.1.4'); % connect to Motive　モーションキャプチャのIP
+motive.getData([], []); % get data from Motive モーションキャプチャからのデータを入手する
+rigid_ids = [1]; % rigid-body number on Motive　モーションキャプチャの剛体番号
+sstate = motive.result.rigid(rigid_ids); %状態の取得？
+initial_state.p = sstate.p; %初期位置の取得
+initial_state.q = sstate.q; %初期角度の取得
+initial_state.v = [0; 0; 0]; %初期速度の取得
+initial_state.w = [0; 0; 0]; %初期角加速度の取得
 
-agent = DRONE;
+agent = DRONE; %対象をドローンにしている？ DRONE.m
 % agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "udp", [1, 253]));
-agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial", "5")); %プロポ有線
+agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial", "5")); %プロポ有線　プロポとの接続
 agent.parameter = DRONE_PARAM("DIATONE");
 agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)), ["p", "q"]));
 agent.sensor = MOTIVE(agent, Sensor_Motive(1,0, motive));
