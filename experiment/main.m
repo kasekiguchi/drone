c41cb2a86ee21bf4fdf5d42f8af56b356578160f
Naoki-Yata <g2112093@tcu.ac.jp>
2024-09-22 12:49:31 +0900

    別MCUプログラム

--- experiment/main.m
+++ experiment/main.m
@@ -13,14 +13,14 @@ agent = DRONE;
 %agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial", "COM22"));
 agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial", "COM13"));
 % COM：機体番号（ArduinoのCOM番号）
-    %pause(1.45)
+    % pause(1.45)
     % % 途中で一時停止しないとArduinoが信号を送信しない，原因:前の文章でCOMを指定しているので，それが読み込めないうちに進むと信号が送れない
     % % pause(1.45) 当たりを境に信号が送信できなくなる
-    %msg=gen_msg([500,500,0,500,0,0,0,0]); %([Roll,Pitch,Throttle,Yaw,arming])
-    % msg=gen_msg([0,0,1000,0,1000,1000,1000,1000]);
-    % msg=gen_msg([1000,1000,500,1000,500,500,500,500]);
-    % msg=gen_msg([500,500,551,500,70,50,0,0]);
-    % msg=gen_msg([500,500,552,500,70,50,0,0]);
+    % % msg=gen_msg([500,500,0,500,0,0,0,0]); %([Roll,Pitch,Throttle,Yaw,arming])
+    % % msg=gen_msg([0,0,1000,0,1000,1000,1000,1000]);
+    % % msg=gen_msg([1000,1000,500,1000,500,500,500,500]);
+    % % msg=gen_msg([500,500,551,500,70,50,0,0]);
+    % % msg=gen_msg([500,500,552,500,70,50,0,0]);
     % % msg=gen_msg([500,500,671,500,0,0,0,0]);
     % % msg=gen_msg([500,500,672,500,0,0,0,0]);
     % % msg=gen_msg([500,500,0,500,671,0,0,0]);
@@ -33,7 +33,7 @@ agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial",
     % % msg=gen_msg([1000,1000,1000,1000,1000,1000,1000,1000]);
     % % msg=gen_msg([0,0,0,0,0,0,0,0]);
     % msg=gen_msg([500,42,42,42,42,42,42,42]);
-    %agent.plant.connector.sendData(msg)
+    % agent.plant.connector.sendData(msg)
 
 
 pause(1.45)
