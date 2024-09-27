f656dcdf9f1ece23ebee37203ebad75fda75dd24
kemiyake <166671387+kemiyake@users.noreply.github.com>
2024-09-27 18:29:58 +0900

    実験結果、位置を下げるのは期待できる

--- sample/InputTransform_Thrust2Throttle_drone.m
+++ sample/InputTransform_Thrust2Throttle_drone.m
@@ -4,11 +4,11 @@ function u_trans_param = InputTransform_Thrust2Throttle_drone(varargin)
 
     %% transmitter system
     % u_trans_param.gain_tl =[600;600;600;20]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
-     u_trans_param.gain_tl =[100;100;100;20];
+     u_trans_param.gain_tl =[150;150;150;20];%good100.100.100.20
     % u_trans_param.th_offset_tl = 245;         % offset 3s[1021] 4s[900]　発掘[926]
      u_trans_param.th_offset_tl = 346;%252  %ここ変えるとテークオフとランディング中の釣り合うスロットル変わる。
     % u_trans_param.gain_f =[600;600;600;20];  
-     u_trans_param.gain_f =[150;150;100;20];
+     u_trans_param.gain_f =[155;155;155;20];%good150.150.100.20
     % u_trans_param.th_offset_f = 260;%牽引物込み
     u_trans_param.th_offset_f = 330;%330;%牽引物込み267 フライトしたときに326+20で346ぐらいの値になる+20ぐらいされる理由は謎。
 
