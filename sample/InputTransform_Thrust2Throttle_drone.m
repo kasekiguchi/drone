c13194f5152ee9c45d6f3929eaa6445a3789652d
YoshikiK-tcu <gg1912037@gmail.com>
2024-09-30 17:39:11 +0900

    6cell_HLゲイン調整

--- sample/InputTransform_Thrust2Throttle_drone.m
+++ sample/InputTransform_Thrust2Throttle_drone.m
@@ -8,8 +8,13 @@ function u_trans_param = InputTransform_Thrust2Throttle_drone(varargin)
     % u_trans_param.th_offset = 350;    % offset 3s[1021] 4s[900]　発掘[926]
 
     %iflight
-    u_trans_param.gain =[650;650;650;20]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
-    u_trans_param.th_offset = 450; 
+    % 4cell
+    % u_trans_param.gain =[650;650;650;20]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
+    % u_trans_param.th_offset = 450; 
+
+    % 6cell
+    u_trans_param.gain =[700;700;700;20]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
+    u_trans_param.th_offset = 330; 
 
     u_trans_param.gain2 =u_trans_param.gain;
     u_trans_param.th_offset2 = u_trans_param.th_offset;
