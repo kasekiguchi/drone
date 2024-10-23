4af601a17c08290685995c9b9e833079d7c4b50e
YoshikiK-tcu <gg1912037@gmail.com>
2024-10-23 12:52:57 +0900

    update

--- sample/Controller_MPC_Koopman.m
+++ sample/Controller_MPC_Koopman.m
@@ -106,7 +106,10 @@ function Controller = Controller_MPC_Koopman(dt, model, agent)
     fprintf("Koopman MPC controller\n")
 
     Controller_param.ref_input = Controller_param.input.u; %入力の目標値
+
+    %% 誤差モデル
     Controller_param.ref_input = [0; 0; 0; 0]; % 誤差モデル
+    % Controller_param.weight.R = diag([0.1; 0.1; 0.1; 0.1]);     % 入力
 
     Controller.name = "mpc";
     Controller.type = "MPC_CONTROLLER_KOOPMAN_quadprog_simulation";
