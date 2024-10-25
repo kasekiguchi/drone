de8b6561014a264763c7124aa8a6d14bf4b855d0
Naoki-Yata <g2112093@tcu.ac.jp>
2024-10-25 16:40:59 +0900

    無駄なの消した

--- model/sample/Controller_TrackingMPC.m
+++
@@ -1,6 +0,0 @@
-function Controller= Controller_TrackingMPC(dt)
-%% controller class: construct
-% controller property をController classのインスタンス配列として定義
-Controller.dt = dt;
-Controller.H = 5;
-end
--- model/sample/Reference_Time_Varying_With_Load.m
+++
@@ -1,8 +0,0 @@
-function Reference_Time_Varying_With_Load(agent,f_name,param)
-%% reference class demo
-% reference property をReference classのインスタンス配列として定義
-clear Reference
-Reference.type=["TimeVaryingReferenceE"];
-Reference.name=["timeVarying"];
-Reference.param={{f_name,param,"HL"}};
-end
