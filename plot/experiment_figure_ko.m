afe3a3af0ab4114f0534d0427b1a3582e8d219e2
YoshikiK-tcu <gg1912037@gmail.com>
2024-11-07 21:34:03 +0900

    重心位置修正したらいい結果得られた

--- plot/experiment_figure_ko.m
+++ plot/experiment_figure_ko.m
@@ -10,7 +10,7 @@ set(0,'defaultTextFontsize',16);
 set(0,'defaultLineLineWidth',1.5);
 set(0,'defaultLineMarkerSize',15);
 
-load("sl1031nabu_en_movie2_Log(31-Oct-2024_18_22_46).mat");
+load("sl1107len_en_juusinn_syuuseigo_Log(07-Nov-2024_21_28_54).mat");
 
 % load("hl0729_rig3_miyatipc_no-sindou_Log(29-Jul-2024_18_41_45).mat");%
 % load("sl1004hun_diag([200000,10000,100,10,10,10]),[0.005]_Log(04-Oct-2024_17_04_52).mat");
@@ -19,7 +19,7 @@ load("sl1031nabu_en_movie2_Log(31-Oct-2024_18_22_46).mat");
 %log = logger;%永久用（↓とどっちかをコメントアウト）
 % log =gui.logger.Data;%gui用
 %%
-figtype = 1;%1でグラフを1タブづつ，2で1タブにグラフを多数．
+figtype = 2;%1でグラフを1タブづつ，2で1タブにグラフを多数．
 Agent = log.Data.agent;
 
 % arming_start_idx = find(log.Data.phase==102, 1, 'first');%フライト開始からのグラフにできる．↓と切り替え
