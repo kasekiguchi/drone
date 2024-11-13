a8a5c640903e9a66854f7a96dc231de612fd7184
YoshikiK-tcu <gg1912037@gmail.com>
2024-11-13 14:30:34 +0900

    commit 1コンKMPC

--- sample/Connector_Natnet.m
+++ sample/Connector_Natnet.m
@@ -3,9 +3,14 @@ function motive = Connector_Natnet(HostIP)
 arguments
   HostIP % motive server IP
 end
-[~,hostname] = system('hostname');
-hostname = string(strtrim(hostname));
-% ClientIP = resolvehost(hostname,"address"); % client ip
-ClientIP = '192.168.1.3'; % 実験用PC
+% [~,hostname] = system('hostname');
+% hostname = string(strtrim(hostname));
+% ClientIP = resolvehost(hostname,"address"); % client ip 1系統以外もあると誤認識が起こる
+% ClientIP = '192.168.1.3'; % 実験用PC
+%% ipconfigから取得
+[~, cmdout] = system('ipconfig');
+ipidx = strfind(cmdout, '192.168.1.');
+ClientIP = cmdout(ipidx:ipidx+10);
+%%
 motive=NATNET_CONNECTOR("HostIP",HostIP,"ClientIP",ClientIP);%ClientIP:実験用PC,HostIP:MotivePC
 end
