2344e711a47c27f9bed328d3226984ea57a55526
YoshikiK-tcu <gg1912037@gmail.com>
2024-11-15 12:58:59 +0900

    オフセット、データセットなどいじった

--- model/DRONE_PARAM.m
+++ model/DRONE_PARAM.m
@@ -40,10 +40,10 @@ classdef DRONE_PARAM < PARAMETER_CLASS
                 type = "row";
                 % param.mass = 0.540; %KMPC下限 sim30s
                 % param.mass = 0.5884;
-                 param.mass = 0.540;
+                % param.mass = 0.540;
                 % param.mass = 0.669; %KMPC上限 sim30s
                 % param.mass = 0.640; %2024/1/15計量、機体+battery
-                % param.mass = 0.730; %iflight+6cell
+                 param.mass = 0.730; %iflight+6cell
                 % param.Lx = 0.16;
                 % param.Ly = 0.16;
                 % param.lx = 0.16/2;%0.05;
--- sample/Controller_MPC_Koopman.m
+++ sample/Controller_MPC_Koopman.m
@@ -3,10 +3,10 @@ function Controller = Controller_MPC_Koopman(~) %%#codegen
 %   各種値
    % Controller_param.m = 0.540; %下限sim30s
    % Controller_param.m = 0.5884; %ドローンの質量、質量は統一 eachine+4cell
-    Controller_param.m = 0.540;
+   % Controller_param.m = 0.540;
    % Controller_param.m = 0.669; %上限sim30s
    % Controller_param.m = 0.640;
-   % Controller_param.m = 0.730; %ドローンの質量、質量は統一 iflight+6cell
+    Controller_param.m = 0.730; %ドローンの質量、質量は統一 iflight+6cell
     Controller_param.dt = 0.07; % MPCステップ幅
     Controller_param.H = 10; %ホライズン数
     Controller_param.state_size = 12;
@@ -15,8 +15,8 @@ function Controller = Controller_MPC_Koopman(~) %%#codegen
 
     %% Koopman
     % modeファイルとファイル名をそろえる
-     load("EstimationResult_12state_2_7_Exp_sprine+zsprine+P2Pz_torque_incon_150data_vzからz算出.mat",'est') %vzから算出したzで学習、総推力
-  %   load("20241110_iflight_randam100_z50_est.mat",'est') %vzから算出したzで学習、総推力
+     % load("EstimationResult_12state_2_7_Exp_sprine+zsprine+P2Pz_torque_incon_150data_vzからz算出.mat",'est') %vzから算出したzで学習、総推力
+     load("20241110_iflight_randam100_z50_est.mat",'est') %vzから算出したzで学習、総推力
 %    load("EstimationResult_2024-05-03_Exp_Kiyama_code03_2.mat", "est");
 %    load("EstimationResult_2024-06-10_Exp_Kiyama_code03_2.mat", "est");
 %    load("EstimationResult_2024-06-10_code02_Exp_Kiyama_code03_2.mat", "est");
