d097d7bb96e6617ff9b83dc1c314cd07a498737e
YoshikiK-tcu <gg1912037@gmail.com>
2024-11-19 13:47:19 +0900

    ゲイン調整大体終わり

--- sample/InputTransform_Thrust2Throttle_drone_KMPC.m
+++ sample/InputTransform_Thrust2Throttle_drone_KMPC.m
@@ -11,8 +11,9 @@ function u_trans_param = InputTransform_Thrust2Throttle_drone_KMPC(varargin)
     % u_trans_param.th_offset2 = 332.5; %hov,p2p332.5
 
     % iflight 6cell
-    u_trans_param.gain2 =[500;500;500;100]; %hov,p2p[800,800,800,500]
-    u_trans_param.th_offset2 = 340.2; %hov,p2p332.5
+    u_trans_param.gain2 =[1000;1000;1000;100]; %hov,p2p[800,800,800,500]
+   % u_trans_param.th_offset2 = 335; %hov,olddata,[1000,1000,1000,100]のとき
+    u_trans_param.th_offset2 = 335;
 
     % u_trans_param.gain_SuspendedLoad =[500;500;500;100]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
     % u_trans_param.th_offset_SuspendedLoad = 450;         % offset 3s[1021] 4s[900]　発掘[926]
