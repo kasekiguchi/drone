481bb3c72525c3284a69a9f00f88acb42882947e
YoshikiK-tcu <gg1912037@gmail.com>
2024-11-26 18:48:34 +0900

    牽引物位置のセンサ値の反映割合を変化

--- estimator/FOR_LOAD.m
+++ estimator/FOR_LOAD.m
@@ -5,6 +5,10 @@ classdef FOR_LOAD < SENSOR_CLASS
         rigid_num
         self
         fpLXY=0;
+        tf0
+        tl0
+        te = 10;%ÉZÉìÉTÅ[ílÇâΩïbÇ≈100%égÇ§Ç©
+        rate = 1/te^2;%ìÒéüä÷êîÇ≈0-1ÇÃä‘Ç≈ïœâªÇ∑ÇÈ
     end
     
     methods
@@ -24,19 +28,42 @@ classdef FOR_LOAD < SENSOR_CLASS
         
         function [result]=do(obj,varargin)
             %   param : optional
-            obj.result.state.p = obj.self.sensor.motive.result.state.p;
-            obj.result.state.q = obj.self.sensor.motive.result.state.q;
-            obj.result.state.pL = obj.result.state.p -[0;0;obj.self.parameter.get("cableL")];% For:PE-Model
-
+            sp = obj.self.sensor.motive.result.state.p;
+            sq = obj.self.sensor.motive.result.state.q;
             spL=obj.self.sensor.motive.result.rigid(obj.rigid_num).p;
+            ipL = sp -[0;0;obj.self.parameter.get("cableL")];% For:PE-Model
             if strcmp(varargin{1}{2},'f')%obj.result.state.pL(3) >= 0.2&&(cha,'f')||strcmp(cha,'l')
                 obj.result.state.pL = spL;
-            elseif strcmp(varargin{1}{2},'t')&&spL(3)>0.35&&(norm(spL(1:2) - obj.result.state.p(1:2))<0.01||obj.fpLXY==1)
-            % elseif strcmp(varargin{1}{2},'t')&&(norm(spL(1:2) - obj.result.state.p(1:2))<0.01||obj.fpLXY==1)
-                obj.result.state.pL(1:2) = spL(1:2);
-                obj.fpLXY=1;
+                obj.tf0=[];
+                obj.tl0=[];
+            % elseif strcmp(varargin{1}{2},'t')&&spL(3)>0.35&&(norm(spL(1:2) - obj.result.state.p(1:2))<0.01||obj.fpLXY==1)
+            % % elseif strcmp(varargin{1}{2},'t')&&(norm(spL(1:2) - obj.result.state.p(1:2))<0.01||obj.fpLXY==1)
+            %     obj.result.state.pL(1:2) = spL(1:2);
+            %     obj.fpLXY=1;
+            elseif strcmp(varargin{1}{2},'t')&&ipL(3)>0.1%take off
+                if isempty(obj.tf0)
+                    obj.tf0 = varargin{1}{1}.t;
+                end
+                t = min((varargin{1}{1}.t - obj.tf0),obj.te);
+                k = obj.rate*t^2;%îΩâfäÑçá
+                spL(1:2) = sp(1:2) + k*(spL(1:2) - sp(1:2));
+                % spL = spL + min(k,0)*(spL - sp);
+            elseif strcmp(varargin{1}{2},'l')&&ipL(3)>0.1%landing
+                if isempty(obj.tl0)
+                    obj.tl0 = varargin{1}{1}.t;
+                end
+                t = min(varargin{1}{1}.t - obj.tl0, obj.te);
+                k = obj.rate*(t - obj.te)^2;%îΩâfäÑçá
+                spL = spL + k*(spL - sp);
+            else
+                spL = ipL;% For:PE-Model
+                obj.tf0=[];
+                obj.tl0=[];
             end
-            obj.result.state.pT = (obj.result.state.pL-obj.result.state.p)/norm(obj.result.state.pL-obj.result.state.p);% For:PE-Model
+            obj.result.state.p = sp;
+            obj.result.state.q = sq;
+            obj.result.state.pL = spL;
+            obj.result.state.pT = (spL-sp)/norm(spL-sp);
             result = obj.result;
         end
         function show()
