c2a36c06919b87c345d77d036fb7213115927644
YoshikiK-tcu <gg1912037@gmail.com>
2024-12-04 12:14:06 +0900

    EKF

--- sample/Estimator_EKF.m
+++ sample/Estimator_EKF.m
@@ -64,8 +64,9 @@ function Estimator = Estimator_EKF(agent,dt,model,output,opts)
 
     if strcmp(Estimator.model.name,"Expand")
         % Estimator.Q = blkdiag(eye(3)*1E-3, eye(3)*1E-3,eye(2)*1E-3); % システムノイズ（Modelクラス由来）
-        Estimator.Q = blkdiag(eye(3)*1E-3, eye(3)*1E-3,eye(2)*1E-3); % システムノイズ（Modelクラス由来）
-        Estimator.B = blkdiag([0.5*dt^2*eye(6);dt*eye(6)],dt^2*eye(2));
+        Estimator.Q = blkdiag(2*eye(3)*1E-1, 2*eye(3)*1E-1,eye(2)*1E-1); % システムノイズ（Modelクラス由来）
+       % Estimator.B = blkdiag([0.5*dt^2*eye(6);dt*eye(6)],dt^2*eye(2));
+        Estimator.B = blkdiag([0.5*dt^2*eye(6);dt*eye(6)],dt*eye(2)*0);
     end
     if contains(Estimator.model.name,"cable_suspended_rigid_body")
       N = length(Estimator.model.state.Oi)/3;
