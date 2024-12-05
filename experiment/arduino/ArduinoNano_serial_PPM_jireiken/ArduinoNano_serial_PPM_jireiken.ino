1af0ccb549bfc27c982252044d41b71da1d4d330
Naoki-Yata <g2112093@tcu.ac.jp>
2024-12-05 18:01:17 +0900

    事例研究無駄なコメント削除

--- experiment/arduino/ArduinoNano_serial_PPM_jireiken/ArduinoNano_serial_PPM_jireiken.ino
+++ experiment/arduino/ArduinoNano_serial_PPM_jireiken/ArduinoNano_serial_PPM_jireiken.ino
@@ -4,162 +4,98 @@
 // PPM 1周期は start_H + TOTAL_CH_H + TIME_LOW からなる
 #include <TimerOne.h>
 
-uint8_t i; //符号なし8bit整数型(0~255)のi
-// [ Green Red ] : HIGHで消灯、LOWで点滅
-// 飛行可能（初期状態）： [ LOW HIGH ]
-// Arming           :  [ LOW LOW  ]
-// Emergence        :  [ HIGH LOW ]
-volatile boolean isEmergency = false; //volatile:変数をレジスタではなくRAMからロードするよう,コンパイラに指示(割り込み関係のコードが関係)　変数isEmergencyにfalseを格納
-boolean fReset = false; //変数boolean fResetにfalseを格納
-boolean fInitial = true; //変数boolean fInitialにtrueを格納
-boolean fstarthalf = false; //変数boolean fInitialにtrueを格納
+uint8_t i;
+volatile boolean isEmergency = false;
+boolean fReset = false;
+boolean fInitial = true;
+boolean fstarthalf = false;
 /////////////////// PPM関係 ////////////////////
-#define OUTPUT_PIN 2 // ppm output pin　2ピン(D2)をOUTPUT_PINと定義　このピンからPPM信号を出力する
-char packetBuffer[255]; //char:符号付きの型(signed)で,-128から127までの数値として扱われる　受信したデータの格納場所packetBufferを定義している　[255]は一度に受信できる最大のバッファサイズ
-#define TOTAL_INPUT 4 // number of input channels　使用されていないため考えなくてよい
-#define TOTAL_CH 8    // number of channels 1フレームにおけるチャンネルの数を8と定義
+#define OUTPUT_PIN 2
+char packetBuffer[255];
+#define TOTAL_INPUT 4 // number of input channels
+#define TOTAL_CH 8    // number of channels
 // https://create-it-myself.com/research/study-ppm-spec/
 // PPM信号の周期  [us] = 22.5 [ms] // オシロスコープでプロポ信号を計測した結果：上のリンク情報とも合致
 #define PPM_PERIOD 22500 // PPMの周期判定はHIGHの時間が一定時間続いたら新しい周期の始まりと認知すると予想できるので、22.5より多少短くても問題無い＝＞これにより信号が安定した
-#define TIME_LOW 360     // PPM信号 LOWパルス幅 [us]
-#define CH_MIN 0         // PPM幅の最小 [us] MATLABから送信される信号の最小を定義　オフセットにも関係
-#define CH_NEUTRAL 500   // PPM幅の中間 [us] MATLABから送信される信号の中間を定義　オフセットにも関係
-#define CH_MAX 1000      // PPM幅の最大 [us] MATLABから送信される信号の最大を定義　オフセットにも関係
-// PPM Channelの基本構造
-// TIME_LOW + CH_OFFSET = 2000 = 2 [ms]　
-// TIME_LOW + CH_MAX + CH_OFFSET = 1000 = 1 [ms]　計算が合わないこの計算式では3[ms]となるはずである．　400 + 1000 + 1620 = 3020　もしくは，CH_MAXが-となっていることが正しいと考えられる
-// volatile uint16_t CH_OFFSET; // 共通オフセット値 2*CH_MAX - TIME_LOW + 20 = 2000 - 400 + 20 = 1620 ★オフセットとは周波数オフセットを表しているのか確認
-// #define CH_OFFSET 1620       // transmitterシステムでは20が必要　★オフセットには20が追加されているが何故必要であるのかが不明
-// #define TOTAL_CH_OFFSET 12960 //  8*CH_OFFSET 1フレーム分の合計オフセットを定義
-#define CH_OFFSET 1659       // transmitterシステムでは20が必要　★オフセットには20が追加されているが何故必要であるのかが不明
-#define TOTAL_CH_OFFSET 13272 //  8*CH_OFFSET 1フレーム分の合計オフセットを定義
-//（特にroll入力が他の値が増加することで必要なoffset値が一度変化するので、AUX5をMAX値にしておくことで変化した後の値で一定にした。）
-// CH1の値が他のCHの値に比べて不安定なのは上記の動作が原因だと考えられる　transmitterのプログラムで行っている可能性あり AUX5の意味が不明AUX4までしか定義されていないはずである
-// volatile uint16_t TOTAL_CH_OFFSET = 0; // CH_OFFSETの合計
-volatile uint8_t n_ch = TOTAL_CH; // 現在の chを保存　符号なし8bit整数型(0~255)n_chに1フレームにおけるチャンネルの数8を代入
-volatile uint16_t t_sum = 0;      // us単位  1周期中の現在の使用時間　16bit整数型(-32768~32767)t_sumに1周期の始まりである0を代入
-volatile uint16_t pw[TOTAL_CH];   // ch毎のパルス幅を保存　16bit整数型(-32768~32767)pwはch毎のパルス幅を記録して，その上限は8である
-volatile uint16_t phw[TOTAL_CH];  // PPM周期を保つため、Pulse_control内のみで使用
-volatile boolean isReceive_Data_Updated = false; //boolean isReceive_Data_Updatedにfalseを代入
-volatile uint16_t start_H = PPM_PERIOD; //16bit整数型(-32768~32767)start_HにPPM_PERIOD(22500)を代入 スタート時のパルス幅
-volatile uint16_t start_Hh = PPM_PERIOD; //16bit整数型(-32768~32767)start_HhにPPM_PERIOD(22500)を代入
-volatile uint16_t REMAINING_W; //16bit整数型(-32768~32767)REMAINING_Wを定義
-volatile uint16_t plus = 0;
+#define TIME_LOW 360    
+#define CH_MIN 0         
+#define CH_NEUTRAL 500  
+#define CH_MAX 1000      
+#define CH_OFFSET 1659    
+#define TOTAL_CH_OFFSET 13272 //  8*CH_OFFSET 
+volatile uint8_t n_ch = TOTAL_CH; 
+volatile uint16_t t_sum = 0;     
+volatile uint16_t pw[TOTAL_CH];
+volatile uint16_t phw[TOTAL_CH];
+volatile boolean isReceive_Data_Updated = false; 
+volatile uint16_t start_H = PPM_PERIOD; 
+volatile uint16_t start_Hh = PPM_PERIOD; 
+volatile uint16_t REMAINING_W; 
 //////////// シリアル通信が途絶えたとき用 ////////////////////////////////
 volatile unsigned long last_received_time;
 
 // ==================================================================
 void setup()
 {
-  Serial.begin(115200); // MATLABの設定と合わせる　パソコンとマイコンの通信速度を合わせている
-  // Serial.setTimeout(10); //
-  Serial.println("Start"); //シリアル通信でメッセージ(Start)をPCに送信
-  setupPPM(); // ppm 出力開始
+  Serial.begin(115200); // MATLABの設定と合わせる
+  Serial.println("Start"); 
+  setupPPM();
 
   // 緊急停止
-  while (Serial.available() <= 0) //受信データを受け取っていない時繰り返す　繰り返す中身がないため何もしない．
+  while (Serial.available() <= 0) 
   {
   }
-  last_received_time = micros(); //micros():Arduinoボードがプログラムの実行を開始した時から現在までの時間をマイクロ秒単位で返す約70分で0に戻る　最後に受信されたメッセージが読み取られた時刻をast_received_timeに格納
+  last_received_time = micros();
 }
 
 void loop()
 {
-  //receive_serial(); //ここは半透明となっているため動かない　信号を受信した場合
-    
-      receive_serial();
-    
+  receive_serial(); //ここは半透明となっているため動かない　信号を受信した場合
 }
 //*********** local functions  *************************//
-void receive_serial() // ---------- loop function : receive signal by UDP 信号を受信したら実行
+void receive_serial() // ---------- loop function : receive signal
 {
   // ch : 0 - 1000 is converted to 1000 - 2000 throttle on FC
-  if (Serial.available() > 0) //信号を受信したら実行する
+  if (Serial.available() > 0)
   {
-    last_received_time = micros(); //最後に信号を受け取った時間を更新している
-    Serial.println("received"); //シリアル通信でメッセージ(received)をPCに送信 信号が受信できたことを通知
-    Serial.readBytes(packetBuffer, 2 * TOTAL_CH); //2 * TOTAL_CH(8) = 16バイトの数値をシリアル通信で受信してpacketBufferに保存
-    Serial.println(micros() - last_received_time); //シリアル通信でメッセージをPCに送信　現在時間から最後に信号を受け取った時間を引いている
-    if (packetBuffer) //もしpacketBufferならとはどういう意味？
+    last_received_time = micros();
+    Serial.println("received"); 
+    Serial.readBytes(packetBuffer, 2 * TOTAL_CH);
+    Serial.println(micros() - last_received_time);
+    if (packetBuffer)
     {
-      REMAINING_W = PPM_PERIOD; //REMAINING_WにPPM_PERIOD(22500)を代入
-      for (i = 0; i < TOTAL_CH; i++) //i=0から始めて，i<8まで1ずつ加算して繰り返す　恐らく8チャンネル繰り返せるようにしている
+      REMAINING_W = PPM_PERIOD;
+      for (i = 0; i < TOTAL_CH; i++)
       {
-        pw[i] = uint16_t(packetBuffer[i]) * 100 + uint16_t(packetBuffer[i + TOTAL_CH]); //16bit整数型(-32768~32767)の受信データ * 100 + 16bit整数型(-32768~32767)の受信データ[i+8] 全部で16バイトあるため
-        // if (i == 0) //チャンネルが始まっていないとき(Start)
-        // {
-        //   pw[0] = pw[0] +8; // pw = ? + 5 
-        // }
-        if (pw[i] < CH_MIN) //受け取った信号が0未満の時
+        pw[i] = uint16_t(packetBuffer[i]) * 100 + uint16_t(packetBuffer[i + TOTAL_CH]);
+        if (pw[i] < CH_MIN) 
         {
-          pw[i] = CH_MIN; // pw = 0
+          pw[i] = CH_MIN; 
         }
-        else if (pw[i] > CH_MAX) //1000より大きいとき
+        else if (pw[i] > CH_MAX)
         {
-          pw[i] = CH_MAX; // pw = 1000
+          pw[i] = CH_MAX;
         }
-        pw[i] = CH_OFFSET - pw[i]; // transmitter システムの場合必要 1620-pw 1620 - pw ここでは信号が上下限を超えた時を決定している
-        REMAINING_W -= pw[i]; //REMAINING_W - pw[i] 1フレームの内現在の残りから，使用したパルス幅を引いている
-        
-
-        /*
-    if (i == 4)
-    {
-      if(pw[i] < CH_OFFSET - CH_NEUTRAL){// arming 時
-        if (fInitial == true){
-        //Serial.println("Deactivate arming");
-        digitalWrite( GLED_PIN, HIGH );
-        digitalWrite( RLED_PIN, LOW );
-        }else{
-        //Serial.println("Arming");
-        digitalWrite( GLED_PIN, LOW );
-        digitalWrite( RLED_PIN, LOW );
-        }
-        else
-        {
-          Serial.println("Arming");
-          digitalWrite(GLED_PIN, LOW);
-          digitalWrite(RLED_PIN, LOW);
-        }
-      }
-      else
-      {
-        if (fInitial == true)
-        {
-          fInitial = false;
-        }
-        //Serial.println("Ready");
-        digitalWrite( GLED_PIN, LOW );
-        digitalWrite( RLED_PIN, HIGH );
-      }
-    }
-*/
+        pw[i] = CH_OFFSET - pw[i];
+        REMAINING_W -= pw[i];
       }
       last_received_time = micros();
-      isReceive_Data_Updated = true; //isReceive_Data_Updatedにtrueを代入
-      // if (pw[0] + pw[1] + pw[2] + pw[3] + pw[4] + pw[5] + pw[6] + pw[7] <= 11068)
-      //   {
-      //     pw[0] = pw[0] - 8;
-      //   }
-      start_H = REMAINING_W - 9 * TIME_LOW;// 9 times LOW time in each PPM period 1フレームから8つのHigh幅を引いた残り - 1フレーム分のLowパルス幅 = Start時のパルス幅
-      //start_H = REMAINING_W - ( pw[0] + pw[1] + pw[2] + pw[3] + pw[4] + pw[5] + pw[6] + pw[7] ) - 9 * TIME_LOW;
-      Serial.println(micros() - last_received_time); //最後に信号を受け取ってからどれくらい進行したか
+      isReceive_Data_Updated = true;
+      start_H = REMAINING_W - 9 * TIME_LOW;// 9 times LOW time in each PPM period
+      Serial.println(micros() - last_received_time);
     }
   }
-  else if (micros() - last_received_time >= 200000) // Stop propellers after 0.5s signal lost. 0.2s信号が送られてこなかったら実行する 停止状態となる信号を送信するためのもの
+  else if (micros() - last_received_time >= 200000) // Stop propellers after 0.2s signal lost.
   {
-    pw[0] = CH_OFFSET - CH_NEUTRAL; // roll 1620 - 500 =1120
-    pw[1] = CH_OFFSET - CH_NEUTRAL; // pitch 1620 - 500 =1120
-    pw[2] = CH_OFFSET - CH_MIN;     // throttle 1620 - 0 =1620
-    pw[3] = CH_OFFSET - CH_NEUTRAL; // yaw 1620 - 500 =1120
-    pw[4] = CH_OFFSET;              // AUX1 1620
-    pw[5] = CH_OFFSET;              // AUX2 1620
-    pw[6] = CH_OFFSET;              // AUX3 1620
-    pw[7] = CH_OFFSET;              // AUX4 1620
-    start_H = PPM_PERIOD - (TOTAL_CH_OFFSET - 3 * CH_NEUTRAL - CH_MIN) - 9 * TIME_LOW; // 22500 - (12960 - 3 * 500 - 0) - 9 * 400 = 7440 Startのパルス幅
-    //start_H = PPM_PERIOD - (( pw[0] + pw[1] + pw[2] + pw[3] + pw[4] + pw[5] + pw[6] + pw[7] + pw[8])) - 9 * TIME_LOW; // 22500 - (12960 - 3 * 500 - 0) - 9 * 400 = 7440 Startのパルス幅
-    //        digitalWrite( GLED_PIN, HIGH );
-    // digitalWrite( RLED_PIN, LOW );
+    pw[0] = CH_OFFSET - CH_NEUTRAL; // roll
+    pw[1] = CH_OFFSET - CH_NEUTRAL; // pitch
+    pw[2] = CH_OFFSET - CH_MIN;     // throttle
+    pw[3] = CH_OFFSET - CH_NEUTRAL; // yaw
+    pw[4] = CH_OFFSET;              // AUX1
+    pw[5] = CH_OFFSET;              // AUX2
+    pw[6] = CH_OFFSET;              // AUX3
+    pw[7] = CH_OFFSET;              // AUX4
+    start_H = PPM_PERIOD - (TOTAL_CH_OFFSET - 3 * CH_NEUTRAL - CH_MIN) - 9 * TIME_LOW;
   }
 }
 
@@ -168,52 +104,49 @@ void Pulse_control() //★パルスの制御
   if(fstarthalf == true)
   {
     fstarthalf = false;
-    Timer1.setPeriod(start_Hh);     // start 判定の H 時間待つ Start時のパルス幅分次の操作を行う
-    digitalWrite(OUTPUT_PIN, HIGH); // PPM -> HIGH 2ピンから出力されている出力を5Vにする
+    Timer1.setPeriod(start_Hh);
+    digitalWrite(OUTPUT_PIN, HIGH);
   }
-  else if (digitalRead(OUTPUT_PIN) == HIGH) //D2ピンから出力されている出力が5Vの時実行 Lowパルスの制御
+  else if (digitalRead(OUTPUT_PIN) == HIGH)
   {
-    Timer1.setPeriod(TIME_LOW);    // 次の割込み時間を指定　Timer1.setPeriod:ライブラリが初期化された後に新しい期間を設定 次の操作を400us行う
-    digitalWrite(OUTPUT_PIN, LOW); // PPM -> LOW　D2ピンからの出力を0にする
+    Timer1.setPeriod(TIME_LOW);
+    digitalWrite(OUTPUT_PIN, LOW);
   }
-  else if (n_ch == TOTAL_CH) //2ピンの出力が5Vでなく，n_chが8に等しいとき(1フレームが終了した時)
+  else if (n_ch == TOTAL_CH)
   {
-    n_ch = 0; //n_chを0に戻す
-    start_Hh = start_H / 2; //スタート時のパルス幅をstart_Hhに代入
-    //    memcpy(phw, pw, sizeof(pw));// PPM 1周期を22.5 msに保つため、途中で変更されたものには対応しない
-    for (i = 0; i < TOTAL_CH; i++) // PPM 1周期を22.5 msに保つため、途中で変更されたものには対応しない i = 0からi < 8 が成り立つ間iを1ずつ増やして繰り返す
+    n_ch = 0;
+    start_Hh = start_H / 2;
+    for (i = 0; i < TOTAL_CH; i++)
     {
-      phw[i] = pw[i]; //チャンネルiのHighパルス幅をphw[i]に代入
+      phw[i] = pw[i];
     }
     fstarthalf = true;
-    Timer1.setPeriod(start_Hh);     // start 判定の H 時間待つ Start時のパルス幅分次の操作を行う
-    digitalWrite(OUTPUT_PIN, HIGH); // PPM -> HIGH D2ピンから出力されている出力を5Vにする
+    Timer1.setPeriod(start_Hh); 
+    digitalWrite(OUTPUT_PIN, HIGH);
   }
   else //上記2つのどちらでもないとき
   {
-    Timer1.setPeriod(phw[n_ch]);    // 時間を指定 Highパルス幅分次の操作を実行する
-    digitalWrite(OUTPUT_PIN, HIGH); // PPM -> HIGH D2ピンから出力されている出力を5Vにする
-    n_ch++; //チャンネルを進める
+    Timer1.setPeriod(phw[n_ch]); 
+    digitalWrite(OUTPUT_PIN, HIGH);
+    n_ch++;
   }
 }
 
-void setupPPM() // ---------- setup ppm signal configuration　ppm信号構成のセットアップ これは何も信号を送信していないときのPPM信号の出力設定だと思う
+void setupPPM() // ---------- setup ppm signal configuration
 {
-  pinMode(OUTPUT_PIN, OUTPUT); //D2ピンを出力に設定
-  digitalWrite(OUTPUT_PIN, LOW); //D2ピンが0Vを出力
+  pinMode(OUTPUT_PIN, OUTPUT);
+  digitalWrite(OUTPUT_PIN, LOW);
   // CH_OFFSET = 2*CH_MAX - TIME_LOW + 20;// commom offset
   // TOTAL_CH_OFFSET = 8*CH_OFFSET;
-  pw[0] = CH_OFFSET - CH_NEUTRAL; // roll 1620-500=1120
-  pw[1] = CH_OFFSET - CH_NEUTRAL; // pitch 1620-500=1120
-  pw[2] = CH_OFFSET - CH_MIN;     // throttle 1620- 0=1620
-  pw[3] = CH_OFFSET - CH_NEUTRAL; // yaw 1620-500=1120
-  pw[4] = CH_OFFSET;              // AUX1 1620
-  pw[5] = CH_OFFSET;              // AUX2 1620
-  pw[6] = CH_OFFSET;              // AUX3 1620
-  pw[7] = CH_OFFSET;              // AUX4 1620
-  start_H = PPM_PERIOD - (TOTAL_CH_OFFSET - 3 * CH_NEUTRAL - CH_MIN) - 9 * TIME_LOW; //22500-(12960 - 3*500 - 0) - 9 * 400 = 7440 Startのパルス幅
-  // start_H = PPM_PERIOD - (( pw[0] + pw[1] + pw[2] + pw[3] + pw[4] + pw[5] + pw[6] + pw[7] + pw[8])) - 9 * TIME_LOW; // 22500 - (12960 - 3 * 500 - 0) - 9 * 400 = 7440 Startのパルス幅
-  // CPUのクロック周波数でPPM信号を制御
-  Timer1.initialize(PPM_PERIOD); //マイクロ秒単位で設定 initialize(microseconds): Timer1の初期化とマイクロ秒単位でのタイマー時間指定　フレーム幅が終わったらタイマーを初期化
-  Timer1.attachInterrupt(Pulse_control); //attachInterrupt(func): タイマー終了時に呼び出す関数の指定 タイマーが終了したら1つ前のvoidのPulse_controlを読み込んでいる？
+  pw[0] = CH_OFFSET - CH_NEUTRAL; // roll
+  pw[1] = CH_OFFSET - CH_NEUTRAL; // pitch
+  pw[2] = CH_OFFSET - CH_MIN;     // throttle
+  pw[3] = CH_OFFSET - CH_NEUTRAL; // yaw
+  pw[4] = CH_OFFSET;              // AUX1
+  pw[5] = CH_OFFSET;              // AUX2
+  pw[6] = CH_OFFSET;              // AUX3
+  pw[7] = CH_OFFSET;              // AUX4
+  start_H = PPM_PERIOD - (TOTAL_CH_OFFSET - 3 * CH_NEUTRAL - CH_MIN) - 9 * TIME_LOW; 
+  Timer1.initialize(PPM_PERIOD);
+  Timer1.attachInterrupt(Pulse_control);
 }
--- experiment/arduino/ESPr_UART_PPM_jireiken/ESPr_UART_PPM_jireiken.ino
+++ experiment/arduino/ESPr_UART_PPM_jireiken/ESPr_UART_PPM_jireiken.ino
@@ -1,4 +1,4 @@
-// Wi-Fi または UART を利用して ESPWROOM にプロポ信号を送信し
+//UART を利用して ESPWROOM にプロポ信号を送信し
 // ESPWROOM から　FUTABA T10J にトレーナーコード経由でPPMを送るためのプログラム
 // arduino の設定は
 // http://trac.switch-science.com/wiki/esp_dev_arduino_ide
@@ -6,28 +6,11 @@
 // Arduino IDEの環境設定のほうもCPU Frequency を 160 MHz
 // PPM は　Down pulse
 #include <Arduino.h>
-// #include <ESP8266WiFi.h> //https://github.com/esp8266/Arduino
-// #include <WiFiUDP.h>
 #define CPU_FRE 160 // CPUクロック周波数 [MHz]
 #include <math.h>
 uint8_t i;
 unsigned int droneNumber = 252; //機体番号を入力
 #define SIGNAL_TIMEOUT 200000 //[us] Matlabと通信が切断されてから信号がリセットされるまでの時間
-
-/////////////////// WiFi関係 ////////////////////
-// ESPrのIPアドレスの設定
-// const char *ssid = "ACSLexperimentWiFi";
-// const char *password = "wifi-acsl-mse";
-// const char *ssid = "aterm-b3cabe-g";
-// const char *password = "1ddd2634b2f3e";
-// IPAddress myIP(192, 168, 1, droneNumber); // 機体により下番号変更
-
-// IPAddress gateway(192, 168, 1, 1); // PCが接続されているネットワークのゲートウェイのIPアドレスを入力する（MATLABのPCのIP）
-// const int my_udp_port = 8000;       //開放する自ポート
-// IPAddress subnet(255, 255, 255, 0);
-
-// WiFiUDP udp;
-
 boolean connected = false;
 volatile unsigned long last_received_time; // 最後に信号を受信した時間[us]を格納
 /////////////////// PPM関係 ////////////////////
@@ -65,24 +48,6 @@ unsigned long t_now;
 #define USEC2CLOCK(us) (us * CPU_FRE * 1L)
 
 //*********** local functions  *************************//
-// void connectToWiFi() // ---- setup connection to wifi
-// {
-//   WiFi.begin(ssid, password);
-//   WiFi.config(myIP, gateway, subnet);
-//   Serial.println("start_connect");
-//   while (WiFi.status() != WL_CONNECTED)
-//   {
-//     delay(500);
-//     Serial.print(".");
-//   }
-//   Serial.println("CONNECTED!");
-
-//   Serial.println("WiFi connected");
-//   Serial.print("Local IP address: ");
-//   Serial.println(myIP);
-//   Serial.println(WiFi.localIP());
-// }
-
 // 信号値をデフォルトに戻す関数
 void setDefaultPulseWidth(){
   pw[0] = CH_OFFSET - CH_NEUTRAL; // roll 1620 - 500 =1120
@@ -114,11 +79,6 @@ void receive() // ---------- loop function : receive signal by UDP
 {
   // ch : 0 - 1000 is converted to 1000 - 2000 throttle on FC
   int len = 0;
-  /////////////////// for UDP ////////////////////
-  // int packetSize = udp.parsePacket();
-  // if (packetSize){
-  //   len = udp.read(packetBuffer, packetSize);
-  // }
   /////////////////// for UART ////////////////////
   if (Serial.available() >= 2 * TOTAL_CH) 
   { 
@@ -142,14 +102,7 @@ void receive() // ---------- loop function : receive signal by UDP
 
       pw[i] = CH_OFFSET - pw[i];
       TOTAL_CH_W -= pw[i];
-      /*Serial.print("pw[ ");
-      Serial.print(i);
-      Serial.print(" ]");
-      Serial.println(pw[i]);*/
     }
-
-    // start_H = PPM_PERIOD - TOTAL_CH_W - 8 * CH_OFFSET - 9 * TIME_LOW; // 9 times LOW time in each PPM period
-    // start_H = PPM_PERIOD - (8 * CH_OFFSET - 3 * CH_NEUTRAL - CH_MIN) - 9 * TIME_LOW;
     start_H = TOTAL_CH_W - 9 * TIME_LOW;
   /////////////////// for Failsafe ////////////////////
   }else if (micros() - last_received_time >= SIGNAL_TIMEOUT){
@@ -164,7 +117,6 @@ void Pulse_control()
   {
     digitalWrite(OUTPUT_PIN, LOW);              // PPM -> LOW
     timer0_write(t_now + USEC2CLOCK(TIME_LOW)); // 次の割込み時間を指定
-    // Serial.println(TIME_LOW);
   }
   else if (n_ch == TOTAL_CH)
   {
@@ -176,15 +128,11 @@ void Pulse_control()
     }
     digitalWrite(OUTPUT_PIN, HIGH);             // PPM -> HIGH
     timer0_write(t_now + USEC2CLOCK(start_Hh)); // start 判定の H 時間待つ
-    // Serial.println(start_Hh);
   }
   else
   {
     digitalWrite(OUTPUT_PIN, HIGH);                            // PPM -> HIGH
     timer0_write(t_now + USEC2CLOCK((phw[n_ch]))); // 時間を指定
-    // Serial.print(phw[n_ch]);
-    // Serial.print(" + ");
-    // Serial.println(CH_OFFSET);
     n_ch++;
   }
 }
@@ -193,9 +141,6 @@ void Pulse_control()
 void setup()
 {
   Serial.begin(115200);
-  // connectToWiFi();
-  // udp.begin(my_udp_port);
-
   /////////////////// PPM関係 ////////////////////
   setupPPM();
   last_received_time = micros();
