4878bea69b7be5cd691fd960186f2fe3f0385c5b
YoshikiK-tcu <gg1912037@gmail.com>
2024-12-12 22:11:57 +0900

    牽引物の重心を求めるプログラム

--- Z_MOMOSE/cog_keisann.m
+++ Z_MOMOSE/cog_keisann.m
@@ -1,26 +1,96 @@
 %G
-% m1=allData.mAll{1, 1}.y{1};
-% m2=allData.mAll{1, 1}.y{2};
-% m3=allData.mAll{1, 1}.y{3};
-% mAll=allData.mAll{1, 1}.y{4};
-% a1 = loggers{2, 1}.estimator.pL.*m1;
-% a2 = loggers{3, 1}.estimator.pL.*m2; 
-% a3 = loggers{4, 1}.estimator.pL.*m3; 
-
-rigid_num=199;
-numberOFpc = 4;%pcの総数
-PCId = 3;%pcの番号
-N = round(rigid_num/2);%機体と分割後の牽引物の組数
-s = N-1*mod(rigid_num,2);%牽引物の分を引く(複数牽引でなかったら引かない)
-r = mod(s,numberOFpc);
-sParPc = (s-r)/numberOFpc;%各PCでいくつの組を制御するか
-Ns = ones(1,numberOFpc)*sParPc + [ones(1,r),zeros(1,numberOFpc-r)];%各PCで制御する組を決定
-N = Ns(PCId)+1*mod(rigid_num,2);%(複数牽引でなかったら足さない)
-addIds = zeros(1,length(Ns));%機体と分割後の牽引物分+牽引物分ずらしていく
-for i = 1:length(Ns)-1
-    addIds(i+1) = sum(Ns(1:i),2)+1*mod(rigid_num,2);
+clear time tl mL pL
+close all
+logNum = length(loggers);
+id = find(loggers{1, 1}.phase==102 );
+pL0 = loggers{1, 1}.sensor.p(:,id(1):id(end));  
+for i = 2:logNum
+    time{i-1} = loggers{i, 1}.t(id(1):id(end),:);
+    tl{i-1} = length(time{i-1});
+    mL{i-1} = loggers{i, 1}.estimator.mL(:,id(1):id(end));
+    pL{i-1} = loggers{i, 1}.estimator.pL(:,id(1):id(end));
 end
-% addIds = [0,Ns(1:end-1)*2+1*mod(rigid_num,2)];%機体と分割後の牽引物分+牽引物分ずらしていく
-addId = addIds(PCId);%このpcで加算するrigidのid
+mLAjust = ajust_index(time,logNum,tl,mL);
+pLAjust = ajust_index(time,logNum,tl,pL);
 
-addIds
+M = zeros(1,tl{1});
+for i = 1:logNum-1
+    M = M + mLAjust{i};
+end
+G = zeros(3,tl{1});
+for i = 1:logNum-1
+    G = G + pLAjust{i}.*mLAjust{i}./M;
+end
+G = G - pL0;
+i = 1;
+f(i) = figure;
+plot(time{1},G)
+hold on
+plot(time{1},pL0)
+grid minor
+i = i+1;
+
+f(i) = figure;
+plot3(G(1,:),G(2,:),G(3,:),"Marker","+","LineStyle","none","MarkerSize",0.5)
+hold on
+plot3(pL0(1,:),pL0(2,:),pL0(3,:),"Marker","+","LineStyle","none","MarkerSize",1)
+grid minor
+i = i+1;
+
+f(i) = figure;
+plot(G(1,:),G(2,:),"Marker","+","LineStyle","none","MarkerSize",0.5)
+hold on
+plot(pL0(1,:),pL0(2,:),"Marker","+","LineStyle","none","MarkerSize",1)
+grid minor
+i = i+1;
+% m1=allData.mAll{1, 1}.y{1,1};
+% m2=allData.mAll{1, 1}.y{1,2};
+% m3=allData.mAll{1, 1}.y{1,3};
+% mAll=allData.mAll{1, 1}.y{1,4};
+% a1 = loggers{2, 1}.estimator.pL.*loggers{2, 1}.estimator.mL;
+% a2 = loggers{3, 1}.estimator.pL.*loggers{3, 1}.estimator.mL;
+% a3 = loggers{4, 1}.estimator.pL.*loggers{4, 1}.estimator.mL;
+
+
+function vars =ajust_index(time,logNum,tl,vars)
+        for i = 2:logNum-1
+            if tl{i-1}<tl{i}%短い制御周期を長い物に合わせる
+                tmp{i} = zeros(size(vars{i-1},1),tl{i-1});
+                kNow = 1;
+                for j = 1:tl{i-1}
+                    tBase = time{i-1}(j);
+                    for k = kNow:tl{i-1}
+                            tNow =  time{i-1}(k);
+                        if tBase<tNow 
+                            if abs(tBase-time{i-1}(k))<abs(tBase-time{i-1}(k-1))
+                                tmp{i}(:,j) = vars{i}(:,k);
+                                kNow = k+1;
+                            else
+                                tmp{i}(:,j)= vars{i}(:,k-1);
+                                kNow = k;
+                            end
+                            break
+                        end
+                        
+                    end
+                end
+                vars{i}=tmp{i};
+            elseif tl{i-1}>tl{i}%長い制御周期を短い物に合わせる
+                tmp{i} = zeros(length(vars{i-1}),tl{i-1});
+                kNow = 1;
+                for j = 1:tl{i}
+                    tBase = time{i}(j);
+                    for k = kNow:tl{i-1}
+                            tNow =  time{i-1}(:,k);
+                        if tBase>tNow
+                            tmp{i}(:,k) = vars{i}(:,j);
+                        else
+                            kNow = k;
+                            break
+                        end
+                    end
+                end
+                vars{i}=tmp{i};
+            end
+        end
+    end
