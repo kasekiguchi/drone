3de1f94641577e15793ca54af229f5f229cdfdf6
yurikatooo <g2112022@tcu.ac.jp>
2024-12-19 14:57:34 +0900

    事例研究

--- controller/MPC_CONTROLLER_KOOPMAN_quadprog_experiment.m
+++ controller/MPC_CONTROLLER_KOOPMAN_quadprog_experiment.m
@@ -101,12 +101,19 @@ classdef MPC_CONTROLLER_KOOPMAN_quadprog_experiment < handle
             %%
             cha = vara{2}; %KMPC
 
+<<<<<<< Updated upstream
         if strcmp(cha,'f')
             if obj.flag_anti_spike_f < 10
                 obj.result.input = [obj.result.input(1);0;0;0];
                 obj.flag_anti_spike_f = obj.flag_anti_spike_f + 1;
+=======
+            if strcmp(cha,'f')
+                if obj.flag_anti_spike_f < 5
+                   obj.result.input = [obj.result.input(1);0;0;0];
+                   obj.flag_anti_spike_f = obj.flag_anti_spike_f + 1;
+                end
+>>>>>>> Stashed changes
             end
-        end
 
             %% データ表示用
             obj.input.u = obj.result.input; 
