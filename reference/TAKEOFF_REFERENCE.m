00c76e9b6245465d7aba29d91df8ad927afcacd4
YoshikiK-tcu <gg1912037@gmail.com>
2025-01-07 16:50:43 +0900

    鈴木事例

--- reference/TAKEOFF_REFERENCE.m
+++ reference/TAKEOFF_REFERENCE.m
@@ -25,7 +25,8 @@ classdef TAKEOFF_REFERENCE < handle
       if isempty( obj.base_state ) % first take
         obj.base_time=varargin{1}.t;
         obj.base_state = obj.self.estimator.result.state.p;
-        if obj.self.estimator.model.name == "load_mL_HL"||obj.self.estimator.model.name =="load"
+        obj.base_state(3) = obj.self.sensor.result.state.pL(3);
+        if obj.self.estimator.model.name == "load_mL_HL"
             obj.base_state = obj.self.sensor.result.state.pL;%======質量推定用
             obj.base_state(1:2) = obj.self.sensor.result.state.real_pL(1:2);%======質量推定用
         end
