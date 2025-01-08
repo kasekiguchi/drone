68350e13a2d9d477e655727e75743e73f370168a
yumamomose <104126005+yumamomose@users.noreply.github.com>
2025-01-08 19:25:42 +0900

    更新
    yaw回転可能

--- reference/cooperative/gen_ref_for_HL_Cooperative_Load.m
+++ reference/cooperative/gen_ref_for_HL_Cooperative_Load.m
@@ -7,33 +7,43 @@ syms t real
 % r0d : 牽引物の姿勢を表す回転行列
 % o0d, do0d ：牽引物の角速度・角加速度
 
-%6偕微分
-xd=xdt.pYaw;
-dxd =diff(xd,t);
-d2xd =diff(dxd,t);
-d3xd =diff(d2xd,t);
-d4xd =diff(d3xd,t);
-d5xd =diff(d4xd,t);
-ref = matlabFunction([xd;dxd;d2xd;d3xd;d4xd;d5xd],'vars',t);
-%回転行列
-q = xdt.q;
-Rx = [1 0 0; 0 cos(q(1)) -sin(q(1)); 0 sin(q(1)) cos(q(1))];
-Ry = [cos(q(2)) 0 sin(q(2)); 0 1 0; -sin(q(2)) 0 cos(q(2))];
-Rz = [cos(q(3)) -sin(q(3)) 0; sin(q(3)) cos(q(3)) 0; 0 0 1];
-R = Rx*Ry*Rz;
-dq =diff(q,t);
-dR = R*Skew(dq);
-d2q =diff(dq,t);
-d2R = dR*Skew(d2q);
-d3q =diff(d2q,t);
-d3R = d2R*Skew(d3q);
-d4q =diff(d3q,t);
-d4R = d3R*Skew(d4q);
-d5q =diff(d4q,t);
-d5R = d4R*Skew(d5q);
-zero13 = zeros(1,3);
+%位置
+    %5偕微分まで
+    xd=xdt.pYaw;
+    dxd =diff(xd,t);
+    d2xd =diff(dxd,t);
+    d3xd =diff(d2xd,t);
+    d4xd =diff(d3xd,t);
+    d5xd =diff(d4xd,t);
+    ref = matlabFunction([xd;dxd;d2xd;d3xd;d4xd;d5xd],'vars',t);
+%回転
+    %角度の5階微分まで
+    q = xdt.q;
+    dq =diff(q,t);
+    d2q =diff(dq,t);
+    d3q =diff(d2q,t);
+    d4q =diff(d3q,t);
+    d5q =diff(d4q,t);
+    %角度の微分の歪対称行列
+    dqh = Skew(dq);
+    d2qh = Skew(d2q);
+    d3qh = Skew(d3q);
+    d4qh = Skew(d4q);
+    d5qh = Skew(d5q);
+    %回転行列
+    Rx = [1 0 0; 0 cos(q(1)) -sin(q(1)); 0 sin(q(1)) cos(q(1))];
+    Ry = [cos(q(2)) 0 sin(q(2)); 0 1 0; -sin(q(2)) 0 cos(q(2))];
+    Rz = [cos(q(3)) -sin(q(3)) 0; sin(q(3)) cos(q(3)) 0; 0 0 1];
+    R = Rx*Ry*Rz;
+    %回転行列微分
+    dR = R*dqh;
+    d2R = dR*dqh + R*d2qh;%1,1
+    d3R = d2R*dqh + 2*dR*d2qh + R*d3qh;%1,2,1
+    d4R = d3R*dqh + 3*d2R*d2qh + 3*dR*d3qh + R*d4qh;%1,,3,3,1
+    d5R = d4R*dqh + 4*d3R*d2qh + 6*d2R*d3qh + 4*dR*d4qh + R*d5qh;%1,4,6,4,1
+    zero13 = zeros(1,3);
 
-rotms = matlabFunction([R;zero13;dR;zero13;d2R;zero13;d3R;zero13;d4R;zero13;d5R;zero13],'vars',t);%アルゴリズム上各行列ごとに0を入れている
+rotms = matlabFunction(q,[R;zero13;dR;zero13;d2R;zero13;d3R;zero13;d4R;zero13;d5R;zero13],'vars',t);%アルゴリズム上各行列ごとに0を入れている
 % dixd = [xd;dxd;d2xd;d3xd;d4xd;d5xd];
 % redixd = reshape(dixd,6,[]);
 % ref = matlabFunction(reshape([redixd(1:3,:);redixd(6,:)],[],1),'vars',t);
