7a15e595bba3d21289f45c6672943843f017cc5e
YoshikiK-tcu <gg1912037@gmail.com>
2025-02-03 11:25:52 +0900

    変更

--- sample/InputTransform_Thrust2Throttle_drone_KMPC.m
+++ sample/InputTransform_Thrust2Throttle_drone_KMPC.m
@@ -11,9 +11,8 @@ function u_trans_param = InputTransform_Thrust2Throttle_drone_KMPC(varargin)
     % u_trans_param.th_offset2 = 332.5; %hov,p2p332.5
 
     % iflight 6cell
-    u_trans_param.gain2 =[1000;1000;1000;100]; %hov,p2p[800,800,800,500]
-   % u_trans_param.th_offset2 = 331; %hov,olddata,[1500,1500,1500,200]のとき 
-    u_trans_param.th_offset2 = 333.5;
+    u_trans_param.gain2 =[1500;1500;1500;200]; %hov,p2p[800,800,800,500]
+    u_trans_param.th_offset2 = 331.5;
 
     % u_trans_param.gain_SuspendedLoad =[500;500;500;100]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
     % u_trans_param.th_offset_SuspendedLoad = 450;         % offset 3s[1021] 4s[900]　発掘[926]
