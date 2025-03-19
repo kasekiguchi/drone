d0732df7ff2ad7a9bcb831f1480e325592d8cc0a
YoshikiK-tcu <g2112022@tcu.ac.jp>
2025-03-19 16:46:27 +0900

    更新

--- sample/InputTransform_Thrust2Throttle_drone.m
+++ sample/InputTransform_Thrust2Throttle_drone.m
@@ -24,7 +24,7 @@ function u_trans_param = InputTransform_Thrust2Throttle_drone(varargin)
      %単純HL用↑
     
     %単機牽引用↓
-    u_trans_param.th_offset     = 331;%331;%tlとfで分けている理由はtlとfで異なったコントローラー・機体質量を扱えるようにするため．
+    u_trans_param.th_offset     = 331;%機体質量と釣り合うスロットルオフセット
     u_trans_param.th_offset_tl  = 260;  %テークオフとランディング初期オフセット。
     u_trans_param.gain          = [300;300;300;20];%　
     u_trans_param.gain_tl       = [300;300;300;20];%　　　
