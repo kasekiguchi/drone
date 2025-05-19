417ed0c76d67d5aa1f82df7d0616694f5579afb6
kyo fushu <g2401201@tcu.ac.jp>
2025-05-19 15:04:55 +0900

    1

--- mainGUI.m
+++ mainGUI.m
@@ -12,8 +12,9 @@ cellfun(@(xx) addpath(xx), tmp, 'UniformOutput', false);
 cd(cf); close all hidden; clear all; userpath('clear');
 %%
 clc
+ExpBaseMode = ["ExpMPC_KMC","ExpTestMotiveConnection","ExpHL","ExpMPC_HLMC"];
 SimBaseMode = ["SimVoronoi","SimHL","SimLiDAR","SimMCMPC","SimMPC","SimHLMCMPC","SimHLMPC"];
-ExpBaseMode = ["","ExpTestMotiveConnection","ExpHL","ExpMPC_HLMC"];
+
 fExp = 0;
 fDebug = 0; % 1: active : for debug function
 PInterval = 0.6; % sec : poling interval for emergency stop
