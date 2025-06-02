ed8162f8daee5072e32ffc267114c740eb7ced62
Shin-Aka <g2401201@tcu.ac.jp>
2025-06-02 16:52:21 +0900

    1

--- mainGUI.m
+++ mainGUI.m
@@ -9,14 +9,15 @@ else
 end
 [~, tmp] = regexp(genpath('.'), '\.\\\.git.*?;', 'match', 'split');
 cellfun(@(xx) addpath(xx), tmp, 'UniformOutput', false);
-cd(cf); close all hidden; clear all; userpath('clear');
+% cd(cf); 
+close all hidden; clear all; userpath('clear');
 %%
 clc
 ExpBaseMode = ["","ExpMPC_KMC","ExpTestMotiveConnection","ExpHL","ExpMPC_HLMC"];
 SimBaseMode = ["","SimMPC_KMC_GUI","SimHL","SimLiDAR","SimMCMPC","SimMPC","SimHLMCMPC","SimHLMPC"];
 
 fExp = 0;
-fDebug = 0; % 1: active : for debug function
+fDebug = 1; % 1: active : for debug function
 PInterval = 0.6; % sec : poling interval for emergency stop
 gui = SimExp(fExp,fDebug,PInterval);
 setappdata(gui.UIFigure, 'sim_running', true);
